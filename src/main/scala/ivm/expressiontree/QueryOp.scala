e2826cf3e3ff6aa29642f032f1776e70446c4346
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-10 15:49:58 +0200

    Comment on previous commit

--- src/main/scala/ivm/expressiontree/QueryOp.scala
+++ src/main/scala/ivm/expressiontree/QueryOp.scala
@@ -1,5 +1,9 @@
 package ivm.expressiontree
 
+/**
+ * An expression tree node operating on expressions. Interpretation returns the node itself because only exec will
+ * produce the actual result.
+ */
 trait QueryOp[T] extends QueryReifier[T] {
   override def interpret() = this
 }
