f5f900f62d1c245bf5cbd2acfb6b5025ecdb5a75
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-14 08:15:25 +0200

    Split QueryableTest.scala from Queryable.scala, misc

--- src/main/scala/ivm/expressiontree/Queryable.scala
+++ src/main/scala/ivm/expressiontree/Queryable.scala
@@ -1,8 +1,8 @@
 package ivm
 package expressiontree
 
-import collections.CollectionReifier
 import collection.mutable.ArrayBuffer
+import collections.CollectionReifier
 
 /**
  * Trait to mix in with Scala collections to make them queryable.
@@ -25,47 +25,19 @@ import collection.mutable.ArrayBuffer
  *
  */
 
-
 //Probably Repr is also needed, especially if the produced Iterable must also offer stronger
 //interfaces.
-trait Queryable[T, Repr] extends ChildlessQueryReifier[T] {
-  self : scala.collection.Traversable[T] with Repr =>
+//TODO: add notification interface!
+trait Queryable[T, Repr] extends ChildlessQueryReifier[T] /*with Publisher[Message[T]]*/ {
+  self : Traversable[T] with Repr =>
+  //type Pub = Queryable[T, Repr] //XXX? Should this be defined here already? Or should Pub be even more specific?
   def asQueryable: QueryReifier[T] = this
   def asCollection: Repr = this
   override def exec(isLazy: Boolean) = this
   //This allows selecting early how the query is to be executed.
-  // The alternative is to choose between exec(isLazy = true) and exec(isLazy = false)
-  //def asQueryableLazy: Iterable[T] = new CollectionReifier[T](this.view)
-}
-
-object QueryableTest {
-  def vect[T] = {
-    //val res = new Vector[Int] with Queryable[Int, Vector[Int]] //Vector is final
-    val res = new ArrayBuffer[Int] with Queryable[Int, ArrayBuffer[Int]]
-    res ++= Seq(1, 2, 3)
-    res
-  }
-  def main(args: Array[String]) {
-    import Lifting._
-    val v = vect[Int]
-
-    println("v: " + v)
-    val vPlusOne: ArrayBuffer[Int] = v.map(_ + 1) // confusingly, this works
-    println("vPlusOne: " + vPlusOne)
-    val vQueryable: QueryReifier[Int] = v.asQueryable
-    println("vQueryable: " + vQueryable)
-    assert(vQueryable == v)
-    //val vQueryablePlusOne2: ArrayBuffer[Int] = vQueryable.map((i: Int) => i + 1) //gives error
-    val vQueryablePlusOne: QueryReifier[Int] = vQueryable.map(_ + 1)
-    println("vQueryablePlusOne: " + vQueryablePlusOne)
-    println("vQueryablePlusOne.interpret.exec(): " + vQueryablePlusOne.interpret.exec())
-
-    val vColl: ArrayBuffer[Int] = v.asCollection
-    println("vColl: " + vColl)
-    assert(vColl == v)
-    val vCollPlusOne: ArrayBuffer[Int] = vColl.map(_ + 1)
-    println("vCollPlusOne: " + vCollPlusOne)
-  }
+  // The alternative is to choose between exec(isLazy = true) and exec(isLazy = false).
+  // XXX: this returns another object!
+  def asQueryableLazy: QueryReifier[T] = new CollectionReifier[T](this.view)
 }
 
 // vim: set ts=4 sw=4 et:
