2f350cffb556f8d65511e7a130e9c38d7570cf12
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-16 15:45:32 +0200

    Finish structure of Incr. view maintenance - some typing problems need to be discussed, and probably solved by merging back IncQueryReifier into QueryReifier.

--- src/main/scala/ivm/expressiontree/Observable.scala
+++ src/main/scala/ivm/expressiontree/Observable.scala
@@ -1,8 +1,6 @@
 package ivm.expressiontree
 
-import collection.mutable.{Subscriber, Publisher, Buffer, ArrayBuffer, Set}
-
-//import collection.mutable._
+import collection.mutable.{HashMap, Subscriber, Publisher, Buffer, Set}
 
 /**
  * Question: should we send updates before or after modifying the underlying collection? Here we do it before, so that
@@ -17,6 +15,27 @@ case class Remove[T](t: T) extends Message[T]
 case class Update[T](old: T, curr: T) extends Message[T]
 case class Reset[T]() extends Message[T]
 case class Script[T](changes: Message[T]*) extends Message[T]
+// All the maintainer classes/traits (MapMaintener, WithFilterMaintainer, FlatMapMaintainer) have a common structure of
+// "message transformers". That can be probably abstracted away: they should have a method
+// producedMessages: Message[T] => Seq[Message[T]], we should remove Script[T], implement publish in term of it in type:
+// Forwarder[T, U, Repr] extends Subscriber[Seq[Message[T]], Repr] with Publisher[Seq[Message[U]]]
+// notify(evts: Seq[Message[T]]) = publish(evts flatMap producedMessages)
+// This should just be the default implementation though, because it doesn't use batching.
+// Moreover, we might want to have by-name values there (impossible) or sth. alike - otherwise the pipeline will always
+// execute the maintenance (say, apply the mapped function) before we get a chance to say "better not".
+// Maybe we just bind the transformers together and pass them upwards together with the input, so that the concrete node
+// decides whether to do the first application or not.
+//
+// Importantly, producedMessages can be recursive to reuse code - that's better than making notify recursive, because
+// that forces to split notifications:
+//case Update(old, curr) =>
+//  notify(pub, Remove(old))
+//  notify(pub, Include(curr))
+//case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+//Moreover producedMessages is purely functional, unlike notify.
+//
+// Other point: this way, a pipeline of message transformers becomes simply a sequencing through >>= of monadic actions.
+// Plus we can use MonadPlus.mplus for composing observables.
 
 // This is for a modifiable sequence. Actually, it's a problem since we don't preserve ordering, but we should.
 // On the one hand, this interface matches a relation in the closest way.
@@ -85,18 +104,20 @@ trait BufferObserver[T, U, Repr] extends Subscriber[Message[T], Repr]/*Queryable
 
 //case class MyBufferMap[T, U, Repr](f: T => U) extends ArrayBuffer[U] with BufferObserver[T, U, Repr]
 
-//Trait implementing incremental view maintenance for Map operations.
-case class MapMaintainer[T, U, Repr](f: T => U) extends Subscriber[Message[T], Repr] with Publisher[Message[U]] {
+// Let us first implement incremental view maintenance for multisets.
+//Trait implementing incremental view maintenance for Map operations
+trait MapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Publisher[Message[U]] {
+  def fInt: T => U
   override def notify(pub: Repr, evt: Message[T]) {
     evt match {
-      case Include(v) => publish(Include(f(v)))
-      case Remove(v) => publish(Remove(f(v)))
+      case Include(v) => publish(Include(fInt(v)))
+      case Remove(v) => publish(Remove(fInt(v)))
       case Reset() => publish(Reset())
       //Here we implement an update by sending an update of the mapped element.
       //It's not clear whether this is any better. Moreover, what happens when we update a complex observable element?
       //If listeners also listen on the element itself, they are gonna get too many notifications.
       //Otherwise, they might ignore the notification from us... it's not clear.
-      case Update(old, curr) => publish(Update(f(old), f(curr)))
+      case Update(old, curr) => publish(Update(fInt(old), fInt(curr)))
 
       // These two cases are quite common: they basically mean that no special handling is provided for bulk events.
       // Implement these in a superclass for these maintenance operations.
@@ -109,12 +130,20 @@ case class MapMaintainer[T, U, Repr](f: T => U) extends Subscriber[Message[T], R
   }
 }
 
-//Trait implementing incremental view maintenance for WithFilter operations.
-case class WithFilterMaintainer[T, Repr](p: T => Boolean) extends Subscriber[Message[T], Repr] with Publisher[Message[T]] {
+// Trait implementing incremental view maintenance for WithFilter operations.
+// WithFilter clearly allows to represent the difference between two collections - however, such a WithFilter node
+// can be converted to a difference node.
+// OTOH, the semantics are the same for set difference, and slightly different but simpler to implement for multiset
+// difference. Imagine c1@{a, a, b} - c2@{a} implemented with c1 withFilter (x => !(c2 exists (x == _))) or with
+// multiset difference: we get {b} in the first case, {a, b} in the second.
+// XXX: suppose we're using a bag for incremental view maintenance of a uniqueness constraint.
+// Do we get a problem because of the different properties of subtraction?
+trait WithFilterMaintainer[T, Repr] extends Subscriber[Message[T], Repr] with Publisher[Message[T]] {
+  def pInt: T => Boolean
   override def notify(pub: Repr, evt: Message[T]) {
     evt match {
-      case Include(v) => if (p(v)) publish(Include(v))
-      case Remove(v) => if (p(v)) publish(Remove(v))
+      case Include(v) => if (pInt(v)) publish(Include(v))
+      case Remove(v) => if (pInt(v)) publish(Remove(v))
       case Reset() => publish(Reset())
 
       // These two cases are quite common: they basically mean that no special handling is provided for bulk events.
@@ -128,11 +157,24 @@ case class WithFilterMaintainer[T, Repr](p: T => Boolean) extends Subscriber[Mes
 }
 
 //Trait implementing incremental view maintenance for FlatMap operations.
-case class FlatMapMaintainer[T, U, Repr](f: T => Traversable[U]) extends Subscriber[Message[T], Repr] with Publisher[Message[U]] {
+//XXX: we don't listen yet to the individual collections! To subscribe, this needs to become a member of IncQueryReifier.
+//Moreover, we need the passed function to return IncQueryReifiers.
+trait FlatMapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Publisher[Message[U]] {
+  self: IncQueryReifier[U] => //? [T]? That's needed for the subscribe.
+  def fInt: T => QueryReifier[U] //XXX: QR |-> IncQR
+  var cache = new HashMap[T, QueryReifier[U]] //XXX: QR |-> IncQR
+  val subCollListener: Subscriber[Message[U], IncQueryReifier[U]] = null
   override def notify(pub: Repr, evt: Message[T]) {
     evt match {
-      case Include(v) => publish(Script(f(v).toSeq map (Include(_)): _*))
-      case Remove(v) => publish(Script(f(v).toSeq map (Remove(_)): _*))
+      case Include(v) =>
+        val fV = fInt(v)
+        //fV subscribe subCollListener
+        publish(Script(fV.exec().toSeq map (Include(_)): _*))
+      case Remove(v) =>
+        //val fV = fInt(v) //fV will not always return the _same_ result. We need a map from v to the returned collection. Damn!
+        val fV = cache(v)
+        //fV removeSubscription subCollListener
+        publish(Script(fV.exec().toSeq map (Remove(_)): _*))
       case Reset() => publish(Reset())
       //Here we cannot implement an update by sending an update of the mapped element. But we should.
       //It's not clear whether this is any better. Moreover, what happens when we update a complex observable element?
@@ -153,3 +195,85 @@ case class FlatMapMaintainer[T, U, Repr](f: T => Traversable[U]) extends Subscri
 // Probably they can be both implemented together. Look into the other implementation, use bags or sth.
 // There was a use-case I forget where other context information, other than a simple count, had to be stored.
 // Was it a path in a hierarchical index?
+
+//Don't make Repr so specific as IncCollectionReifier. Making Repr any specific
+//is entirely optional - it just enables the listener to get a more specific
+//type for the pub param to notify(), if he cares.
+class MapMaintainerExp[T,U](col: QueryReifier[T], f: FuncExp[T,U]) extends Map[T,U](col, f) with MapMaintainer[T, U, IncQueryReifier[T]] {
+  override def fInt = f.interpret()
+}
+class FlatMapMaintainerExp[T,U](col: QueryReifier[T], f: FuncExp[T,/* XXX Inc */QueryReifier[U]]) extends FlatMap[T,U](col, f) with FlatMapMaintainer[T, U, IncQueryReifier[T]] with IncQueryReifier[U] {
+  override def fInt = x => f.interpret()(x)
+}
+class WithFilterMaintainerExp[T](col: QueryReifier[T], p: FuncExp[T,Boolean]) extends WithFilter[T](col, p) with WithFilterMaintainer[T, IncQueryReifier[T]] {
+  override def pInt = p.interpret()
+}
+
+// Variant of CollectionReifier, which also sends event to derived collections. Note that this is not reified!
+// XXX: we forget to add mutation operations. But see Queryable and QueryableTest. So make this a trait which is mixed in
+// by Queryable.
+trait IncQueryReifier[T] extends QueryReifier[T] with Publisher[Message[T]] {
+  type Pub <: IncQueryReifier[T]
+  override def map[U](f: Exp[T] => Exp[U]): QueryReifier[U] = {
+    val res = new MapMaintainerExp[T, U](this, FuncExp(f))
+    this subscribe res
+    res
+    //val sub = new AnyRef with res.Sub /*Subscriber[Message[U], Map[T, U]]*/ {
+      //override def notify(pub: res.Pub, evt: Message[U]) {
+    //val sub = new Subscriber[Message[U], Publisher[Message[U]] /*MapMaintainerExp[T, U]*/] {
+    //}
+  }
+  /*override def notify[U](pub: MapMaintainerExp[Message[T], ], evt: Message[T]) {
+  }*/
+  override def withFilter(p: Exp[T] => Exp[Boolean]): QueryReifier[T] = {
+    val res = new WithFilterMaintainerExp[T](this, FuncExp(p))
+    this subscribe res
+    res
+  }
+  override def flatMap[U](f: Exp[T] => Exp[QueryReifier[U]]): QueryReifier[U] = {
+    val res = new FlatMapMaintainerExp[T, U](this, FuncExp(f))
+    this subscribe res
+    res
+  }
+  //XXX add join, and add union
+}
+
+
+// The root of an incremental view maintenance chain.
+
+//XXX: we might later want to inherit from ObservableSet and some specific set, instead of being an event forwarder.
+// Especially because you must add elements to innercol, not to an instanc of this class, which is inconvenient!
+class IncrementalSet[T](val innercol: ObservableSet[T]) extends IncQueryReifier[T] with ChildlessQueryReifier[T] with Subscriber[Message[T], ObservableSet[T]] {
+  type Pub <: IncrementalSet[T]
+  innercol subscribe this
+  override def exec(isLazy: Boolean) = if (isLazy) innercol.view else innercol
+  override def notify(pub: ObservableSet[T], evt: Message[T]) {
+    publish(evt)
+  }
+}
+
+//A class representing an intermediate or final result of an incremental query.
+class IncrementalResult[T] extends ChildlessQueryReifier[T] with Subscriber[Message[T], IncQueryReifier[T]] {
+  var set = new HashMap[T, Int]
+  override def exec(isLazy: Boolean) = set.keySet
+  private[this] def count(v: T) = set.getOrElse(v, 0)
+  override def notify(pub: IncQueryReifier[T], evt: Message[T]) {
+    evt match {
+      case Include(v) => set(v) = count(v) + 1
+      case Remove(v) =>
+        val vCount = count(v) - 1
+        if (vCount > 0)
+          set(v) = vCount
+        else
+          set -= v
+      case Reset() => set.clear()
+
+      // These two cases are quite common: they basically mean that no special handling is provided for bulk events.
+      // The handling here is valid more in general, but no batching is done.
+      case Update(old, curr) =>
+        notify(pub, Remove(old))
+        notify(pub, Include(curr))
+      case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+    }
+  }
+}
