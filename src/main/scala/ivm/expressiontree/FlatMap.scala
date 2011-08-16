c457b306a7576a98f43462220edbbdf65599c5f2
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-16 14:10:58 +0200

    Misc

--- src/main/scala/ivm/expressiontree/FlatMap.scala
+++ src/main/scala/ivm/expressiontree/FlatMap.scala
@@ -5,6 +5,4 @@ case class FlatMap[S, T](col: QueryReifier[S], f: FuncExp[S,QueryReifier[T]]) ex
   def genericConstructor = (v) => FlatMap(v(0).asInstanceOf[QueryReifier[S]], v(1).asInstanceOf[FuncExp[S,QueryReifier[T]]])
   override def containsExp[X](e: Exp[X]) = f.isOrContains(e) || col.isOrContains(e)
   override def exec(isLazy: Boolean) = col.exec(isLazy).flatMap((x) => f.interpret()(x).exec())
-
-
 }
