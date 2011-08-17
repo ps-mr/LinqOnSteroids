cf250706be76f69ea11a9cab47dce70a8ca26ebd
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-17 03:17:37 +0200

    IVM: cleanup of update processors Message have a new type (Seq[Message[T]]) and operations on collections (which are update processors) can be combined together through monadic bind (flatMap).

--- src/main/scala/ivm/expressiontree/Observable.scala
+++ src/main/scala/ivm/expressiontree/Observable.scala
@@ -4,17 +4,48 @@ import collection.mutable.{HashMap, Subscriber, Publisher, Buffer, Set}
 
 /**
  * Question: should we send updates before or after modifying the underlying collection? Here we do it before, so that
- * the subscribers can check the original collection value. Scala's interface does it afterwards. On the o
+ * the subscribers can check the original collection value. Scala's interface does it afterwards, so that the update 
  * User: pgiarrusso
  * Date: 13/8/2011
  */
 
-sealed trait Message[T]
+sealed trait Message[+T]
 case class Include[T](t: T) extends Message[T]
 case class Remove[T](t: T) extends Message[T]
 case class Update[T](old: T, curr: T) extends Message[T]
-case class Reset[T]() extends Message[T]
-case class Script[T](changes: Message[T]*) extends Message[T]
+case class Reset() extends Message[Nothing]
+//case class Script[T](changes: Message[T]*) extends Message[T]
+
+//Note: Publisher hangs onto the listeners directly, while in our scenario weak references are badly needed.
+trait MsgSeqPublisher[T] extends Publisher[Seq[Message[T]]] {
+  def publish(evt: Message[T]) {
+    publish(Seq(evt))
+  }
+}
+
+trait MsgSeqSubscriber[-T, -Repr] extends Subscriber[Seq[Message[T]], Repr]
+
+trait EvtTransformer[-T, U, -Repr] extends MsgSeqSubscriber[T, Repr] with MsgSeqPublisher[U] {
+  //Contract: transforms messages, potentially executes them.
+  def transformedMessages(v: Message[T]): Seq[Message[U]]
+
+  //Precondition: only ever pass Reset or Update nodes.
+  protected def defTransformedMessages(v: Message[T]): Seq[Message[U]] = {
+    v match {
+      case Reset() => Seq(Reset())
+      case Update(old, curr) =>
+        Seq(Remove(old), Include(curr)) flatMap transformedMessages
+      case _ =>
+        throw new IllegalArgumentException("Unexpected message in defTransformedMessages")
+      /*case Script(msgs @ _*) =>
+        msgs flatMap transformedMessages*/
+      //case v => producedMessages(v) //typechecks, makes the match exhaustive, causes a loop at runtime.
+    }
+  }
+
+  override def notify(pub: Repr, evts: Seq[Message[T]]) = publish(evts flatMap transformedMessages)
+}
+
 // All the maintainer classes/traits (MapMaintener, WithFilterMaintainer, FlatMapMaintainer) have a common structure of
 // "message transformers". That can be probably abstracted away: they should have a method
 // producedMessages: Message[T] => Seq[Message[T]], we should remove Script[T], implement publish in term of it in type:
@@ -34,12 +65,17 @@ case class Script[T](changes: Message[T]*) extends Message[T]
 //case Script(msgs @ _*) => msgs foreach (notify(pub, _))
 //Moreover producedMessages is purely functional, unlike notify.
 //
+//XXX: mostly done up to here.
+//
 // Other point: this way, a pipeline of message transformers becomes simply a sequencing through >>= of monadic actions.
 // Plus we can use MonadPlus.mplus for composing observables.
 
+
+
+
 // This is for a modifiable sequence. Actually, it's a problem since we don't preserve ordering, but we should.
 // On the one hand, this interface matches a relation in the closest way.
-trait ObservableBuffer[T] extends Buffer[T] with Publisher[Message[T]] {
+trait ObservableBuffer[T] extends Buffer[T] with MsgSeqPublisher[T] {
   type Pub <: ObservableBuffer[T]
   abstract override def clear() = {
     //publish(Script(this.map(Remove(_))))
@@ -66,7 +102,7 @@ trait ObservableBuffer[T] extends Buffer[T] with Publisher[Message[T]] {
     // Where we call toSeq is important - by creating the sequence once, we guarantee that the order of events is the
     // same as the actual insertion order.
     val seq = iter.toSeq
-    publish(Script(seq.map(Include(_)): _*))
+    publish(seq.map(Include(_)))
     super.insertAll(n, seq)
   }
 }
@@ -83,7 +119,7 @@ trait QueryableSet[T, Repr] extends Queryable[T, Repr] with ObservableSet[T] {
 
 // Here we don't get info about replaced elements. However, for observable elements, we should still register ourselves
 // to forward them.
-trait ObservableSet[T] extends Set[T] with Publisher[Message[T]] {
+trait ObservableSet[T] extends Set[T] with MsgSeqPublisher[T] {
   type Pub <: ObservableSet[T]
   abstract override def clear() = {
     //publish(Script(this.map(Remove(_))))
@@ -100,19 +136,21 @@ trait ObservableSet[T] extends Set[T] with Publisher[Message[T]] {
   }
 }
 
-trait BufferObserver[T, U, Repr] extends Subscriber[Message[T], Repr]/*QueryableBuffer[T, Repr]#Sub*/ with ObservableBuffer[U]
+trait BufferObserver[T, U, Repr] extends MsgSeqSubscriber[T, Repr]/*QueryableBuffer[T, Repr]#Sub*/ with ObservableBuffer[U]
 
 //case class MyBufferMap[T, U, Repr](f: T => U) extends ArrayBuffer[U] with BufferObserver[T, U, Repr]
 
 // Let us first implement incremental view maintenance for multisets.
 //Trait implementing incremental view maintenance for Map operations
-trait MapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Publisher[Message[U]] {
+trait MapMaintainer[T, U, Repr] extends EvtTransformer[T, U, Repr] {
   def fInt: T => U
-  override def notify(pub: Repr, evt: Message[T]) {
+  override def transformedMessages(evt: Message[T]) = {
     evt match {
-      case Include(v) => publish(Include(fInt(v)))
-      case Remove(v) => publish(Remove(fInt(v)))
-      case Reset() => publish(Reset())
+      case Include(v) => Seq(Include(fInt(v)))
+      case Remove(v) => Seq(Remove(fInt(v)))
+      case _ => defTransformedMessages(evt)
+
+      /*case Reset() => Reset()
       //Here we implement an update by sending an update of the mapped element.
       //It's not clear whether this is any better. Moreover, what happens when we update a complex observable element?
       //If listeners also listen on the element itself, they are gonna get too many notifications.
@@ -125,7 +163,7 @@ trait MapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Publis
       /*case Update(old, curr) =>
         notify(pub, Remove(old))
         notify(pub, Include(curr))*/
-      case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+      case Script(msgs @ _*) => msgs foreach (notify(pub, _))*/
     }
   }
 }
@@ -138,20 +176,13 @@ trait MapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Publis
 // multiset difference: we get {b} in the first case, {a, b} in the second.
 // XXX: suppose we're using a bag for incremental view maintenance of a uniqueness constraint.
 // Do we get a problem because of the different properties of subtraction?
-trait WithFilterMaintainer[T, Repr] extends Subscriber[Message[T], Repr] with Publisher[Message[T]] {
+trait WithFilterMaintainer[T, Repr] extends EvtTransformer[T, T, Repr] {
   def pInt: T => Boolean
-  override def notify(pub: Repr, evt: Message[T]) {
+  override def transformedMessages(evt: Message[T]) = {
     evt match {
-      case Include(v) => if (pInt(v)) publish(Include(v))
-      case Remove(v) => if (pInt(v)) publish(Remove(v))
-      case Reset() => publish(Reset())
-
-      // These two cases are quite common: they basically mean that no special handling is provided for bulk events.
-      // The handling here is valid more in general, but no batching is done.
-      case Update(old, curr) =>
-        notify(pub, Remove(old))
-        notify(pub, Include(curr))
-      case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+      case Include(v) => if (pInt(v)) Seq(Include(v)) else Seq.empty
+      case Remove(v) => if (pInt(v)) Seq(Remove(v)) else Seq.empty
+      case _ => defTransformedMessages(evt)
     }
   }
 }
@@ -159,23 +190,24 @@ trait WithFilterMaintainer[T, Repr] extends Subscriber[Message[T], Repr] with Pu
 //Trait implementing incremental view maintenance for FlatMap operations.
 //XXX: we don't listen yet to the individual collections! To subscribe, this needs to become a member of IncQueryReifier.
 //Moreover, we need the passed function to return IncQueryReifiers.
-trait FlatMapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Publisher[Message[U]] {
+trait FlatMapMaintainer[T, U, Repr] extends EvtTransformer[T, U, Repr] {
   self: IncQueryReifier[U] => //? [T]? That's needed for the subscribe.
   def fInt: T => QueryReifier[U] //XXX: QR |-> IncQR
   var cache = new HashMap[T, QueryReifier[U]] //XXX: QR |-> IncQR
-  val subCollListener: Subscriber[Message[U], IncQueryReifier[U]] = null
-  override def notify(pub: Repr, evt: Message[T]) {
+  val subCollListener: MsgSeqSubscriber[U, IncQueryReifier[U]] = null
+  override def transformedMessages(evt: Message[T]) = {
     evt match {
       case Include(v) =>
         val fV = fInt(v)
         //fV subscribe subCollListener
-        publish(Script(fV.exec().toSeq map (Include(_)): _*))
+        fV.exec().toSeq map (Include(_))
       case Remove(v) =>
         //val fV = fInt(v) //fV will not always return the _same_ result. We need a map from v to the returned collection. Damn!
         val fV = cache(v)
         //fV removeSubscription subCollListener
-        publish(Script(fV.exec().toSeq map (Remove(_)): _*))
-      case Reset() => publish(Reset())
+        fV.exec().toSeq map (Remove(_))
+      case _ => defTransformedMessages(evt)
+      /*case Reset() => publish(Reset())
       //Here we cannot implement an update by sending an update of the mapped element. But we should.
       //It's not clear whether this is any better. Moreover, what happens when we update a complex observable element?
       //If listeners also listen on the element itself, they are gonna get too many notifications.
@@ -186,7 +218,7 @@ trait FlatMapMaintainer[T, U, Repr] extends Subscriber[Message[T], Repr] with Pu
       case Update(old, curr) =>
         notify(pub, Remove(old))
         notify(pub, Include(curr))
-      case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+      case Script(msgs @ _*) => msgs foreach (notify(pub, _))*/
     }
   }
 }
@@ -212,15 +244,15 @@ class WithFilterMaintainerExp[T](col: QueryReifier[T], p: FuncExp[T,Boolean]) ex
 // Variant of CollectionReifier, which also sends event to derived collections. Note that this is not reified!
 // XXX: we forget to add mutation operations. But see Queryable and QueryableTest. So make this a trait which is mixed in
 // by Queryable.
-trait IncQueryReifier[T] extends QueryReifier[T] with Publisher[Message[T]] {
+trait IncQueryReifier[T] extends QueryReifier[T] with MsgSeqPublisher[T] {
   type Pub <: IncQueryReifier[T]
   override def map[U](f: Exp[T] => Exp[U]): QueryReifier[U] = {
     val res = new MapMaintainerExp[T, U](this, FuncExp(f))
     this subscribe res
     res
-    //val sub = new AnyRef with res.Sub /*Subscriber[Message[U], Map[T, U]]*/ {
+    //val sub = new AnyRef with res.Sub /*MsgSeqSubscriber[U, Map[T, U]]*/ {
       //override def notify(pub: res.Pub, evt: Message[U]) {
-    //val sub = new Subscriber[Message[U], Publisher[Message[U]] /*MapMaintainerExp[T, U]*/] {
+    //val sub = new MsgSeqSubscriber[U, MsgSeqPublisher[U] /*MapMaintainerExp[T, U]*/] {
     //}
   }
   /*override def notify[U](pub: MapMaintainerExp[Message[T], ], evt: Message[T]) {
@@ -243,37 +275,38 @@ trait IncQueryReifier[T] extends QueryReifier[T] with Publisher[Message[T]] {
 
 //XXX: we might later want to inherit from ObservableSet and some specific set, instead of being an event forwarder.
 // Especially because you must add elements to innercol, not to an instanc of this class, which is inconvenient!
-class IncrementalSet[T](val innercol: ObservableSet[T]) extends IncQueryReifier[T] with ChildlessQueryReifier[T] with Subscriber[Message[T], ObservableSet[T]] {
+class IncrementalSet[T](val innercol: ObservableSet[T]) extends IncQueryReifier[T] with ChildlessQueryReifier[T] with MsgSeqSubscriber[T, ObservableSet[T]] {
   type Pub <: IncrementalSet[T]
   innercol subscribe this
   override def exec(isLazy: Boolean) = if (isLazy) innercol.view else innercol
-  override def notify(pub: ObservableSet[T], evt: Message[T]) {
-    publish(evt)
+  override def notify(pub: ObservableSet[T], evts: Seq[Message[T]]) {
+    publish(evts)
   }
 }
 
 //A class representing an intermediate or final result of an incremental query.
-class IncrementalResult[T] extends ChildlessQueryReifier[T] with Subscriber[Message[T], IncQueryReifier[T]] {
+class IncrementalResult[T] extends ChildlessQueryReifier[T] with MsgSeqSubscriber[T, IncQueryReifier[T]] {
   var set = new HashMap[T, Int]
   override def exec(isLazy: Boolean) = set.keySet
   private[this] def count(v: T) = set.getOrElse(v, 0)
-  override def notify(pub: IncQueryReifier[T], evt: Message[T]) {
-    evt match {
-      case Include(v) => set(v) = count(v) + 1
-      case Remove(v) =>
-        val vCount = count(v) - 1
-        if (vCount > 0)
-          set(v) = vCount
-        else
-          set -= v
-      case Reset() => set.clear()
+  override def notify(pub: IncQueryReifier[T], evts: Seq[Message[T]]) {
+    for (evt <- evts) {
+      evt match {
+        case Include(v) => set(v) = count(v) + 1
+        case Remove(v) =>
+          val vCount = count(v) - 1
+          if (vCount > 0)
+            set(v) = vCount
+          else
+            set -= v
+        case Reset() => set.clear()
 
-      // These two cases are quite common: they basically mean that no special handling is provided for bulk events.
-      // The handling here is valid more in general, but no batching is done.
-      case Update(old, curr) =>
-        notify(pub, Remove(old))
-        notify(pub, Include(curr))
-      case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+        // These two cases are quite common: they basically mean that no special handling is provided for bulk events.
+        // The handling here is valid more in general, but no batching is done.
+        case Update(old, curr) =>
+          notify(pub, Seq(Remove(old), Include(curr)))
+        //case Script(msgs @ _*) => msgs foreach (notify(pub, _))
+      }
     }
   }
 }
