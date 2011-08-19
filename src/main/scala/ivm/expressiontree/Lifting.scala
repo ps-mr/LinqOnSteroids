73ed74794477cc531c853ecf77adb1484aa3f837
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-19 12:49:26 +0200

    Implement experimental lifter for function objects (test code still missing)
    These lifters are for now part of a separate object and must be thus explicitly imported.

--- src/main/scala/ivm/expressiontree/Lifting.scala
+++ src/main/scala/ivm/expressiontree/Lifting.scala
@@ -1,6 +1,5 @@
 package ivm.expressiontree
 
-
 object Lifting {
   //In Haskell:
   //instance Numeric a => Summable a (Plus a) where...
@@ -36,4 +35,30 @@ object Lifting {
   implicit def liftBool(x: Boolean) : Exp[Boolean] = Const(x)
   implicit def liftString(x: String) : Exp[String] = Const(x)
   implicit def liftPair[A,B](pair: (Exp[A],Exp[B])) : Exp[(A,B)] = Pair[A,B](pair._1, pair._2)
+
+  // Some experimental implicit conversions.
+  // With the current Scala compiler, given (f_ )(x), the compiler will try to use implicit conversion on (f _), because
+  // that code is equivalent to (f _).apply(x), and the compiler applies conversion to the target of a method invocation.
+  // However, given f(x), since f is just a method name and not the target of a method invocation, the compiler will not
+  // apply implicit conversions, including the ones below. This is highly irregular, and hopefully could be solved
+  // through a compiler plugin.
+  object FunctionLifter {
+    //Such an implicit conversion makes no sense - it might be needed if the function call, instead of its result, is
+    //to be present in the expression tree, but the compiler will not insert this call, but rather a Const conversion on
+    //the result. Should Const take its argument by-name? It can be argued that it should instead take its argument
+    //by-value.
+
+    //implicit def liftCall0[Res](f: () => Res) = Call0(f)
+
+    implicit def liftCall1[A0, Res](f: A0 => Res):
+      Exp[A0] => Exp[Res] = Call1(f, _)
+    implicit def liftCall2[A0, A1, Res](f: (A0, A1) => Res):
+      (Exp[A0], Exp[A1]) => Exp[Res] = Call2(f, _, _)
+    implicit def liftCall3[A0, A1, A2, Res](f: (A0, A1, A2) => Res):
+      (Exp[A0], Exp[A1], Exp[A2]) => Exp[Res] = Call3(f, _, _, _)
+    implicit def liftCall4[A0, A1, A2, A3, Res](f: (A0, A1, A2, A3) => Res):
+      (Exp[A0], Exp[A1], Exp[A2], Exp[A3]) => Exp[Res] = Call4(f, _, _, _, _)
+    implicit def liftCall5[A0, A1, A2, A3, A4, Res](f: (A0, A1, A2, A3, A4) => Res):
+      (Exp[A0], Exp[A1], Exp[A2], Exp[A3], Exp[A4]) => Exp[Res]= Call5(f, _, _, _, _, _)
+  }
 }
