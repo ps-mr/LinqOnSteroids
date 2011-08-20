e0906b1f5627e63eb24260e30f39328f4c96c457
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-20 20:18:59 +0200

    Appropriate CanBuildFrom definitions for Inc* collections in QueryableTest

--- src/test/scala/ivm/tests/QueryableTest.scala
+++ src/test/scala/ivm/tests/QueryableTest.scala
@@ -4,7 +4,7 @@ package tests
 //import expressiontree.{ObservableSet, Queryable, QueryReifier, Lifting, ObservableBuffer}
 import expressiontree._
 import collection.mutable.{SetLike, HashSet, Builder, BufferLike, IndexedSeqOptimized, ArrayBuffer}
-import collection.generic.{MutableSetFactory, GenericSetTemplate, GenericCompanion, GenericTraversableTemplate}
+import collection.generic.{SeqFactory, CanBuildFrom, MutableSetFactory, GenericSetTemplate, GenericTraversableTemplate}
 
 /*
  * Just an experiment, don't use for real! We are going to support Sets first, not Buffers, as semantics and use cases
@@ -13,27 +13,30 @@ import collection.generic.{MutableSetFactory, GenericSetTemplate, GenericCompani
 class IncArrayBuffer[T] extends ArrayBuffer[T] with
     Queryable[T, ArrayBuffer[T]] with ObservableBuffer[T] with GenericTraversableTemplate[T, IncArrayBuffer] with
     BufferLike[T, IncArrayBuffer[T]] with IndexedSeqOptimized[T, IncArrayBuffer[T]] with Builder[T, IncArrayBuffer[T]] {
-  override def companion = new GenericCompanion[IncArrayBuffer] {
-    override def newBuilder[U] = new IncArrayBuffer[U]
-  }
+  override def companion = IncArrayBuffer
   override def result() = this
   override def newBuilder = new IncArrayBuffer[T]
 }
 
-// The below usage of Queryable is broken! For that, I really need to have IncrementalSet be a mixin instead of a
-// decorator!
+object IncArrayBuffer extends SeqFactory[IncArrayBuffer] {
+  override def newBuilder[U] = new IncArrayBuffer[U]
+  implicit def canBuildFrom[U] = new GenericCanBuildFrom[U]
+}
 
 
 class IncHashSet[T] extends HashSet[T] with ObservableSet[T] with IncrementalSet[T]
    with Queryable[T, HashSet[T]]
    with /*extra templates:*/ GenericSetTemplate[T, IncHashSet] with SetLike[T, IncHashSet[T]] {
   type Pub <: IncHashSet[T] //Two different definitions of Pub are inherited, this one is a common subtype.
-  override def companion = new MutableSetFactory[IncHashSet] {
-    override def empty[U] = new IncHashSet[U]
-    // XXX: without the above definition the code compiles, but empty is defined through newBuilder, and newBuilder
-    // through empty, because one of the two is supposed to be overriden; not doing so results in infinite recursion.
-    // TODO: write test for that.
-  }
+  override def companion = IncHashSet
+}
+
+object IncHashSet extends MutableSetFactory[IncHashSet] {
+  implicit def canBuildFrom[A]: CanBuildFrom[Coll, A, IncHashSet[A]] = setCanBuildFrom[A]
+  override def empty[A] = new IncHashSet[A]
+  // XXX: without the above definition the code compiles, but empty is defined through newBuilder, and newBuilder
+  // through empty, because one of the two is supposed to be overriden; not doing so results in infinite recursion.
+  // TODO: write test for that.
 }
 
 object QueryableTest {
@@ -43,7 +46,7 @@ object QueryableTest {
     v ++= Seq(1, 2, 3)
 
     println("v: " + v)
-    val vPlusOne: ArrayBuffer[Int] = v.map(_ + 1) // confusingly, this works
+    val vPlusOne: IncArrayBuffer[Int] = v.map(_ + 1) // confusingly, this works
     val vQueryable: QueryReifier[Int] = v.asQueryable
     assert(vQueryable == v)
     //val vQueryablePlusOne2: ArrayBuffer[Int] = vQueryable.map((i: Int) => i + 1) //gives error
@@ -68,10 +71,13 @@ object QueryableTest {
     //under ++= Seq(1, 2, 3) //XXX
 
     println("v: " + v)
-    //val vPlusOne: IncHashSet[Int] = v.map(_ + 1) //Actually broken, we don't get IncHashSet here.
-    val vPlusOne: HashSet[Int] = v.map(_ + 1) //XXX return type
+    val vPlusOne: IncHashSet[Int] = v.map(_ + 1) //canBuildFrom gives us the expected return type
 
     val vIncUpd = new IncrementalResult[Int](v)
+    val v2: Queryable[Int, HashSet[Int]] = v //Queryable does not inherit from Traversable!
+    println("v2.map(_ + 1): " + v2.map(_ + 1))
+    println("v2.asQueryable.map(_ + 1): " + v2.asQueryable.map(_ + 1))
+    println("v2.asCollection.map(_ + 1): " + v2.asCollection.map(_ + 1))
     val vQueryable: QueryReifier[Int] = v.asQueryable  //XXX support this
     assert(vQueryable == v)
     //val vQueryablePlusOne2: ArrayBuffer[Int] = vQueryable.map((i: Int) => i + 1) //gives error
@@ -104,10 +110,11 @@ object QueryableTest {
     println("vQueryablePlusOne: " + vQueryablePlusOne)
     println("vQueryablePlusOne.interpret.exec(): " + vQueryablePlusOne.interpret.exec())
 
-    val vColl: HashSet[Int] = v.asCollection
+    val vColl: HashSet[Int] = v.asCollection //This is a simple upcast...
     println("vColl: " + vColl)
     assert(vColl == v)
-    val vCollPlusOne: HashSet[Int] = vColl.map(_ + 1)
+    val vCollPlusOne: HashSet[Int] = vColl.map(_ + 1) //Here, the resulting object has actually HashSet as dynamic type.
+    //Since vColl has a different static type, a different implicit is passed here.
     println("vCollPlusOne: " + vCollPlusOne)
   }
 
