378bad48e5ff2c177991ff63c5840dd9d2860e83
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-22 15:06:56 +0200

    OpenEncoding: try to extend it to full functionality
    I modularized the internal content (by splitting it into different traits) and started implementing TraversableExp, analogous to NumExp. Its implementation is definitely more complex and reveals some unexpected problems in comparison - namely, how to ensure that NumExp[T] instead of Exp[T] is deduced as the type of the passed closure. At least, we don't have the problem that T cannot be deduced.
    This commit also starts solving the given problem by saving into each TraversableExp an instance of CanBuildExp, and having ExpT as a type parameter of TraversableExp.
    Next, we must use ExpT in signatures and bodies of members of TraversableExp.

--- src/main/scala/ivm/expressiontree/OpenEncoding.scala
+++ src/main/scala/ivm/expressiontree/OpenEncoding.scala
@@ -21,57 +21,13 @@ import scala.math.Numeric
  * others in an object inheriting from that trait.
  * @author Paolo G. Giarrusso
  */
+
 object OpenEncoding {
   trait Exp[+T] {
     def interpret: T
-    def map[U, That <: Exp[U]](f: T => U)(implicit c: CanBuildExp[U, That]): That =
-      c buildExp Exp.app(f, this)
+    /*def map[U, That <: Exp[U]](f: T => U)(implicit c: CanBuildExp[U, That]): That =
+      c buildExp Exp.app(f, this)*/
     //TODO add flatMap and withFilter.
-  }
-  object Exp {
-    def app[T, U](f: T => U, t: Exp[T]): Exp[U] = App(f, t)
-  }
-
-  //A few nodes for expression trees.
-  case class Const[T](t: T) extends Exp[T] {
-    def interpret = t
-  }
-
-  case class App[T, U](f: T => U, t: Exp[T]) extends Exp[U] {
-    def interpret = f(t.interpret)
-  }
-  //Alternative encoding of operations which are not available for every Exp[T]
-  //but only for e.g. Exp[String], Exp[T <% Numeric] and so on.
-  //Idea: instead of having all those methods in Exp[T], and instead of having to
-  //convert instances of Exp[T] to instances of DoubleExp, StringExp, and so
-  //on, ensure that automatic conversions automatically and directly produce
-  //instances of DoubleExp, StringExp. This avoids the old problem with
-  //needing to apply two implicit conversions in sequence. Additionally,
-  //methods like map and so on reuse a trick like CanBuildFrom in scala
-  //collections to produce the right expression type.
-  //The mixins containing extra methods must be mixed in with ExpWrapper.
-
-  class ExpWrapper[+T](t: Exp[T]) extends Exp[T] {
-    def interpret = t.interpret
-  }
-  //trait NumExp[T](implicit isNum: Numeric[T]) extends Exp[T]
-  trait NumExp[T] extends Exp[T] {
-    //If this were not a mixin, this would be an implicit parameter:
-    def isNum: Numeric[T]
-    def +(that: NumExp[T]): NumExp[T] = Plus(this, that)(isNum)
-  }
-
-  case class NumExpWrap[T](t: Exp[T])(implicit val isNum: Numeric[T]) extends
-  ExpWrapper[T](t) with NumExp[T]
-
-  //Root node for all binary, associative and commutative operations. The
-  //intuition is that many operations (including optimizations) might apply
-  //for all of those - e.g. expression normalization.
-  abstract class CommutativeOp[T](a: NumExp[T], b: NumExp[T]) extends NumExp[T]
-
-  case class Plus[T](a: NumExp[T], b: NumExp[T])(implicit val isNum: Numeric[T]) extends CommutativeOp(a, b) {
-    def interpret =
-      isNum.plus(a.interpret, b.interpret)
   }
 
   //The other problem is constructing the right object, but I think that a
@@ -82,9 +38,94 @@ object OpenEncoding {
   //trait CanBuildExp[-Elem, +To <: Exp[Elem]]
   trait CanBuildExp[-Elem, +To] {
     def buildExp(e: Exp[Elem]): To
+    //XXX: This naming is probably better - maybe instances would then work as implicit conversions themselves!
+    def apply(e: Exp[Elem]): To = buildExp(e)
   }
-  trait OpenEncodingBase {
 
+  class ExpWrapper[+T](t: Exp[T]) extends Exp[T] {
+    def interpret = t.interpret
+  }
+
+  trait ExpressionTree {
+    //A few nodes for expression trees.
+    case class Const[T](t: T) extends Exp[T] {
+      def interpret = t
+    }
+    //Not exactly sure what I should use to represent applications.
+    case class App[T, U](f: T => U, t: Exp[T]) extends Exp[U] {
+      def interpret = f(t.interpret)
+    }
+    /*case class App2[T, U](f: Exp[T] => Exp[U], t: Exp[T]) extends Exp[U] {
+      def interpret = f(t).interpret
+    }*/
+
+    object Exp {
+      def app[T, U](f: T => U, t: Exp[T]): Exp[U] = App(f, t)
+    }
+    case class FuncExp[-T, +U](f: Exp[T] => Exp[U]) extends Exp[T => U] {
+      def interpret = t => f(Const(t)).interpret
+    }
+  }
+
+  trait NumExps {
+    this: ExpressionTree with NumExpressionTree =>
+    //Alternative encoding of operations which are not available for every Exp[T]
+    //but only for e.g. Exp[String], Exp[T <% Numeric] and so on.
+    //Idea: instead of having all those methods in Exp[T], and instead of having to
+    //convert instances of Exp[T] to instances of DoubleExp, StringExp, and so
+    //on, ensure that automatic conversions automatically and directly produce
+    //instances of DoubleExp, StringExp. This avoids the old problem with
+    //needing to apply two implicit conversions in sequence. Additionally,
+    //methods like map and so on reuse a trick like CanBuildFrom in scala
+    //collections to produce the right expression type.
+    //The mixins containing extra methods must be mixed in with ExpWrapper.
+
+    //trait NumExp[T](implicit isNum: Numeric[T]) extends Exp[T]
+    trait NumExp[T] extends Exp[T] {
+      //If this were not a mixin, this would be an implicit parameter:
+      def isNum: Numeric[T]
+      def +(that: NumExp[T]): NumExp[T] = Plus(this, that)(isNum)
+    }
+
+    case class NumExpWrap[T](t: Exp[T])(implicit val isNum: Numeric[T]) extends
+      ExpWrapper[T](t) with NumExp[T]
+  }
+
+  trait TraversableExps {
+    this: ExpressionTree =>
+    trait TraversableExp[T, ExpT] extends Exp[Traversable[T]] {
+      val cbeT: CanBuildExp[T, ExpT]
+      //XXX: Here I should use Map, FlatMap and WithFilter nodes, instead of generic application nodes. But that's beside the point I'm making for this encoding.
+      def map[U, That <: Exp[Traversable[U]]](f: Exp[T] => Exp[U])(implicit c: CanBuildExp[Traversable[U], That]): That =
+        c buildExp App((_: Traversable[T]) map FuncExp(f).interpret, this)
+
+      def flatMap[U, That <: Exp[Traversable[U]]](f: Exp[T] => Exp[Traversable[U]])(implicit c: CanBuildExp[Traversable[U], That]): That =
+        c buildExp App((_: Traversable[T]) flatMap FuncExp(f).interpret, this)
+
+      //XXX: probably we could just return the same type as us... couldn't we? Probably yes, but we still need a CanBuildExp to actually do the creation.
+      def withFilter[That <: Exp[Traversable[T]]](f: Exp[T] => Exp[Boolean])(implicit c: CanBuildExp[Traversable[T], That]): That =
+        c buildExp App((_: Traversable[T]) filter FuncExp(f).interpret, this) //We can't use withFilter underneath.
+
+      def union[U >: T, That <: Exp[Traversable[U]]](u: Exp[Traversable[U]])(implicit c: CanBuildExp[Traversable[U], That]): That =
+        c buildExp App((_: Traversable[T]) ++ u.interpret, this) //Should use an App node with two params.
+    }
+    case class TraversableExpWrap[T, ExpT](t: Exp[Traversable[T]])(implicit val cbeT: CanBuildExp[T, ExpT]) extends
+      ExpWrapper[Traversable[T]](t) with TraversableExp[T, ExpT]
+  }
+  trait NumExpressionTree {
+    this: NumExps =>
+    //Root node for all binary, associative and commutative operations. The
+    //intuition is that many operations (including optimizations) might apply
+    //for all of those - e.g. expression normalization.
+    abstract class CommutativeOp[T](a: NumExp[T], b: NumExp[T]) extends NumExp[T]
+
+    case class Plus[T](a: NumExp[T], b: NumExp[T])(implicit val isNum: Numeric[T]) extends CommutativeOp(a, b) {
+      def interpret =
+        isNum.plus(a.interpret, b.interpret)
+    }
+  }
+
+  trait OpenEncodingBase extends NumExps with TraversableExps with ExpressionTree with NumExpressionTree {
     //Use CanBuildExp for more precise automatic lifting. Problem: Scala seems
     //to shy to apply this conversion automatically, because "That" does not
     //match so obviously with the target type. Therefore, specializations such
@@ -102,6 +143,10 @@ object OpenEncoding {
         def buildExp(e: Exp[T]) = NumExpWrap(e)
       }
     implicit def toExp[T](t: T): Exp[T] = toExpTempl(t)
+    implicit def canBuildExpTrav[T, ExpT](implicit c: CanBuildExp[T, ExpT]) =
+      new CanBuildExp[Traversable[T], TraversableExp[T, ExpT]] {
+        def buildExp(e: Exp[Traversable[T]]) = TraversableExpWrap(e)(c)
+      }
   }
 
   object OpenEncoding extends OpenEncodingBase {
@@ -109,11 +154,19 @@ object OpenEncoding {
     //to toExpTempl. The difference between these two methods is thus in the
     //implicitly passed parameter (canBuildExpNum vs canBuildExp)
     implicit def toNumExp[T : Numeric](t: T): NumExp[T] = toExpTempl(t)
+    implicit def toTraversableExp[T, ExpT](t: Traversable[T])(implicit c: CanBuildExp[T, ExpT]): TraversableExp[T, ExpT] = toExpTempl(t)
+
     //Question: does implicit lookup find a most-specific solution just because
     //the target type is NumExp[T], and only passing canBuildExpNum gives that
     //type? toNumExp2 shows that this is not the case.
     def _toNumExp2[T : Numeric](t: T): Exp[T] = toExpTempl(t)
 
+    def testTraversable() {
+      val a: TraversableExp[Int, NumExp[Int]] = Seq(1)
+      //Neither of these work.
+      //val b = a.map(_ + 1)
+      //val b = a.map(1 + _)
+    }
     def main(args: Array[String]) {
       val a: NumExp[Int] = 1
       val b = a + 2
@@ -122,6 +175,7 @@ object OpenEncoding {
       //However, it does prefer to choose implicits defined in a subclass to implicits in a superclass.
       val d = Exp.app((x: Int) => x + 1, 1) //This works.
       println(b)
+      testTraversable()
     }
   }
 }
