ca9ff7547bf8ba67c67c8a6ec29a2b29ea5ea5e3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-22 17:10:18 +0200

    Remove redundant parens

--- src/main/scala/ivm/expressiontree/FlatMap.scala
+++ src/main/scala/ivm/expressiontree/FlatMap.scala
@@ -4,5 +4,5 @@ case class FlatMap[S, T](col: QueryReifier[S], f: FuncExp[S,QueryReifier[T]]) ex
   def children = Seq(col,f)
   def genericConstructor = (v) => FlatMap(v(0).asInstanceOf[QueryReifier[S]], v(1).asInstanceOf[FuncExp[S,QueryReifier[T]]])
   override def containsExp[X](e: Exp[X]) = f.isOrContains(e) || col.isOrContains(e)
-  override def exec() = col.exec().flatMap((x) => f.interpret()(x).exec())
+  override def exec() = col.exec().flatMap(x => f.interpret()(x).exec())
 }
