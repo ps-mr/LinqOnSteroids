1ef7de5be5282966160d7803c8d96012162c847b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-08-27 12:02:08 +0200

    FuncExp: fix formatting

--- src/main/scala/ivm/expressiontree/FuncExp.scala
+++ src/main/scala/ivm/expressiontree/FuncExp.scala
@@ -6,32 +6,26 @@ case class FuncExp[-S, +T](f: Exp[S] => Exp[T]) extends Exp[S => T] {
   override def toString() = {
     "(" + x.name + ") => " + f(x)
   }
-  def interpret() = (z) => f(Const(z)).interpret()
+  def interpret() = z => f(Const(z)).interpret()
   def apply(z: Exp[S]): Exp[T] = f(z) // or rather create App node here?
   def children = Seq(f(x))
-  def genericConstructor = (v) => makefun(v(0).asInstanceOf[Exp[T]], x)
+  def genericConstructor = v => makefun(v(0).asInstanceOf[Exp[T]], x)
 
   // alpha equivalence for functions! (modulo calls to scala functions)
-  override def equals(other: Any) : Boolean = other match {
-     case that : FuncExp[_,_] => (
-        { 
-          val s = gensym()  
-          that.canEqual(this) && f(s).equals(that.f(s))
-        }
-     )
-     case _ => false 
+  override def equals(other: Any): Boolean = other match {
+     case that: FuncExp[_,_] =>
+       val s = gensym()
+       that.canEqual(this) && f(s).equals(that.f(s))
+     case _ => false
   }
-  override def canEqual(other : Any) : Boolean = other.isInstanceOf[FuncExp[_,_]]
+  override def canEqual(other: Any): Boolean = other.isInstanceOf[FuncExp[_,_]]
   override def potentiallyEquals[S](other: Exp[S]) = other match {
-     case that : FuncExp[_,_] => (
-        { 
-          val s = gensym()  
-          that.canEqual(this) && f(s).potentiallyEquals(that.f(s))
-        }
-     )
-     case _ => false 
+     case that: FuncExp[_,_] =>
+       val s = gensym()
+       that.canEqual(this) && f(s).potentiallyEquals(that.f(s))
+     case _ => false
   }
-  
+
   // by using varzero, this definition makes sure that alpha-equivalent functions have the same hashcode
   // some non-alpha-equivalent functions will also have the same hash code, such as
   // (x) => (y) => x+y and (x) => (y) => x+x
@@ -42,9 +36,10 @@ case class FuncExp[-S, +T](f: Exp[S] => Exp[T]) extends Exp[S => T] {
 }
 
 object FuncExp {
-  private var varCounter : Int = 0;
+  private var varCounter: Int = 0;
   val varzero = gensym()
-  def gensym() : Var = { varCounter = varCounter + 1;  new Var("v"+varCounter) }
-  def makefun[S,T](e: Exp[T], v: Var) : FuncExp[S,T] = FuncExp((x) => e.substVar(v.name,x))
-  def makepairfun[S1,S2,T](e: Exp[T], v1: Var, v2: Var) : FuncExp[(S1,S2),T] = FuncExp(p => e.substVar(v1.name,Proj1(p)).substVar(v2.name,Proj2(p)))
+  def gensym(): Var = { varCounter += 1;  new Var("v"+varCounter) }
+  def makefun[S,T](e: Exp[T], v: Var): FuncExp[S,T] = FuncExp(x => e.substVar(v.name,x))
+  def makepairfun[S1,S2,T](e: Exp[T], v1: Var, v2: Var): FuncExp[(S1,S2),T] =
+    FuncExp(p => e.substVar(v1.name, Proj1(p)).substVar(v2.name, Proj2(p)))
 }
