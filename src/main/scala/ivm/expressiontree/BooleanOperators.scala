601bce7727de1a688b0a11983452cda80347c5e1
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-09-16 17:58:45 +0200

    Codingstyle fix on Klaus new code

--- src/main/scala/ivm/expressiontree/BooleanOperators.scala
+++ src/main/scala/ivm/expressiontree/BooleanOperators.scala
@@ -18,22 +18,26 @@ case class Not(x: Exp[Boolean]) extends UnaryOpExp[Boolean, Boolean](x) {
 
 object BooleanOperators {
   // convert formula to CNF using naive algorithm
-  def cnf(in: Exp[Boolean]) : Set[Exp[Boolean]] = {
+  def cnf(in: Exp[Boolean]): Set[Exp[Boolean]] = {
      in match {
        case And(x,y) => cnf(x) ++ cnf(y)
        case Not(Not(x)) => cnf(x)
        case Not(And(x,y)) => cnf(Or(Not(x), Not(y)))
-       case Not(Or(x,y)) => cnf(And(Not(x),Not(y)))
+       case Not(Or(x,y)) => cnf(And(Not(x), Not(y)))
        case Or(x,y) => {
          val cnfx = cnf(x)
          val cnfy = cnf(y)
-         if ((cnfx.size == 1) && (cnfy.size == 1)) return Set(Or(cnfx.head, cnfy.head))
-         val (c1,c2) = if (cnfx.size >1) (cnfy,cnfx) else (cnfx,cnfy)
-         val c1factors = c1.fold( Const(true))( (a,b) => And(a,b))
-         cnf(c2.map( (z) => Or(z,c1factors) ).fold( Const(true))( (a,b) => And(a,b)))
+         if ((cnfx.size == 1) && (cnfy.size == 1))
+           return Set(Or(cnfx.head, cnfy.head))
+         val (c1, c2) = if (cnfx.size > 1) (cnfy, cnfx) else (cnfx, cnfy)
+         val c1factors = c1.fold(Const(true)){
+           (a, b) => And(a, b)
+         }
+         cnf(c2.map(Or(_,c1factors)).fold(Const(true)){
+           (a, b) => And(a, b)
+         })
        }
        case _ => Set(in)
-
      }
   }
 }
