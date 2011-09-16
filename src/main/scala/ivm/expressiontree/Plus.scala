6926513adeb18a44b51a759a4aa094a598138e19
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-09-16 16:06:16 +0200

    Introduce CommutativeOp and make Plus descend from it

--- src/main/scala/ivm/expressiontree/Plus.scala
+++ src/main/scala/ivm/expressiontree/Plus.scala
@@ -1,6 +1,14 @@
 package ivm.expressiontree
 
-case class Plus[T](x: Exp[T], y: Exp[T])(implicit val isNum: Numeric[T]) extends BinaryOpSymmExp[T, T](x, y) {
+//Root node for all binary, associative and commutative operations. The
+//intuition is that many operations (including optimizations) might apply
+//for all of those - e.g. expression normalization.
+trait CommutativeOp[T] extends Exp[T] {
+  def x: Exp[T]
+  def y: Exp[T]
+}
+
+case class Plus[T](x: Exp[T], y: Exp[T])(implicit val isNum: Numeric[T]) extends BinaryOpSymmExp[T, T](x, y) with CommutativeOp[T] {
   def interpret() = isNum.plus(x.interpret(), y.interpret())
   def copy(x: Exp[T], y: Exp[T]) = Plus(x, y)
 }
