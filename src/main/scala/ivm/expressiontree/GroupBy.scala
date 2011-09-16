d7f77061680c9bf443c432d3c61e55b74052dc62
Klaus Ostermann <klaus@.(none)>
2011-09-16 22:06:18 +0200

    more support for maps and groupBy

--- src/main/scala/ivm/expressiontree/GroupBy.scala
+++ src/main/scala/ivm/expressiontree/GroupBy.scala
@@ -1,8 +1,8 @@
 package ivm.expressiontree
 
 
-case class GroupBy[S, T](col: QueryReifier[S], f: FuncExp[S,T]) extends MapReifier[T,QueryReifier[S]] {
-  def children = Seq(col,f)
-  def genericConstructor = (v) => GroupBy(v(0).asInstanceOf[QueryReifier[S]], v(1).asInstanceOf[FuncExp[S,T]])
-
+case class GroupBy[S, T](col: QueryReifier[S], f: FuncExp[S,T]) extends MapReifier[T,S] {
+  override def children = Seq(col,f)
+  override def genericConstructor = (v) => GroupBy(v(0).asInstanceOf[QueryReifier[S]], v(1).asInstanceOf[FuncExp[S,T]])
+  override def exec = col.exec().groupBy(f.interpret())
 }
--- src/main/scala/ivm/expressiontree/MapReifier.scala
+++ src/main/scala/ivm/expressiontree/MapReifier.scala
@@ -3,4 +3,21 @@ package ivm.expressiontree
 
 trait MapReifier[S,T] extends Exp[MapReifier[S,T]] {
   override def interpret() = this
+  def apply(key: Exp[S]): QueryReifier[T] = MapApply(this, key)
+  def exec : Map[S,Traversable[T]]
+}
+
+/* KO:
+ * This is less general than it should be, since we hardcode here that we only
+ * support maps whose values are lists.
+ * It would be better if MapApply would extend Exp[T] and GroupBy would extend  MapReifier[T,QueryReifier[S]]
+ * such that maps to arbitrary types are allowed.
+ * However, this requires to solve the problem that all "QueryReifier" functions (map, flatMap etc.)
+ * are available on Exp[Traversable[T]] and not only on QueryReifier[T]
+ * Hopefully this will be possible once Paolo is done with the "major surgery" he promised ;)
+*/
+case class MapApply[S,T](map: MapReifier[S,T], key: Exp[S]) extends QueryReifier[T] {
+  override def children = Seq(map,key)
+  override def genericConstructor = (v) => MapApply(v(0).asInstanceOf[MapReifier[S,T]], v(1).asInstanceOf[Exp[S]])
+  override def exec = map.exec(key.interpret())
 }
