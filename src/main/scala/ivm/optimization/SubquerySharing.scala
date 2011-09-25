57d5aee3210af69ac83e7df43c5c75103a615746
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-09-26 00:53:52 +0200

    SubquerySharing: clumsily pass correct class manifests (I guess)
    My only doubt is about the App(Const... line, where I changed
    the cast by guessing the correct type.
    I'll try passing manifests while getting nicer code.

--- src/main/scala/ivm/optimization/SubquerySharing.scala
+++ src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -15,14 +15,16 @@ class SubquerySharing(val subqueries: Map[Exp[_],_]) {
     (e) => e match {
         // KO: Paolo, maybe you can fix the two compiler errors in this code? They are both related to your encoding.
 
-        case WithFilter(c : Exp[FilterMonadic[t, _]], f @ FuncExpBody(Eq(lhs, rhs))) if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x))=> {
+        case WithFilter(c: Exp[FilterMonadic[_ /*t*/, _]], (f: FuncExp[t, _/*Boolean*/]) & FuncExpBody(Eq(lhs, rhs))) if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x))=> {
           f match {
-            case f2 : Eq[t2] => subqueries
-                                   .get(
+            case f2 : Eq[t2] =>
+              implicit val cmT: ClassManifest[t] = f.cmS
+              val cmT2 = f2.x.manifest.asInstanceOf[ClassManifest[t2]]
+              subqueries.get(
                                      c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]]
-                                      .map[t,Traversable[t]]( (x : Exp[t]) => x)(null,null,null,null)
-                                      .groupBy(FuncExp.makefun[t,t2](f2.y, f.x)(null,null).f)(null,null)) match {
-                                         case Some(t) => App(Const(t.asInstanceOf[ Any => _]), lhs)(null)
+                                      .map[t,Traversable[t]](identity)(implicitly, cmT, implicitly, implicitly)
+                                      .groupBy(FuncExp.makefun[t,t2](f2.y, f.x)(implicitly, cmT2).f)(implicitly, cmT2)) match {
+                                         case Some(t) => App(Const(t.asInstanceOf[Any => Traversable[t] /*?*/]), lhs)(implicitly)
                                          case None => e
                                       }
           }
