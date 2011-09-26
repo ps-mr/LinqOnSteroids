4f04b784de02245758689a4c3386ae83901db8be
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-09-26 14:04:53 +0200

    SubquerySharing: write the code more nicely

--- src/main/scala/ivm/optimization/SubquerySharing.scala
+++ src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -2,6 +2,7 @@ package ivm.optimization
 
 import ivm.expressiontree._
 import Lifting._
+import Util._
 import collection.generic.FilterMonadic
 
 class SubquerySharing(val subqueries: Map[Exp[_],_]) {
@@ -11,22 +12,47 @@ class SubquerySharing(val subqueries: Map[Exp[_],_]) {
         case None => e
       }
     }
+
+   private def groupByShareBody[T: ClassManifest, T2: ClassManifest](c: Exp[FilterMonadic[T, Traversable[T]]],
+                                                        f: FuncExp[T, Boolean], fEqBody: Eq[T2],
+                                                        completeExp: Exp[_]) = {
+     /*
+     //How the fuck does this typecheck?
+     val groupedBy2: Exp[T => Traversable[T]] = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+     //Reason: a FuncExp[T, T2] is also FuncExp[T, Any]; groupBy(f: FuncExp[T, Any]) gives Map[Any, Repr]
+     // (where Repr = Traversable[T] here) which can be _upcast_ to Any => Repr and then further upcast to T => Repr.
+     // It's an interesting anomaly of type inference with variance, but it does not produce unsoundness.
+     // Indeed, since Map[A, +B] is invariant in A, we can't upcast Map[Any, Repr] to Map[T, Repr] - see groupedBy5.
+     //val groupedBy3: Exp[T => Traversable[T]] = c.map(identity).groupBy[T2](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+     val groupedBy4: Exp[T => Traversable[T]] = c.map(identity).groupBy[Any](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+     //
+     val groupedBy5: Exp[Map[Any, Traversable[T]]] = c.map(identity).groupBy[Any](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+     */
+     //This code:
+     //val groupedBy = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+     //expands to this:
+     val groupedBy = c.map(identity).groupBy[T2](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+     
+     assertType[Exp[T2 => Traversable[T]]](groupedBy) //Just for documentation.
+
+     subqueries.get(groupedBy) match {
+       case Some(t) => App(Const(t.asInstanceOf[T2 => Traversable[T]]), fEqBody.x)
+       case None => completeExp
+     }
+   }
+
+
    val groupByShare: Exp[_] => Exp[_] = {
     (e) => e match {
         // KO: Paolo, maybe you can fix the two compiler errors in this code? They are both related to your encoding.
 
-        case WithFilter(c: Exp[FilterMonadic[_ /*t*/, _]], (f: FuncExp[t, _/*Boolean*/]) & FuncExpBody(Eq(lhs, rhs))) if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x))=> {
-          f match {
-            case f2 : Eq[t2] =>
-              implicit val cmT: ClassManifest[t] = f.cmS
-              val cmT2 = f2.x.manifest.asInstanceOf[ClassManifest[t2]]
-              subqueries.get(
-                                     c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]]
-                                      .map[t,Traversable[t]](identity)(implicitly, cmT, implicitly, implicitly)
-                                      .groupBy(FuncExp.makefun[t,t2](f2.y, f.x)(implicitly, cmT2).f)(implicitly, cmT2)) match {
-                                         case Some(t) => App(Const(t.asInstanceOf[Any => Traversable[t] /*?*/]), lhs)(implicitly)
-                                         case None => e
-                                      }
+        case WithFilter(c: Exp[FilterMonadic[_ /*t*/, _]], (f: FuncExp[t, _/*Boolean*/]) & FuncExpBody(Eq(lhs, rhs)))
+          if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x)) => {
+          f.body match {
+            case fEqBody: Eq[t2] =>
+              val cmT: ClassManifest[t] = f.cmS
+              val cmT2 = fEqBody.x.manifest.asInstanceOf[ClassManifest[t2]] //we have ClassManifest[_ <: t2], hence we need the cast :-(.
+              groupByShareBody[t, t2](c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]], f, fEqBody, e)(cmT, cmT2)
           }
         }
         case _ => e
