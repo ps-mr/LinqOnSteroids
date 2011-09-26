a8ff8d8a4b911291dcb1dbb44c3fc41fbfb8c2ae
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-09-26 18:19:30 +0200

    SubquerySharing: move implicit extraction

--- src/main/scala/ivm/optimization/SubquerySharing.scala
+++ src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -13,10 +13,12 @@ class SubquerySharing(val subqueries: Map[Exp[_],_]) {
       }
     }
 
-   private def groupByShareBody[T: ClassManifest, T2: ClassManifest](c: Exp[FilterMonadic[T, Traversable[T]]],
+   private def groupByShareBody[T, T2](c: Exp[FilterMonadic[T, Traversable[T]]],
                                                         f: FuncExp[T, Boolean],
                                                         fEqBody: Eq[T2], lhs: Exp[T2], rhs: Exp[T2],
                                                         completeExp: Exp[_]) = {
+     implicit val cmT = f.cmS
+     implicit val cmT2 = fEqBody.x.manifest.asInstanceOf[ClassManifest[T2]] //we have ClassManifest[_ <: t2], hence we need the cast :-(.
      /*
      //How the fuck does this typecheck?
      val groupedBy2: Exp[T => Traversable[T]] = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
@@ -44,19 +46,17 @@ class SubquerySharing(val subqueries: Map[Exp[_],_]) {
 
 
    val groupByShare: Exp[_] => Exp[_] = {
-    (e) => e match {
+    e => e match {
         case WithFilter(c: Exp[FilterMonadic[_ /*t*/, _]], (f: FuncExp[t, _/*Boolean*/]) & FuncExpBody(Eq(lhs, rhs))) =>
             f.body match {
               case fEqBody: Eq[t2] =>
-                val cmT: ClassManifest[t] = f.cmS
-                val cmT2 = fEqBody.x.manifest.asInstanceOf[ClassManifest[t2]] //we have ClassManifest[_ <: t2], hence we need the cast :-(.
-
                 if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x))
-                  groupByShareBody[t, t2](c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]], f, fEqBody, fEqBody.x, fEqBody.y, e)(cmT, cmT2)
+                  groupByShareBody[t, t2](c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]], f, fEqBody, fEqBody.x, fEqBody.y, e)
                 else if (lhs.isOrContains(f.x) && !rhs.isOrContains(f.x))
-                  groupByShareBody[t, t2](c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]], f, fEqBody, fEqBody.y, fEqBody.x, e)(cmT, cmT2)
+                  groupByShareBody[t, t2](c.asInstanceOf[Exp[FilterMonadic[t, Traversable[t]]]], f, fEqBody, fEqBody.y, fEqBody.x, e)
                 else
                   e
+              case _ => e
             }
         case _ => e
     }
