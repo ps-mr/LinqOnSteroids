e4c1d73b9e2deccee37438710a53d8e5d6fbdd63
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-10-10 01:26:17 +0200

    Indentation

--- src/main/scala/ivm/expressiontree/Var.scala
+++ src/main/scala/ivm/expressiontree/Var.scala
@@ -2,6 +2,6 @@ package ivm.expressiontree
 
 case class Var(id: Int) extends NullaryExp[Nothing] {
   def name = "v" + id
-	override def toString() = name
-	def interpret() = throw new Exception("interpret on var")
+  override def toString() = name
+  def interpret() = throw new Exception("interpret on var")
 }
