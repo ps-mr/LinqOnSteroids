07d2019e85a80d68f660bbe6c6d4411be1b4782b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-10-09 23:58:09 +0200

    IfInstanceOf: cleanup
    Among cleanups, I also fetch the erasure of cS once and for all,
    which might be a bit faster.

--- src/main/scala/ivm/expressiontree/IfInstanceOf.scala
+++ src/main/scala/ivm/expressiontree/IfInstanceOf.scala
@@ -1,15 +1,14 @@
 package ivm.expressiontree
 
-import ClassManifest._
 
-case class IfInstanceOf[T,S](x: Exp[T])(implicit val cS: ClassManifest[S]) extends UnaryOpExp[T,Option[S]](x) {
+case class IfInstanceOf[T,S](x: Exp[T])(implicit cS: ClassManifest[S]) extends UnaryOpExp[T,Option[S]](x) {
+  private[this] val classS = cS.erasure
   def copy(x: Exp[T]) = IfInstanceOf(x)
   def interpret() = {
     val v = x.interpret()
-    if (v == null) None else {
-      if (cS.erasure.isInstance(v))
-        Some(v.asInstanceOf[S])
-      else None
-    }
+    if (v == null || !classS.isInstance(v))
+      None
+    else
+      Some(v.asInstanceOf[S])
   }
 }
