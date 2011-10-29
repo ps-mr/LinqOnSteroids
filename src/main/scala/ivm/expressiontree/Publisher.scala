3a8cf38d395ce1acfd2cbc4335f9bc7050de8e16
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-10-29 21:16:33 +0200

    EqWeakReference: fix hashCode bug
    The code computed System.identityHashCode on an instance of Option.

--- src/main/scala/ivm/expressiontree/Publisher.scala
+++ src/main/scala/ivm/expressiontree/Publisher.scala
@@ -47,11 +47,11 @@ case class Reset() extends TravMessage[Nothing]
  */
 class EqWeakReference[+T >: Null <: AnyRef](t: T) extends WeakReference[T](t: T) with Equals {
   override def canEqual(that: Any) = that.isInstanceOf[EqWeakReference[_]]
+  private def getOrNull[S >: Null <: AnyRef](x: WeakReference[S]): S = x.get.orNull
   override def equals(other: Any) =
     other match {
       case that: AnyRef if that eq this => true
       case that: EqWeakReference[_] =>
-        def getOrNull[S >: Null <: AnyRef](x: WeakReference[S]): S = x.get.orNull
         (that canEqual this) &&
           //XXX: use eq or equals? equals makes more sense in general, but eq makes more sense for our use case.
           (getOrNull(this) eq getOrNull(that))
@@ -61,7 +61,8 @@ class EqWeakReference[+T >: Null <: AnyRef](t: T) extends WeakReference[T](t: T)
         // uses identity comparison.
         //super.equals(other)
     }
-  override def hashCode() = System.identityHashCode(get) //Use identityHashCode(_) instead of _.hashCode() to get a constant hashcode.
+  override def hashCode() = System.identityHashCode(getOrNull(this)) //Use identityHashCode(_) instead of _.hashCode() to get a constant hashcode;
+  // but call it on the actual object, not on some Option wrapper.
 }
 
 trait Publisher[+Evt] {
