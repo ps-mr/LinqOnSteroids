2282fffe7d70c087e5d08c07ebb81dea1306cda9
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-10-29 21:39:19 +0200

    Clarify and split tests for EqWeakReferenceTests
    The test content is the same.

--- src/test/scala/ivm/expressiontree/EqWeakReferenceTests.scala
+++ src/test/scala/ivm/expressiontree/EqWeakReferenceTests.scala
@@ -10,23 +10,34 @@ import ref.WeakReference
 */
 
 class EqWeakReferenceTests extends JUnitSuite with ShouldMatchersForJUnit {
-  def compare[T >: Null <: AnyRef](l: EqWeakReference[T], r: EqWeakReference[T]) {
-    l should (be === r)
-    l.hashCode() should (be === r.hashCode())
+  def assertEqualsAndSameHash[T](l: T, r: T) {
+    l should be === (r)
+    l.hashCode should be === (r.hashCode)
   }
+  def assertDiffers[T](l: T, r: T) {
+    l should not be === (r)
+  }
+
+  val o = new AnyRef()
   @Test
   def equals() {
-    val o = new AnyRef()
-    val a = new EqWeakReference(o)
-    val b = new EqWeakReference(o)
-    compare(a, b)
-    val nullRef = new EqWeakReference(null)
-    a should (not (be === nullRef))
-    nullRef should (not (be === a))
-    val d = new EqWeakReference(null)
-    compare(nullRef, d)
-    val e = new WeakReference(o) //Note that this is WeakReference, not EqWeakReference, and thus differs from a.
-    a should (not (be === e))
-    e should (not (be === a))
+    val refO1 = new EqWeakReference(o)
+    val refO2 = new EqWeakReference(o)
+    assertEqualsAndSameHash(refO1, refO2)
+
+    val nullRef1 = new EqWeakReference(null)
+    assertDiffers(refO1, nullRef1)
+    assertDiffers(nullRef1, refO1)
+
+    val nullRef2 = new EqWeakReference(null)
+    assertEqualsAndSameHash(nullRef1, nullRef2)
+  }
+
+  @Test
+  def eqWeakReferenceDiffersFromWeakReference() {
+    val eqWeakRef = new EqWeakReference(o)
+    val weakRef = new WeakReference(o) //Note that this is WeakReference, not EqWeakReference, and thus differs from a.
+    assertDiffers(eqWeakRef, weakRef)
+    assertDiffers(weakRef, eqWeakRef)
   }
 }
