59a47800eb98e9c0d673f59b888a540b0ed5890b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-11-17 00:16:42 +0100

    Performance test for results of identity transform, which are slower
    This is due to the fact that after an identity transform, variable
    substitution is used during execution. This can be addressed by using
    an environment instead of explicit substitution.

--- src/test/scala/ivm/tests/TransformTest.scala
+++ src/test/scala/ivm/tests/TransformTest.scala
@@ -7,6 +7,7 @@ import org.junit.Test
 import expressiontree.Exp
 import expressiontree.Lifting._
 import optimization.Optimization
+import performancetests.Benchmarking.benchMark
 
 
 /**
@@ -15,19 +16,59 @@ import optimization.Optimization
 */
 
 class TransformTest extends JUnitSuite with ShouldMatchersForJUnit {
-  val l: Exp[Traversable[Int]] = toExp(Vector.range(1, 10))
-  val l2: Exp[Traversable[Int]] = toExp(Vector.range(1, 10))
+  val debug = false
+  val warmUpLoops = if (debug) 1 else 2000
+  val sampleLoops = if (debug) 2 else 50
+  val execLoops = 10
+  val collSize = if (debug) 10 else 100
+  val l: Exp[Traversable[Int]] = toExp(Vector.range(1, collSize))
+  val l2: Exp[Traversable[Int]] = toExp(Vector.range(1, collSize))
 
-  def testRebuild[T](t: Exp[T]) {
+  def testRebuild[T](t: Exp[T]) = {
     val tTransf = t transform identity
     tTransf should equal (t)
     assert(tTransf equals t)
+    (t, tTransf)
   }
+
   def testTransforms[T](t: Exp[T]) {
     testRebuild(t)
     testRebuild(Optimization.mergeFilters(t))
+    testRebuild(Optimization.optimize(t))
   }
-  
+
+  def testBenchmarkTransforms[T](t: Exp[T]) {
+    val (_, tTransf) = testRebuild(t)
+    benchMark("base", warmUpLoops = warmUpLoops, sampleLoops = sampleLoops, execLoops = execLoops) {
+      t.interpret()
+    }
+    benchMark("base again", warmUpLoops = warmUpLoops, sampleLoops = sampleLoops, execLoops = execLoops) {
+      t.interpret()
+    }
+    benchMark("after transform identity", warmUpLoops = warmUpLoops, sampleLoops = sampleLoops, execLoops = execLoops) {
+      tTransf.interpret()
+    }
+    benchMark("after transform identity, again", warmUpLoops = warmUpLoops, sampleLoops = sampleLoops, execLoops = execLoops) {
+      tTransf.interpret()
+    }
+    val tOptim = Optimization.optimize(t)
+    benchMark("after optimization", warmUpLoops = warmUpLoops, sampleLoops = sampleLoops, execLoops = execLoops) {
+      tOptim.interpret()
+    }
+    benchMark("after optimization, again", warmUpLoops = warmUpLoops, sampleLoops = sampleLoops, execLoops = execLoops) {
+      tOptim.interpret()
+    }
+    println(t)
+    println(tTransf)
+    println(tOptim)
+  }
+
+  @Test def testTransformIdentityMap1 {
+    val query = for (c <- l) yield c + 1
+    testBenchmarkTransforms(query)
+    println()
+  }
+
   @Test def testTransformIdentity {
     testTransforms(for (c <- l) yield c)
     testTransforms(for (c <- l if true) yield c)
