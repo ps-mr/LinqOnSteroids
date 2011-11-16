10371bfefef7aed3d11d48ab5be58174844245f4
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-11-17 00:40:17 +0100

    Improvements to benchmarking, to make results more repeatable
    * Run GC between measurements, to prevent it from happening during
    measurements
    * Trace the activity of the harness. Using this in combination with
    -XX:+PrintCompilation -verbose:gc allows checking whether JIT compilation
    only happens during the warmup phase.
    The combination of these changes allows making results more stable and/or
    understanding the reasons for these instabilities (e.g. compilation which keeps
    happening after warmup).
    The next step will probably be switching to Caliper (the Java analog of
    Haskell Criterion, in some senses), after understanding how to write a
    frontend for it using the current interface without docs for Caliper.

--- src/test/scala/ivm/performancetests/Benchmarking.scala
+++ src/test/scala/ivm/performancetests/Benchmarking.scala
@@ -28,12 +28,16 @@ object Benchmarking {
    */
   def benchMark(name: String, execLoops: Int = 1, warmUpLoops: Int = 10, sampleLoops: Int = 5, verbose: Boolean = true, hasConsoleOutput: Boolean = false)
                (toBench: => Unit): Double = {
+    if (!hasConsoleOutput)
+      print(">>> Name = %s, starting warmup..." format name)
+
     for (i <- 1 to warmUpLoops)
       toBench
+
     if (hasConsoleOutput)
       println()
     else
-      print(">>> Name = %s, time = " format name)
+      print(" ending warmup, starting benchmarking...")
 
     val stats = new VarianceCalc
     for (i <- 1 to sampleLoops) {
@@ -41,17 +45,20 @@ object Benchmarking {
       for (i <- 1 to execLoops)
         toBench
       stats.update((System.nanoTime() - before) / execLoops)
+      System.gc()
     }
-    val avg = stats.avg / math.pow(10,6)
+    if (!hasConsoleOutput)
+      print(" ended benchmarking, name = %s, time = " format name)
+    val avgMs = stats.avg / math.pow(10,6)
     if (verbose) {
-      val devStd = math.sqrt(stats.variance) / math.pow(10,6)
+      val devStdMs = math.sqrt(stats.variance) / math.pow(10,6)
       //The error of the measured average as an estimator of the average of the underlying random variable
-      val stdErr = devStd / math.sqrt(sampleLoops)
+      val stdErrMs = devStdMs / math.sqrt(sampleLoops)
       if (hasConsoleOutput)
         print(">>> Name = %s, time = " format name)
-      println("%.3f +- %.3f (stdErr = %.3f)" format (avg, devStd, stdErr))
+      println("(%.3f +- %.3f (stdErr = %.3f)) ms" format (avgMs, devStdMs, stdErrMs))
     }
-    avg
+    avgMs
   }
 
   def silentBenchMark(name: String, execLoops: Int = 1, warmUpLoops: Int = 3, sampleLoops: Int = 3)
