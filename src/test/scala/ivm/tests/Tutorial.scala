f5e20e7d30b29769d7e48fa12fcdd8099cca3bf9
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-11-21 12:37:49 +0100

    Finally m	ove tests out of SimpleOpenEncoding

--- src/test/scala/ivm/tests/Tutorial.scala
+++ src/test/scala/ivm/tests/Tutorial.scala
@@ -6,6 +6,7 @@ import org.junit.Test
 import expressiontree._
 import Lifting._
 import optimization.Optimization
+import collection.{mutable, TraversableView}
 
 trait SmartIVMAPI {
   class Pimper[T](t: T) {
@@ -136,5 +137,182 @@ class Tutorial extends JUnitSuite with ShouldMatchersForJUnit with SmartIVMAPI w
     //Let us do the same on a Set, and get a Set out! XXX TODO
     //TODO: paste SimpleOpenEncoding here
     //Let us see that we can also perform interesting optimizations manually, or have them performed by an optimizer.
+  }
+}
+
+object SimpleOpenEncoding {
+  class ToQueryable[T](t: Traversable[T]) {
+    def asQueryable: Exp[Traversable[T]] = Const(t)
+  }
+  implicit def toQueryable[T](t: Traversable[T]) = new ToQueryable(t)
+
+  def show(name: String, v: Any) {
+    print(name + ": ")
+    println(v)
+  }
+
+  def showInterp(name: String, v: Exp[_]) {
+    show(name, v)
+    show(name + ".interpret", v.interpret)
+  }
+
+  def moreTests() {
+    println("testBug:")
+
+    val i: Exp[Int] = 1
+    show("i", i)
+    val i1: Exp[Int] = 1
+    show("i1", i1)
+    //One of the syntaxes we want to support - both ones just fail, with "could not find implicit value for parameter cTTE: ivm.expressiontree.OpenEncoding.CanBuildExp[Int,ExpT]"
+    //val a0: Exp[Traversable[Int]] = Seq(1, 2, 3, 5)
+    //val a0: Exp[Traversable[Int]] = Seq(1, 2, 3, 5).toTraversable
+    val a0: Exp[Seq[Int]] = Seq(1, 2, 3, 5)
+    show("a0", a0)
+
+    /*val a1 = toExpTempl(Seq(1, 2, 3, 5)) //Doesn't work well - canBuildExp[Seq[Int]]: CanBuildExp[Seq[Int], Exp[Seq[Int]]] is preferred to canBuildExpTrav[Int, NumExp[Int]]: CanBuildExp[Traversable[Int], TraversableExp[Int]].
+    show("a1", a1)
+    val a2 = toExpTempl(Seq(1, 2, 3, 5).toTraversable) //Doesn't work well either
+    show("a2", a2)
+    val a3: Any = toExpTempl(Seq(1, 2, 3, 5)) //Works better
+    show("a3", a3)
+    val a4: Exp[Seq[Int]] = toExpTempl(Seq(1, 2, 3, 5)) //Doesn't work well - that's equivalent to a1
+    show("a4", a4)
+    val a5: Exp[Traversable[Int]] = toExpTempl(Seq(1, 2, 3, 5)) //Works well apparently.
+    show("a5", a5)
+    val a6: TraversableExp[Int, NumExp[Int]] = Seq(1, 2, 3, 5) //This one obviously works.
+    show("a6", a6)
+
+    show("(like a3) toExpTempl(Seq(1, 2, 3, 5))", toExpTempl(Seq(1, 2, 3, 5)))
+    //show("toExpTempl(Seq(1, 2, 3, 5))(canBuildExpTrav)", toExpTempl(Seq(1, 2, 3, 5))(canBuildExpTrav))
+    show("toExpTempl(Seq(1, 2, 3, 5).toTraversable)(canBuildExpTrav)", toExpTempl(Seq(1, 2, 3, 5).toTraversable)(canBuildExpTrav))*/
+
+    println()
+  }
+
+  import Util.assertType
+
+  def testInadequate(c: Exp[Map[Int, Int]]) {
+    val e: Exp[Map[Int, Int]] = c map (_ match {
+      case Pair(a, b) => (a, b + 1) //Inadequate term, even if it's the first I wrote; it causes a crash
+    })
+    assertType[Exp[Map[Int, Int]]](e)
+    showInterp("e", e)
+  }
+
+  def testTraversableView(exp: Exp[Traversable[Int]]) {
+    val a = exp.view
+    showInterp("a", a)
+    assertType[Exp[Traversable[Int]]](a)
+    assertType[Exp[TraversableView[Int, Traversable[Int]]]](a)
+    val b = a.map(_ + 1)
+    assertType[Exp[Traversable[Int]]](b)
+    // The underscore is due to the type of TraversableView.canBuildFrom. However, it doesn't really matter - see the
+    // type of forcedB
+    assertType[Exp[TraversableView[Int, Traversable[_]]]](b)
+    //assertType[Exp[TraversableView[Int, Traversable[Int]]]](b)
+    showInterp("b", b)
+    val forcedB = b.force
+    showInterp("forcedB", forcedB)
+    assertType[Exp[Traversable[Int]]](forcedB)
+
+    val c = a.withFilter(_ <= 3)
+    assertType[Exp[Traversable[Int]]](c)
+    assertType[Exp[TraversableView[Int, Traversable[Int]]]](c)
+    showInterp("c", c)
+
+    val forcedC = c.force
+    showInterp("forcedC", forcedC)
+    assertType[Exp[Traversable[Int]]](forcedC)
+  }
+
+  def testNoMutableConst() {
+    val mutableD = mutable.Seq(1)
+    //XXX: As desired, thanks to noConstForMutableColl this code does not compile. It is unfortunate that we can't assert
+    // that some code does not compile. Recheck when changing this code.
+    //val mutExp: Exp[Traversable[Int]] = mutableD
+  }
+
+  def testMap() {
+    val c: Exp[Map[Int, Int]] = Map(1 -> 2, 2 -> 4, 3 -> 4)
+    showInterp("c", c)
+    // Type annotations on the results of map below are not needed to get the correct result, they just check that the
+    // result has the correct type.
+    //XXX: Implicit conversions are not used by the Scala compiler to fix pattern match errors.
+    val d = c map (unliftPair(_) match {
+      case (a, b) => (a, b + 1)
+    })
+    assertType[Exp[Map[Int, Int]]](d)
+    showInterp("d", d)
+    val d2 = c map (ab => (ab._1, ab._2 + 1))
+    assertType[Exp[Map[Int, Int]]](d2)
+    showInterp("d2", d2)
+    val d3 = c map (ab => (ab._1 + ab._2))
+    assertType[Exp[Iterable[Int]]](d3)
+    showInterp("d3", d3)
+
+    val d4 = c filter (ab => (ab._1 + ab._2 <= 4))
+    assertType[Exp[Map[Int, Int]]](d4)
+    showInterp("d4", d4)
+
+    val d5 = c withFilter (ab => (ab._1 + ab._2 <= 4))
+    assertType[Exp[TraversableView[(Int, Int), Map[Int, Int]]]](d5)
+    showInterp("d5", d5)
+
+    val d6 = d5 withFilter (ab => (ab._1 + ab._2 <= 4))
+    assertType[Exp[TraversableView[(Int, Int), Map[Int, Int]]]](d6)
+    showInterp("d6", d6)
+
+    val forced = d6.force
+    assertType[Exp[Map[Int, Int]]](forced)
+
+    val d7 = c groupBy (ab => ab._2)
+    assertType[Exp[Map[Int, Map[Int, Int]]]](d7)
+    showInterp("d7", d7)
+
+    val d8 = d7(4)
+    assertType[Exp[Map[Int, Int]]](d8)
+    showInterp("d8", d8)
+
+    testInadequate(c)
+  }
+
+  def testTraversable() {
+    moreTests()
+    testNoMutableConst()
+
+    val data = Seq(1, 2, 2, 3, 5, 5, 3)
+    val a: Exp[Seq[Int]] = data
+    val a2 = data.asQueryable
+    assertType[Exp[Traversable[Int]]](a2) //assertType[Exp[Seq[Int]]](a2)
+    val b1 = a.map(_ + 1)
+    val b2 = a2.map(1 + _)
+    val b3 = b1.map(2 + _)
+    showInterp("b1", b1)
+    showInterp("b2", b2)
+    showInterp("b3", b3)
+    val b4 = a groupBy identity
+    assertType[Exp[Map[Int, Traversable[Int]]]](b4)
+    showInterp("b4", b4)
+
+    testTraversableView(a)
+    testMap()
+  }
+
+  //Analogues of Exp.app. Given the different argument order, I needed to rename them to get a sensible name:
+  def withExp[T, U](t: Exp[T])(f: T => U): Exp[U] = (f: Exp[T => U])(t)
+  def withExpFunc[T, U](t: Exp[T])(f: Exp[T] => Exp[U]): Exp[U] = f(t)
+
+  def main(args: Array[String]) {
+    val a: Exp[Int] = 1
+    val b = a + 2
+    //With a smart signatures, type inference works:
+    val c2 = withExp(1)(_ + 1)
+    val c3 = withExpFunc(1)(_ + 1)
+
+    println(a)
+    println(b)
+    println(c2)
+    println(c3)
+    testTraversable()
   }
 }
