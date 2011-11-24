1ddbab6984c5dd754954146933c68a3046acbbdb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-11-24 16:07:06 +0100

    Fix IDEA warning
    toString has no side effects, so it must be "parameterless" rather than
    "empty-paren".

--- src/main/scala/ivm/expressiontree/Const.scala
+++ src/main/scala/ivm/expressiontree/Const.scala
@@ -2,7 +2,7 @@ package ivm.expressiontree
 
 case class Const[T](x: T) extends NullaryExp[T] {
   override def interpret() = x
-  override def toString() = {
+  override def toString = {
     val s =
     x match {
       //Printing all elements and then cutting the output is horribly expensive for huge collections, so try to avoid it.
@@ -11,7 +11,7 @@ case class Const[T](x: T) extends NullaryExp[T] {
       case coll: Traversable[_] =>
         coll.take(3).toString()
       case _ =>
-        x.toString()
+        x.toString
     }
     "Const(" + (if (s.length() > 100) s.take(100)+"..." else s) + ")"
   }
