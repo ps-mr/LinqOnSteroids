cd944e81eab36fec522ddd35c120dbf844567bbb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-11-24 16:05:09 +0100

    Const.toString(): make again explicit it's a Const node, and shorten output

--- src/main/scala/ivm/expressiontree/Const.scala
+++ src/main/scala/ivm/expressiontree/Const.scala
@@ -3,7 +3,16 @@ package ivm.expressiontree
 case class Const[T](x: T) extends NullaryExp[T] {
   override def interpret() = x
   override def toString() = {
-    val s = x.toString()
-    if (s.length() > 100) s.take(100)+"..." else s
+    val s =
+    x match {
+      //Printing all elements and then cutting the output is horribly expensive for huge collections, so try to avoid it.
+      //Of course, this does not work when x is not a collection but e.g. contains one, or when for any reason toString()
+      //takes a lot of time for any reason. Still, better than nothing.
+      case coll: Traversable[_] =>
+        coll.take(3).toString()
+      case _ =>
+        x.toString()
+    }
+    "Const(" + (if (s.length() > 100) s.take(100)+"..." else s) + ")"
   }
 }
