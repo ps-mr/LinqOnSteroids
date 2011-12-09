f3c9ee74e5a60192442e6ffb76702c0ea49d79df
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-12-09 14:08:18 +0100

    Add ExoticTermException for "interpret on var"

--- src/main/scala/ivm/expressiontree/TypedVar.scala
+++ src/main/scala/ivm/expressiontree/TypedVar.scala
@@ -5,13 +5,15 @@ package ivm.expressiontree
  * Date: 16/11/2011
  */
 
+case class ExoticTermException(msg: String) extends IllegalArgumentException(msg)
+
 case class TypedVar[+T](id: Int) extends NullaryExp[T] {
   def name = "v" + id
   override def toString() = name
   def interpret(): T = {
     FuncExpInt.env.get().get(id) match {
       case Some(v) => v.asInstanceOf[T]
-      case None => throw new Exception("interpret on var")
+      case None => throw new ExoticTermException("interpret on Var")
     }
   }
 }
