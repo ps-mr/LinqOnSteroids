fad0f9436c58c85072e155747d4594e07d00d97b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-12-10 15:49:31 +0100

    Make also Call0 a case class

--- src/main/scala/ivm/expressiontree/Call0.scala
+++ src/main/scala/ivm/expressiontree/Call0.scala
@@ -1,5 +1,5 @@
 package ivm.expressiontree
 
-class Call0[Res](val id: Symbol, callfunc: () => Res) extends NullaryExp[Res] with Call[Res] {
+case class Call0[Res](id: Symbol, callfunc: () => Res) extends NullaryExp[Res] with Call[Res] {
   def interpret() = callfunc()
 }
