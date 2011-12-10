d23f644cb6d703afe01f50383038fc1c130880cb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-12-10 15:09:42 +0100

    Reindent SubquerySharing

--- src/main/scala/ivm/optimization/SubquerySharing.scala
+++ src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -8,47 +8,47 @@ import scala.collection.Map
 
 // Contract: Each map entry has the form Exp[T] -> T for some T
 class SubquerySharing(val subqueries: Map[Exp[_],Any]) {
-   val directsubqueryShare: Exp[_] => Exp[_] = {
-      (e) => subqueries.get(Optimization.normalize(e)) match {
-        case Some(t) => Const(t)
-        case None => e
-      }
+  val directsubqueryShare: Exp[_] => Exp[_] = {
+    (e) => subqueries.get(Optimization.normalize(e)) match {
+      case Some(t) => Const(t)
+      case None => e
     }
+  }
 
-   private def groupByShareBody[T, T2](c: Exp[Traversable[T]],
-                                                        f: FuncExp[T, Boolean],
-                                                        fEqBody: Eq[T2], lhs: Exp[T2], rhs: Exp[T2]) = {
-     /*
-     //How the fuck does this typecheck?
-     val groupedBy2: Exp[T => Traversable[T]] = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
-     //Reason: a FuncExp[T, T2] is also FuncExp[T, Any]; groupBy(f: FuncExp[T, Any]) gives Map[Any, Repr]
-     // (where Repr = Traversable[T] here) which can be _upcast_ to Any => Repr and then further upcast to T => Repr.
-     // It's an interesting anomaly of type inference with variance, but it does not produce unsoundness.
-     // Indeed, since Map[A, +B] is invariant in A, we can't upcast Map[Any, Repr] to Map[T, Repr] - see groupedBy5.
-     //val groupedBy3: Exp[T => Traversable[T]] = c.map(identity).groupBy[T2](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
-     val groupedBy4: Exp[T => Traversable[T]] = c.map(identity).groupBy[Any](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
-     //
-     val groupedBy5: Exp[Map[Any, Traversable[T]]] = c.map(identity).groupBy[Any](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
-     */
-     //This code:
-     //val groupedBy = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
-     //expands to this:
+  private def groupByShareBody[T, T2](c: Exp[Traversable[T]],
+                                      f: FuncExp[T, Boolean],
+                                      fEqBody: Eq[T2], lhs: Exp[T2], rhs: Exp[T2]) = {
+    /*
+    //How the fuck does this typecheck?
+    val groupedBy2: Exp[T => Traversable[T]] = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+    //Reason: a FuncExp[T, T2] is also FuncExp[T, Any]; groupBy(f: FuncExp[T, Any]) gives Map[Any, Repr]
+    // (where Repr = Traversable[T] here) which can be _upcast_ to Any => Repr and then further upcast to T => Repr.
+    // It's an interesting anomaly of type inference with variance, but it does not produce unsoundness.
+    // Indeed, since Map[A, +B] is invariant in A, we can't upcast Map[Any, Repr] to Map[T, Repr] - see groupedBy5.
+    //val groupedBy3: Exp[T => Traversable[T]] = c.map(identity).groupBy[T2](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+    val groupedBy4: Exp[T => Traversable[T]] = c.map(identity).groupBy[Any](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+    //
+    val groupedBy5: Exp[Map[Any, Traversable[T]]] = c.map(identity).groupBy[Any](FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+    */
+    //This code:
+    //val groupedBy = c.map(identity).groupBy(FuncExp.makefun[T, T2](fEqBody.y, f.x).f)
+    //expands to this:
 
-     val groupedBy = c.groupBy[T2](FuncExp.makefun[T, T2](rhs, f.x).f)
+    val groupedBy = c.groupBy[T2](FuncExp.makefun[T, T2](rhs, f.x).f)
 
-     assertType[Exp[T2 => Traversable[T]]](groupedBy) //Just for documentation.
-     subqueries.get(Optimization.normalize(groupedBy)) match {
-       case Some(t) => Some(App(Const(t.asInstanceOf[T2 => Traversable[T]]), lhs))
-       case None => None
-     }
-   }
+    assertType[Exp[T2 => Traversable[T]]](groupedBy) //Just for documentation.
+    subqueries.get(Optimization.normalize(groupedBy)) match {
+      case Some(t) => Some(App(Const(t.asInstanceOf[T2 => Traversable[T]]), lhs))
+      case None => None
+    }
+  }
 
   private def residualQuery[T](e: Exp[Traversable[T]], conds: Set[Exp[Boolean]], v: TypedVar[_ /*T*/]) : Exp[FilterMonadic[T, Traversable[T]]] = {
     if (conds.isEmpty) return e
     val residualcond : Exp[Boolean] = conds.reduce( (x,y) => And(x,y))
     e.withFilter(FuncExp.makefun[T,Boolean](residualcond,v))
-
   }
+
   private def tryGroupBy[T](c: Exp[Traversable[T]],
                                allConds: Set[Exp[Boolean]],
                                f: FuncExp[T,Boolean])
@@ -70,32 +70,32 @@ class SubquerySharing(val subqueries: Map[Exp[_],Any]) {
   //XXX: we should strip View if needed on _both_ sides before performing the match.
   val groupByShare2: Exp[_] => Exp[_] = {
    e => e match {
-       case Filter(c: Exp[Traversable[_ /*t*/]], f: FuncExp[t, _/*Boolean*/]) =>
-         val conds : Set[Exp[Boolean]] = BooleanOperators.cnf(f.body)
-         //Function.unlift is expensive.
-         val optimized : Option[Exp[_]]=
-            conds.collectFirst( Function.unlift( tryGroupBy(Optimization.stripView(c.asInstanceOf[Exp[Traversable[t]]]),conds,f)))
-         optimized.getOrElse(e)
-       case _ => e
+     case Filter(c: Exp[Traversable[_ /*t*/]], f: FuncExp[t, _/*Boolean*/]) =>
+       val conds : Set[Exp[Boolean]] = BooleanOperators.cnf(f.body)
+       //Function.unlift is expensive.
+       val optimized : Option[Exp[_]]=
+         conds.collectFirst( Function.unlift( tryGroupBy(Optimization.stripView(c.asInstanceOf[Exp[Traversable[t]]]),conds,f)))
+       optimized.getOrElse(e)
+     case _ => e
    }
  }
 
    val groupByShare: Exp[_] => Exp[_] = {
     e => e match {
-        case Filter(View(c: Exp[Traversable[_ /*t*/]]), (f: FuncExp[t, _/*Boolean*/]) & FuncExpBody(fEqBody: Eq[t2])) =>
-          val Eq(lhs, rhs) = fEqBody
-          val coll = Optimization.stripView(c.asInstanceOf[Exp[Traversable[t]]])
-          if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x))
-            groupByShareBody[t, t2](coll, f, fEqBody, lhs, rhs).getOrElse(e)
-          else if (lhs.isOrContains(f.x) && !rhs.isOrContains(f.x))
-            groupByShareBody[t, t2](coll, f, fEqBody, rhs, lhs).getOrElse(e)
-          else
-            e
-        case _ => e
+      case Filter(View(c: Exp[Traversable[_ /*t*/]]), (f: FuncExp[t, _/*Boolean*/]) & FuncExpBody(fEqBody: Eq[t2])) =>
+        val Eq(lhs, rhs) = fEqBody
+        val coll = Optimization.stripView(c.asInstanceOf[Exp[Traversable[t]]])
+        if (rhs.isOrContains(f.x) && !lhs.isOrContains(f.x))
+          groupByShareBody[t, t2](coll, f, fEqBody, lhs, rhs).getOrElse(e)
+        else if (lhs.isOrContains(f.x) && !rhs.isOrContains(f.x))
+          groupByShareBody[t, t2](coll, f, fEqBody, rhs, lhs).getOrElse(e)
+        else
+          e
+      case _ => e
     }
   }
 
   def shareSubqueries[T](query: Exp[T]) : Exp[T] = {
-      query.transform(directsubqueryShare.andThen(groupByShare2))
+    query.transform(directsubqueryShare.andThen(groupByShare2))
   }
 }
