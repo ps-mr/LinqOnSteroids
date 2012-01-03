547c516d195d3b7727b96790edab135d4c3a76b4
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-01-03 21:37:41 +0100

    Reassociation: Simplify tests

--- src/test/scala/ivm/optimization/OptimTests.scala
+++ src/test/scala/ivm/optimization/OptimTests.scala
@@ -12,6 +12,8 @@ import expressiontree.{Const, Plus, FuncExp, Exp}
  */
 
 class OptimTests extends JUnitSuite with ShouldMatchersForJUnit {
+  val x = FuncExp.gensym[Int]()
+
   def testIdempotence[T](e: Exp[T]) = {
     val opt = Optimization.reassociateOps(e)
     opt should be (Optimization.reassociateOps(opt))
@@ -21,40 +23,40 @@ class OptimTests extends JUnitSuite with ShouldMatchersForJUnit {
   @Test
   def reassociateOpsF() {
     def f(e: Exp[Int]) = e + 1
-    val composedF = FuncExp((x: Exp[Int]) => f(f(f(x))))
+    val composedF = f(f(f(x)))
 
     val optF = testIdempotence(composedF)
-    optF should be (FuncExp((x: Exp[Int]) => Plus(Const(3), x)))
+    optF should be (Plus(Const(3), x))
   }
 
   @Test
   def reassociateOpsG() {
     def g(e: Exp[Int]) = 1 + e
-    val composedG = FuncExp((x: Exp[Int]) => g(g(g(x))))
+    val composedG = g(g(g(x)))
     val optG = testIdempotence(composedG)
-    optG should be (FuncExp((x: Exp[Int]) => Plus(Const(3), x)))
+    optG should be (Plus(Const(3), x))
   }
 
   @Test
   def reassociateOpsH() {
-    val h = FuncExp((x: Exp[Int]) => 1 + (1 + x) + 1)
+    val h = 1 + (1 + x) + 1
     val optH = testIdempotence(h)
-    optH should be (FuncExp((x: Exp[Int]) => Plus(Const(3), x)))
+    optH should be (Plus(Const(3), x))
   }
 
   //Optimization results below are not the best, but it's hard to get this kind of patterns right in general
   //(consider increasing distance between constants)
   @Test
   def reassociateOpsI() {
-    val i = FuncExp((x: Exp[Int]) => x + x + 1 + x + 1)
+    val i = x + x + 1 + x + 1
     val optI = testIdempotence(i)
-    optI should be (FuncExp((x: Exp[Int]) => Plus(Plus(Plus(Const(2), x), x), x)))
+    optI should be (Plus(Plus(Plus(Const(2), x), x), x))
   }
 
   @Test
   def reassociateOpsJ() {
-    val j = FuncExp((x: Exp[Int]) => x + 1 + x + 1)
+    val j = x + 1 + x + 1
     val optJ = testIdempotence(j)
-    optJ should be (FuncExp((x: Exp[Int]) => Plus(Plus(Const(2), x), x)))
+    optJ should be (Plus(Plus(Const(2), x), x))
   }
 }
