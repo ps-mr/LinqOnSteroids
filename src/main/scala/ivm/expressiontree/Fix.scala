5251e2d9041cd6c6e75c470e87fdc6dae854402b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-01-16 00:04:09 +0100

    Review XXX: Reintroduce Fix

--- src/main/scala/ivm/expressiontree/Fix.scala
+++ src/main/scala/ivm/expressiontree/Fix.scala
@@ -1,13 +1,13 @@
 package ivm.expressiontree
 
 //Exploration into the design space.
-/*case class Fix[T](col: QueryReifier[T], f: FuncExp[Traversable[T],Traversable[T]]) extends QueryOp[T] {
-  def children = Seq(col,f)
-  def genericConstructor = (v) => Fix(v(0).asInstanceOf[QueryReifier[T]],
-                                       v(1).asInstanceOf[FuncExp[Traversable[T],Traversable[T]]])
-  override def exec() = {
+case class Fix[T](col: Exp[Traversable[T]], f: FuncExp[Traversable[T], Traversable[T]])
+  extends BinaryOp[Exp[Traversable[T]], FuncExp[Traversable[T], Traversable[T]], Traversable[T], Fix[T]](col, f)
+{
+  def copy(col: Exp[Traversable[T]], f: FuncExp[Traversable[T], Traversable[T]]) = Fix(col, f)
+  override def interpret() = {
     // XXX: very very basic implementation of fixpoint computation
-    var newV = col.exec()
+    var newV = col.interpret()
     var curr = newV
     do {
       curr = newV
@@ -16,19 +16,3 @@ package ivm.expressiontree
     newV
   }
 }
-
-case class FixWithReifiers[T](col: QueryReifier[T], f: FuncExp[QueryReifier[T],QueryReifier[T]]) extends QueryOp[T] {
-  def children = Seq(col,f)
-  def genericConstructor = (v) => FixWithReifiers(v(0).asInstanceOf[QueryReifier[T]],
-                                       v(1).asInstanceOf[FuncExp[QueryReifier[T],QueryReifier[T]]])
-  override def exec() = {
-    // XXX: another very very basic implementation. However, here we build QueryReifiers and compare their results. This might be potentially more efficient
-    var newV = col
-    var curr = newV
-    do {
-      curr = newV
-      newV = f.interpret()(curr)
-    } while (newV.interpret() != curr.interpret())
-    newV.exec()
-  }
-}*/
