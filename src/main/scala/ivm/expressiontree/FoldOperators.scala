445222be71ae6ed1c607fe3c8294689b113585aa
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-01-16 00:04:56 +0100

    Move groupBySel to reuse it

--- src/main/scala/ivm/expressiontree/FoldOperators.scala
+++ src/main/scala/ivm/expressiontree/FoldOperators.scala
@@ -3,7 +3,7 @@ package ivm.expressiontree
 import collection.{immutable, TraversableLike, mutable}
 import mutable.{Buffer, ArrayBuffer}
 import ivm.collections.IncArrayBuffer
-import collection.generic.CanBuildFrom
+import Util._
 
 /**
  * User: pgiarrusso
@@ -68,12 +68,6 @@ trait FoldOperators {
         })
     }
   }
-
-  class TraversableLike_GroupBySel_Op[T, Repr <: TraversableLike[T, Repr]](v: TraversableLike[T, Repr]) {
-    def groupBySel[K, Rest, That](f: T => K, g: T => Rest)(implicit c: CanBuildFrom[Repr, Rest, That]): Map[K, That] =
-      v.groupBy(f).map(v => (v._1, v._2.map(g)))
-  }
-  implicit def toTraversableLike_GroupBySel_Op[T, Repr <: TraversableLike[T, Repr]](v: TraversableLike[T, Repr]) = new TraversableLike_GroupBySel_Op(v)
 
   //Here I accept a primitive function because I believe the overhead for expression trees would be too significant, especially with all the wrapping and unwrapping done by convertBinFunInternal.
   //However, normalization-by-evaluation and a two-argument version of FuncExpInt could come to the rescue!
--- src/main/scala/ivm/expressiontree/Util.scala
+++ src/main/scala/ivm/expressiontree/Util.scala
@@ -1,5 +1,14 @@
 package ivm.expressiontree
 
+import collection.TraversableLike
+import collection.generic.CanBuildFrom
+
 object Util {
   def assertType[T](t: T) {}
+
+  class TraversableLike_GroupBySel_Op[T, Repr <: TraversableLike[T, Repr]](v: TraversableLike[T, Repr]) {
+    def groupBySel[K, Rest, That](f: T => K, g: T => Rest)(implicit c: CanBuildFrom[Repr, Rest, That]): Map[K, That] =
+      v.groupBy(f).map(v => (v._1, v._2.map(g)))
+  }
+  implicit def toTraversableLike_GroupBySel_Op[T, Repr <: TraversableLike[T, Repr]](v: TraversableLike[T, Repr]) = new TraversableLike_GroupBySel_Op(v)
 }
