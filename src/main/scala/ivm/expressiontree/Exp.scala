59f52e2bac0df2b3a5977d10a5f1a7c7d6afc20a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-01-16 14:34:35 +0100

    Increase access privilege to ease experimentation
    This makes it easier to experiment with optimizations.

--- src/main/scala/ivm/expressiontree/Exp.scala
+++ src/main/scala/ivm/expressiontree/Exp.scala
@@ -6,21 +6,21 @@ trait Exp[+T] extends MsgSeqPublisher[T, Exp[T]] {
 
   //XXX: does this really belong here?
   private[ivm] def pullAndPropagateContent() {}
-  private[ivm] def isRoot = roots.isEmpty
+  /*private[ivm]*/ def isRoot = roots.isEmpty
 
   //This method recomputes the contained value
-  private[ivm] def interpret(): T
+  /*private[ivm]*/ def interpret(): T
 
   //This method returns the cached value (if any) or invokes interpret().
   def expResult(): T = interpret()
 
   def nodeArity: Int
 
-  private[ivm] def children: Seq[Exp[_]]
-  private[ivm] def roots: Seq[Exp[RootType]] = Seq.empty
+  /*private[ivm]*/ def children: Seq[Exp[_]]
+  /*private[ivm]*/ def roots: Seq[Exp[RootType]] = Seq.empty
   protected def checkedGenericConstructor: Seq[Exp[_]] => Exp[T]
 
-  private[ivm] def genericConstructor(v: Seq[Exp[_]]): Exp[T] =
+  /*private[ivm]*/ def genericConstructor(v: Seq[Exp[_]]): Exp[T] =
     if (v.length == nodeArity)
       checkedGenericConstructor(v)
     else
@@ -28,25 +28,25 @@ trait Exp[+T] extends MsgSeqPublisher[T, Exp[T]] {
 
   // some child management auxiliary functions
 
-  private[ivm] def visitPreorder(visitor: Exp[_] => Unit, childSelector: Exp[_] => Seq[Exp[_]]) {
+  /*private[ivm]*/ def visitPreorder(visitor: Exp[_] => Unit, childSelector: Exp[_] => Seq[Exp[_]]) {
     visitor(this)
     for (c <- childSelector(this)) {
       c.visitPreorder(visitor, childSelector)
     }
   }
-  private[ivm] def visitPreorderRoots(visitor: Exp[_] => Unit) = visitPreorder(visitor, _.roots)
+  /*private[ivm]*/ def visitPreorderRoots(visitor: Exp[_] => Unit) = visitPreorder(visitor, _.roots)
 
-  private[ivm] def transform(transformer: Exp[_] => Exp[_]): Exp[T] = {
+  /*private[ivm]*/ def transform(transformer: Exp[_] => Exp[_]): Exp[T] = {
     val transformedChilds = for (c <- children) yield c.transform(transformer)
     val newself = genericConstructor(transformedChilds)
     transformer(newself).asInstanceOf[Exp[T]]
   }
-  private[ivm] def treeMap[S](mapper: (Exp[_], Seq[S]) => S): S = {
+  /*private[ivm]*/ def treeMap[S](mapper: (Exp[_], Seq[S]) => S): S = {
     val mappedChilds = for (c <- children) yield c.treeMap(mapper)
     mapper(this, mappedChilds)
   }
 
-  private[ivm] def find(filter: PartialFunction[Exp[_], Boolean]): Seq[Exp[_]] = {
+  /*private[ivm]*/ def find(filter: PartialFunction[Exp[_], Boolean]): Seq[Exp[_]] = {
     val baseSeq =
       if (PartialFunction.cond(this)(filter))
         Seq(this)
@@ -57,18 +57,18 @@ trait Exp[+T] extends MsgSeqPublisher[T, Exp[T]] {
 
   // This overload is not called find because that would confuse type inference - it would fail to infer that filter's
   // domain type is Exp[_].
-  private[ivm] def findTotFun(filter: Exp[_] => Boolean): Seq[Exp[_]] = find(filter.asPartial)
+  /*private[ivm]*/ def findTotFun(filter: Exp[_] => Boolean): Seq[Exp[_]] = find(filter.asPartial)
 
-  private[ivm] def isOrContains(e: Exp[_]): Boolean =
+  /*private[ivm]*/ def isOrContains(e: Exp[_]): Boolean =
     (this findTotFun (_ == e)).nonEmpty
 
-  private[ivm] def substSubTerm[S](SubTerm: Exp[_], e: Exp[S]) =
+  /*private[ivm]*/ def substSubTerm[S](SubTerm: Exp[_], e: Exp[S]) =
     transform(exp => exp match {
       case SubTerm => e
       case _ => exp
     })
 
-  private[ivm] def freeVars: Set[TypedVar[_]] = {
+  /*private[ivm]*/ def freeVars: Set[TypedVar[_]] = {
     def mapper(e: Exp[_], c: Seq[Set[TypedVar[_]]]): Set[TypedVar[_]] = e match {
       case v@Var(_) => Set(v)
       case fe@FuncExp(_) => c.fold(Set.empty)(_ union _).filter(!_.equals(fe.x))
@@ -91,7 +91,7 @@ trait Exp[+T] extends MsgSeqPublisher[T, Exp[T]] {
   final def !==(that: Null): Exp[Boolean] = Not(this === that)
 }
 
-private[ivm] object Exp {
+/*private[ivm]*/ object Exp {
   private def ordering[T] = new Ordering[Exp[T]] {
     override def compare(a: Exp[T], b: Exp[T]): Int = a.toString compareTo b.toString
   }
