a84b2152ba31130d3fdb435d930e1f63e7a073fa
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-02-22 23:37:18 +0100

    Indexing & Scalac bug report WIP

--- src/main/scala/ivm/optimization/SubquerySharing.scala
+++ src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -97,14 +97,47 @@ class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
   def lookupEq[T](e: Exp[T]): Set[Exp[_]] = {
     e match {
       case Filter(c: Exp[Traversable[_ /*t*/]], f: FuncExp[t, _ /*Boolean*/]) =>
+        def par2[A, B, C](f: (A, B) => C): ((A, B), (A, B)) => (C, C) = { case ((a1, b1), (a2, b2)) => (f(a1, b1), f(a2, b2))}
+        def union[T] = (_: Seq[T]) union (_: Seq[T])
+        def union2[T] = (_: Set[T]) union (_: Set[T])
+
         val conds: Set[Exp[Boolean]] = BooleanOperators.cnf(f.body)
+
+        /*conds.map {
+          case eq @ Eq(l, r) => l.find {case Var(_) => true}.toSet[Exp[Any]]
+          case _ => Set.empty[Exp[Any]]
+        }.fold(Set.empty[Exp[Any]])(_)
+
+        conds.map {
+          case eq @ Eq(l, r) => ((l.find {case Var(_) => true}).toSet, Set[Exp[_]](eq))
+          case _ => (Set.empty[Exp[_]], Set.empty[Exp[_]])
+        }.fold((Set.empty[Exp[_]], Set.empty[Exp[_]]))(par2(union2))*/
+        /*
+        //
+        [error]  found   : [T](scala.collection.Set[T], scala.collection.Set[T]) => scala.collection.Set[T]
+[error]  required: (Iterable[ivm.expressiontree.Exp[_]] with Int with ivm.expressiontree.Exp[Any] => Any, scala.collection.Set[ivm.expressiontree.Exp[_]]) => scala.collection.Set[ivm.expressiontree.Exp[_]]
+[error]         }.fold((Set.empty[Exp[_]], Set.empty[Exp[_]]))(par2(union2))
+[error]                                                             ^
+
+        */
+        /*
+        import scalaz._
+        import Scalaz._
+        */
+        def par[A, B, C](f: (A, B) => C): ((A, A), (B, B)) => (C, C) = { case ((a1, a2), (b1, b2)) => (f(a1, b1), f(a2, b2))}
+
         //Using Sets here directly is close to impossible, due to the number of wildcards and the invariance of Set.
         conds.map {
-          case Eq(l, r) => l.find {case Var(_) => true}
+          case eq @ Eq(l, r) => (l.find {case Var(_) => true}, Seq[Exp[_]](eq))
+          case _ => (Seq.empty, Seq.empty)
+        }.fold((Seq.empty, Seq.empty))(par2(union))
+
+        conds.map {
+          case eq @ Eq(l, r) => l.find {case Var(_) => true} union r.find {case Var(_) => true}
           case _ => Seq.empty
         }.fold(Seq.empty)(_ union _).toSet
       case FlatMap(c, f) =>
-        //Add to this the variables on which the free vars of subexp depend?
+        //Add to this the variables on which the free vars of subexp depend? No. Add all free variables bound in the location.
         lookupEq(c) union lookupEq(f.body)
       case MapOp(c, f) =>
         lookupEq(c) union lookupEq(f.body)
