63a7ea995f9acafb813a300f1b72f31857b3055d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-02-23 00:02:45 +0100

    Finish reducing bug

--- src/main/scala/ivm/optimization/BugReport.scala
+++ src/main/scala/ivm/optimization/BugReport.scala
@@ -1,22 +1,23 @@
+/*
 class BugReport {
-  trait Exp[+T] {
-    //def find(filter: PartialFunction[Exp[_], Boolean]): Seq[Exp[_]] = null
-  }
+  trait Exp[+T]
   case class Foo[T](v: Exp[T]) extends Exp[T]
 
   def bugReports() {
     val conds: Set[Exp[Boolean]] = null
 
-    //Both the following expressions must be present to trigger the bug.
+    //Both the following expressions must be present to trigger the bug. Commented out are variations I tried - when no
+    // explanation is present, it means that either variant triggers the bug.
 
     conds.map {
-      case /*eq @ */Foo(l) => l//.find {case _ => true}
-      case _ => Seq.empty
-    }.fold(Seq.empty)(_);
+      case Foo(l) => l //this must be l to trigger the bug
+      case _ => null// Seq.empty
+    }.fold(null /*Seq.empty*/)(_);
 
     conds.map {
-      case /*eq @ */Foo(l) => l//.find {case _ => true}
-      case _ => Seq.empty
-    }.fold(Seq.empty) _
+      case Foo(l) => 1// null//l //this can be null, l, 1, i.e. whatever.
+      case _ => null//Seq.empty
+    }.fold(null /*Seq.empty*/) _
   }
 }
+*/
