90e3ce98b8760c65791da09bedcf89ccde5dc6fd
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-02-23 16:00:30 +0100

    BooleanOperators.cnf: hide mutability

--- src/main/scala/ivm/expressiontree/booleanOperators.scala
+++ src/main/scala/ivm/expressiontree/booleanOperators.scala
@@ -1,6 +1,6 @@
 package ivm.expressiontree
 
-import collection.mutable.{Set, LinkedHashSet}
+import collection.mutable.{Set => MSet, LinkedHashSet}
 
 // Warning: it is important that these operators are short-circuiting. Therefore, they cannot be expressed through
 // CommOp like Plus and Times, and they are not commutative.
@@ -32,16 +32,16 @@ case class OptionGetOrElse[T](opt: Exp[Option[T]], default: Exp[T]) extends Arit
 object BooleanOperators {
   // convert formula to CNF using naive algorithm
   // contract: returns a set of clauses, each of which is a disjunction of literals
-  def cnf(in: Exp[Boolean]): Set[Exp[Boolean]] = {
+  def cnfInternal(in: Exp[Boolean]): MSet[Exp[Boolean]] = {
      in match {
        case And(x,y) =>
-         cnf(x) ++= cnf(y)
-       case Not(Not(x)) => cnf(x)
-       case Not(And(x, y)) => cnf(Or(Not(x), Not(y)))
-       case Not(Or(x, y)) => cnf(And(Not(x), Not(y)))
+         cnfInternal(x) ++= cnfInternal(y)
+       case Not(Not(x)) => cnfInternal(x)
+       case Not(And(x, y)) => cnfInternal(Or(Not(x), Not(y)))
+       case Not(Or(x, y)) => cnfInternal(And(Not(x), Not(y)))
        case Or(x, y) => {
-         val cnfx = cnf(x)
-         val cnfy = cnf(y)
+         val cnfx = cnfInternal(x)
+         val cnfy = cnfInternal(y)
          for (clauseX <- cnfx; clauseY <- cnfy)
            //Since both clauseX and clauseY are disjunction of literals, so is their disjunction, hence the result is a
            //correct CNF conversion.
@@ -50,4 +50,5 @@ object BooleanOperators {
        case _ => LinkedHashSet(in)
      }
   }
+  def cnf(in: Exp[Boolean]): Set[Exp[Boolean]] = cnfInternal(in).toSet
 }
