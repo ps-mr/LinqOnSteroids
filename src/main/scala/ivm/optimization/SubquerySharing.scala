1770af4bc3adfc08cab6ed854c7457bfdd060cfc
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-02-23 18:34:23 +0100

    First test-passing implementation of the nested indexing transformation

--- src/main/scala/ivm/optimization/SubquerySharing.scala
+++ src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -95,10 +95,10 @@ class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
   //Preconditions:
   //Postconditions: when extracting Some(parent), filterExp, foundEqs, allFVSeq, parent is a MapOp/FlatMap node which
   // contains filterExp as child as base collection (and not through a FuncExp node).
-  def lookupEq(parent: Option[Exp[_]],
+  def lookupEq(parent: Option[(Exp[Traversable[_]], FuncExp[_, _])],
                e: Exp[_],
                freeVars: Set[Exp[_]] = Set.empty,
-               fvSeq: Seq[Exp[_]] = Seq.empty): Seq[(Option[Exp[_]], Exp[_], Set[Exp[_]], Seq[Exp[_]])] = {
+               fvSeq: Seq[Exp[_]] = Seq.empty): Seq[(Option[(Exp[Traversable[_]], FuncExp[_, _])], Exp[_], Set[Exp[_]], Seq[Exp[_]])] = {
     require (fvSeq.toSet == freeVars)
     e match {
       case Filter(c: Exp[Traversable[_ /*t*/]], f: FuncExp[t, _ /*Boolean*/]) if parent.isDefined =>
@@ -115,35 +115,79 @@ class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
             case _ => Seq.empty
           }.fold(Seq.empty)(_ union _).toSet[Exp[_]]
         Seq((parent, e, foundEqs, fvSeq /*allFVSeq*/)) //Don't include the variable of the filter, which is going to be dropped anyway.
-      case FlatMap(c, f) =>
+      case FlatMap(c, f: FuncExp[t, u]) =>
         //Add all free variables bound in the location.
-        lookupEq(Some(e), c, freeVars, fvSeq) union lookupEq(None, f.body, freeVars + f.x, fvSeq :+ f.x)
-      case MapOp(c, f) =>
-        lookupEq(Some(e), c, freeVars, fvSeq) union lookupEq(None, f.body, freeVars + f.x, fvSeq :+ f.x)
+        lookupEq(Some((e.asInstanceOf[Exp[Traversable[u]]], f)), c, freeVars, fvSeq) union lookupEq(None, f.body, freeVars + f.x, fvSeq :+ f.x)
+      case MapOp(c, f: FuncExp[t, u]) =>
+        lookupEq(Some((e.asInstanceOf[Exp[Traversable[u]]], f)), c, freeVars, fvSeq) union lookupEq(None, f.body, freeVars + f.x, fvSeq :+ f.x)
       case _ => Seq.empty
     }
   }
 
-  private def tryGroupByNested[T](c: Exp[Traversable[T]],
+  private def groupByShareBodyNested[T, U](indexBaseToLookup: Exp[Traversable[Seq[T]]],
+                                      fx: Var,
+                                      fEqBody: Eq[U],
+                                      constantEqSide: Exp[U], 
+                                      varEqSide: Exp[U],
+                                      allFVSeq: Seq[Exp[_]],
+                                      parentNode: Exp[_/*T*/], parentF: FuncExp[_, _/*T, U*/],
+                                      tuplingTransform: (Exp[U], TypedVar[Seq[T]]) => Exp[U]): Option[Exp[Traversable[Seq[T]]]] = {
+    //val fxIdx = allFVSeq.indexOf(fx)
+    //val tupleV = FuncExp.gensym
+    val varEqSideTransf = tuplingTransform(varEqSide, fx.asInstanceOf[TypedVar[Seq[T]]]).asInstanceOf[Exp[U]]
+    val groupedBy = indexBaseToLookup.groupBy[U](FuncExp.makefun[Seq[T], U](varEqSideTransf, fx).f)
+
+    assertType[Exp[U => Traversable[Seq[T]]]](groupedBy) //Just for documentation.
+    subqueries.get(Optimization.normalize(groupedBy)) match {
+      case Some(t) => Some(App(Const(t.asInstanceOf[U => Traversable[Seq[T]]]), constantEqSide))
+      case None => None
+    }
+  }
+
+  private def tryGroupByNested[T /*, U, V*/](indexBaseToLookup: Exp[Traversable[Seq[T]]],
                             allConds: Set[Exp[Boolean]],
-                            fx: Var)
+                            fx: Var, FVSeq: Seq[Exp[_]], parentNode: Exp[Traversable[T]], parentF: FuncExp[_ /*T*/, _/*U*/])
                            (cond: Exp[Boolean]): Option[Exp[Traversable[T]]] =
     cond match {
       case eq: Eq[t2] =>
-        val oq: Option[Exp[Traversable[T]]] =
-          if (eq.t1.isOrContains(fx) && !eq.t2.isOrContains(fx))
-            groupByShareBody[T, t2](c, fx, eq, eq.t2, eq.t1)
-          else if (eq.t2.isOrContains(fx) && !eq.t1.isOrContains(fx))
-            groupByShareBody[T, t2](c, fx, eq, eq.t1, eq.t2)
+        val allFVSeq = FVSeq :+ fx
+        val allFVMap = allFVSeq.zipWithIndex.toMap
+        //val allFreeVars = allFVSeq.toSet
+        def usesFVars(e: Exp[_]) = e.findTotFun(allFVMap.contains(_)).nonEmpty
+        def tuplingTransform[T, U](e: Exp[T], v: TypedVar[Seq[U]]) = e.transform(
+          e => allFVMap.get(e) match {
+            case Some(idx) => v(idx)
+            case None => e
+          })
+
+        val oq: Option[Exp[Traversable[Seq[T]]]] =
+          if (usesFVars(eq.t1) && !usesFVars(eq.t2))
+            groupByShareBodyNested[T, t2](indexBaseToLookup, fx, eq, eq.t2, eq.t1, allFVSeq, parentNode, parentF, tuplingTransform)
+          else if (usesFVars(eq.t2) && !usesFVars(eq.t1))
+            groupByShareBodyNested[T, t2](indexBaseToLookup, fx, eq, eq.t1, eq.t2, allFVSeq, parentNode, parentF, tuplingTransform)
           else None
-        oq.map(e => residualQuery(e, allConds - eq, fx))
+        //We need replacements both in allConds and in parentF
+        //First transform parentF. Note that fx is _not_ in scope in its body, but it uses another variable; we need it to use
+        //the same variable for our purposes.
+        //XXX we should use gensym, not duplicate vars!
+        val newVar = fx.asInstanceOf[TypedVar[Seq[Any]]]
+        val transfParentF = parentF.body.substSubTerm(parentF.x, newVar)
+        val step1 = oq.map(e => residualQuery(e, (allConds - eq).map(tuplingTransform(_, newVar)), newVar))
+        
+        parentNode match {
+          case MapOp(_, _) =>
+            step1.map(e => e map FuncExp.makefun[Seq[T], T](tuplingTransform(transfParentF.asInstanceOf[Exp[T]], newVar), newVar).f)
+          case FlatMap(_, _) =>
+            step1.map(e => e flatMap FuncExp.makefun[Seq[T], TraversableOnce[T]](tuplingTransform(transfParentF.asInstanceOf[Exp[TraversableOnce[T]]], newVar), newVar).f)
+        }
+          //Note that we use always _map_ inside!
       case _ => None
     }
 
   val groupByShareNested: Exp[_] => Exp[_] = {
     e => {
       (for {
-        (Some(parent), filterExp, foundEqs, allFVSeq) <- lookupEq(None, e)
+        (Some((parentNode: Exp[Traversable[t]], parentF)), filterExp, foundEqs, allFVSeq) <- lookupEq(None, e)
       } yield {
         //if (foundEqs.nonEmpty) {}
         filterExp match {
@@ -152,8 +196,8 @@ class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
             val replacementBase = OptimizationTransforms.stripViewUntyped(c).asInstanceOf[Exp[Traversable[t]]]
             val indexQuery = replacementBase map (x => Seq(allFVSeq :+ x: _*))
 
-            val indexToLookup = e.substSubTerm(parent, indexQuery).asInstanceOf[Exp[Traversable[t]]]
-            val optimized: Option[Exp[_]] = collectFirst(conds)(tryGroupByNested(indexToLookup, conds, f.x.asInstanceOf[Var])(_))
+            val indexBaseToLookup = e.substSubTerm(parentNode, indexQuery).asInstanceOf[Exp[Traversable[Seq[Any]]]]
+            val optimized: Option[Exp[_]] = collectFirst(conds)(tryGroupByNested(indexBaseToLookup, conds, f.x.asInstanceOf[Var], allFVSeq, parentNode, parentF)(_))
             optimized.getOrElse(e)
           //case _ => e //Unneeded
         }
@@ -180,6 +224,6 @@ class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
 
   //Entry point
   def shareSubqueries[T](query: Exp[T]): Exp[T] = {
-    query.transform(directsubqueryShare.andThen(groupByShare2))
+    query.transform(directsubqueryShare.andThen(groupByShare2).andThen(groupByShareNested))
   }
 }
