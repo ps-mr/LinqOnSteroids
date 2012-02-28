50a9009ee76c30efa986ccfc1f855c55e8823c30
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-02-28 17:20:16 +0100

    Write specific test for the previous failure
    Writing this test was hard, because the bug breaks substitution and also
    impairs equality comparison: the body of ExpOption is not subject to
    substitution, hence vars are not alpha-renamed during transformation in the
    ExpOption body.
    This bug is hidden because variables are also not alpha-renamed during
    equality comparison - hence v1 => ExpOption(Some(v1)) seems the same as
    v2 => ExpOption(Some(v1)).

--- src/test/scala/ivm/tests/TransformTest.scala
+++ src/test/scala/ivm/tests/TransformTest.scala
@@ -4,7 +4,7 @@ package tests
 import org.scalatest.junit.{ShouldMatchersForJUnit, JUnitSuite}
 import org.junit.Test
 
-import expressiontree.Exp
+import expressiontree.{FuncExp, Exp}
 import expressiontree.Lifting._
 import optimization.Optimization
 import collection.mutable.ArrayBuffer
@@ -17,11 +17,22 @@ import collection.mutable.ArrayBuffer
 
 trait TransformTestHelper {
   this: JUnitSuite with ShouldMatchersForJUnit =>
-  def testRebuild[T](t: Exp[T]) = {
-    val tTransf = t transform identity
+  val forceRebuild: Exp[_] => Exp[_] =
+    e => e match {
+      case f: FuncExp[_, _] => FuncExp(f.f)
+      case _ => e
+    }
+
+  def testRebuildP[T](t: Exp[T], transf: Exp[_] => Exp[_]) = {
+    val tTransf = t transform transf
     tTransf should equal (t)
     assert(tTransf equals t)
     (t, tTransf)
+  }
+
+  def testRebuild[T](t: Exp[T]) = {
+    testRebuildP(t, forceRebuild)
+    testRebuildP(t, identity)
   }
 }
 
@@ -31,10 +42,13 @@ class TransformTest extends JUnitSuite with ShouldMatchersForJUnit with Transfor
   private val l: Exp[Traversable[Int]] = toExp(ArrayBuffer.range(1, collSize))
   private val l2: Exp[Traversable[Int]] = toExp(ArrayBuffer.range(1, collSize))
 
-  def testTransforms[T](t: Exp[T]) {
-    testRebuild(t)
-    testRebuild(Optimization.mergeFilters(t))
-    testRebuild(Optimization.optimize(t))
+  def testTransforms[T](t: => Exp[T]) {
+    val tEval = t
+    //Building the same expression twice should produce equal expression - which in practice was not always true.
+    tEval should equal (t)
+    testRebuild(tEval)
+    testRebuild(Optimization.mergeFilters(tEval))
+    testRebuild(Optimization.optimize(tEval))
   }
 
   @Test def testTransformIdentity {
@@ -47,5 +61,9 @@ class TransformTest extends JUnitSuite with ShouldMatchersForJUnit with Transfor
     testTransforms(for (k <- l; k2 <- l2 if k + k2 is k2 + k) yield k+k2)
     testTransforms(for (k <- l; k2 <- l2 if k + k2 is k2 + k) yield k*k2)
     testTransforms(for (c <- l if c * 3 is 7; if c + 8 is 19) yield c)
+  }
+
+  @Test def testTransformSome {
+    testTransforms(l map (Some(_)))
   }
 }
