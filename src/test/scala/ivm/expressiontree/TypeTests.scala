d50f7a819af0a27d4da4f9abc79a6538b0aee826
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-03-05 20:17:03 +0100

    Continue type query WIP

--- src/test/scala/ivm/expressiontree/TypeTests.scala
+++ src/test/scala/ivm/expressiontree/TypeTests.scala
@@ -3,8 +3,8 @@ package ivm.expressiontree
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import collection.{immutable, TraversableLike, mutable}
-import mutable.Builder
 import collection.generic.CanBuildFrom
+import mutable.{ArrayBuffer, Builder}
 
 /**
  * User: pgiarrusso
@@ -23,9 +23,13 @@ object NormalPriority extends LowPriority {
 import NormalPriority._
 
 class TypeTests extends FunSuite with ShouldMatchers {
-  private def queryTransitiveClosure[T](map: Map[T, mutable.Set[T]], v: T): mutable.Set[T] = {
-    //XXX buggy, we need the transitive closure!
-    map(v)
+  //returns all b such that a R* b, where R is the relation represented by map.
+  //XXX Note that this is inefficient since we use an immutable array as a mutable one.
+  /*private*/ def transitiveQuery[T](map: Map[T, mutable.Set[T]], a: T): mutable.Set[T] = {
+    for {
+      b <- map.get(a).getOrElse(mutable.Set())
+      c <- transitiveQuery(map, b) + b
+    } yield c
   }
 
   class TypeMapping[C[+X] <: TraversableLike[X, C[X]], D[+_], Base](val map: Map[ClassManifest[_], C[D[_]]], val subtypeRel: Map[Class[_], mutable.Set[Class[_]]], origColl: C[D[Base]])(implicit cm: ClassManifest[Base]) {
@@ -41,7 +45,7 @@ class TypeTests extends FunSuite with ShouldMatchers {
         case NoSub() =>
           val baseResult = map(tmf).asInstanceOf[C[D[Base /*T*/]]]
           val coll = cbf(baseResult)
-          for (t <- queryTransitiveClosure(subtypeRel, IfInstanceOf.getErasure(tmf)))
+          for (t <- transitiveQuery(subtypeRel, IfInstanceOf.getErasure(tmf)))
             //Jumping back and forth from Class[_] to ClassManifest[_] is extremely annoying.
             coll ++= map(ClassManifest.fromClass(t)).asInstanceOf[C[D[T]]]
           coll.result()
@@ -51,7 +55,7 @@ class TypeTests extends FunSuite with ShouldMatchers {
   }
 
   //This must be only used inside the implementation. Mutability fun!
-  private def groupBySel[A, K, B, Repr <: Traversable[A], That](coll: Repr with Traversable[A])(f: A => K, g: A => B)(implicit cbf: CanBuildFrom[Repr, B, That]): immutable.Map[K, That] = {
+  /*private*/ def groupBySel[A, K, B, Repr <: Traversable[A], That](coll: Repr with Traversable[A])(f: A => K, g: A => B)(implicit cbf: CanBuildFrom[Repr, B, That]): immutable.Map[K, That] = {
     val m = mutable.Map.empty[K, Builder[A, Traversable[A]]]
     for (elem <- coll) {
       val key = f(elem)
@@ -62,11 +66,11 @@ class TypeTests extends FunSuite with ShouldMatchers {
     val b = immutable.Map.newBuilder[K, That]
     for ((k, v) <- m)
       b += ((k, v.mapResult(c => (cbf(coll) ++= (c map g)).result()).result()))
-    
+
     b.result
   }
-  
-  private def groupBySelAndForeach[A, K, B, Repr <: TraversableLike[A, Repr], That](coll: Repr with TraversableLike[A, Repr])(f: A => K, g: A => B)(h: K => Unit)(implicit cbf: CanBuildFrom[Repr, B, That]): immutable.Map[K, That] = {
+
+  /*private*/ def groupBySelAndForeach[A, K, B, Repr <: TraversableLike[A, Repr], That](coll: Repr with TraversableLike[A, Repr])(f: A => K, g: A => B)(h: K => Unit)(implicit cbf: CanBuildFrom[Repr, B, That]): immutable.Map[K, That] = {
     val m = mutable.Map.empty[K, Builder[A, Traversable[A]]]
     for (elem <- coll) {
       val key = f(elem)
@@ -78,13 +82,40 @@ class TypeTests extends FunSuite with ShouldMatchers {
     val b = immutable.Map.newBuilder[K, That]
     for ((k, v) <- m)
       b += ((k, v.mapResult(c => (cbf(coll) ++= (c map g)).result()).result()))
-    
+
     b.result
   }
 
   //Class.getSuperclass can return null, filter that out. Now make sure that Object is always included? Add testcases for primitive types?
-  private def superTypes(c: Class[_]): Seq[Class[_]] = c.getInterfaces ++ Option(c.getSuperclass)
+  /*private*/ def superClass(c: Class[_]): Option[Class[_]] = Option(c.getSuperclass) orElse (if (c == classOf[Any]) None else Some(classOf[Any]))
+  //getInterfaces returns all implemented interfaces :-), while getSuperclass does not.
+  /*private*/ def superTypes(c: Class[_]): Seq[Class[_]] = c.getInterfaces ++ superClass(c)
 
+  //XXX Careful: T must be passed explicitly! Otherwise it will be deduced to be Nothing
+  def computeSubTypeRel[T: ClassManifest](seenTypes: mutable.Set[ClassManifest[_]]) = {
+    val subtypeRel = mutable.Set.empty[(Class[_], Class[_])]
+    var toScan: List[Class[_]] = Nil
+    for {
+      t <- seenTypes
+      if t != ClassManifest.Null
+      t_ = IfInstanceOf.getErasure(t)
+      s <- superTypes(t_)
+      if t != classManifest[T]
+    } {
+      toScan = s :: toScan
+      subtypeRel += (s -> t_) //Map s to its subtypes.
+    }
+    while (toScan.nonEmpty) {
+      val t = toScan.head
+      toScan = toScan.tail
+      //superClass is special.
+      for (s <- superClass(t)) {
+        toScan = s :: toScan
+        subtypeRel += (s -> t)
+      }
+    }
+    groupBySel(subtypeRel)(_._1, _._2)
+  }
   case class GroupByType[T: ClassManifest, C[+X] <: TraversableLike[X, C[X]], D[+_]](base: Exp[C[D[T]]], f: Exp[D[T] => T]) extends Arity2OpExp[C[D[T]], D[T] => T, TypeMapping[C, D, T],
     GroupByType[T, C, D]](base, f) {
     override def interpret() = {
@@ -104,17 +135,20 @@ class TypeTests extends FunSuite with ShouldMatchers {
 
       //val map = coll groupBy getType
       val map = groupBySelAndForeach(coll)(getType, identity)(seenTypes += _)
-      val subtypeRel = mutable.Set.empty[(Class[_], Class[_])]
-      for {
-        t <- seenTypes
-        if t != classManifest[T] && t != ClassManifest.Null
-        t_ = IfInstanceOf.getErasure(t)
-        s <- superTypes(t_)
-      }
-        subtypeRel += (s -> t_) //Map s to its subtypes.
+      val subtypeRel = computeSubTypeRel[T](seenTypes)
 
-      new TypeMapping[C, D, T](map.asInstanceOf[Map[ClassManifest[_], C[D[_]]]], groupBySel(subtypeRel)(_._1, _._2), coll)
+      new TypeMapping[C, D, T](map.asInstanceOf[Map[ClassManifest[_], C[D[_]]]], subtypeRel, coll)
     }
     override def copy(base: Exp[C[D[T]]], f: Exp[D[T]=>T]) = GroupByType[T, C, D](base, f)
+  }
+  val seenTypesEx: mutable.Set[ClassManifest[_]] = mutable.Set(classManifest[Int], classManifest[Null], classManifest[AnyRef], classManifest[String])
+
+  test("foo") {
+    val rel = computeSubTypeRel[Void](seenTypesEx)
+    val res = transitiveQuery(rel, classOf[Any])
+    assert(res(classOf[Number]))
+    //type C = Class[_]
+    //val res: Traversable[C] = transitiveQuery(rel, classOf[Any])
+    //res should contain (classOf[Number].asInstanceOf[C])
   }
 }
