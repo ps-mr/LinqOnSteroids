b968d32f53645ff36c90884bd8f9aac88e27bf60
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-01 12:15:46 +0200

    Reindent

--- src/main/scala/ivm/expressiontree/Const.scala
+++ src/main/scala/ivm/expressiontree/Const.scala
@@ -11,17 +11,17 @@ case class Const[T](x: T) extends Arity0Exp[T] {
   */
   override def toString = {
     val s =
-    x match {
-      //Printing all elements and then cutting the output is horribly expensive for huge collections, so try to avoid it.
-      //Of course, this does not work when x is not a collection but e.g. contains one, or when for any reason toString()
-      //takes a lot of time for any reason. Still, better than nothing.
-      case coll: Traversable[_] =>
-        coll.take(3).toString() + (if (coll.size > 3) "..." else "")
-      case s: String =>
-        """"%s"""" format s
-      case _ =>
-        x.toString
-    }
+      x match {
+        //Printing all elements and then cutting the output is horribly expensive for huge collections, so try to avoid it.
+        //Of course, this does not work when x is not a collection but e.g. contains one, or when for any reason toString()
+        //takes a lot of time for any reason. Still, better than nothing.
+        case coll: Traversable[_] =>
+          coll.take(3).toString() + (if (coll.size > 3) "..." else "")
+        case s: String =>
+          """"%s"""" format s
+        case _ =>
+          x.toString
+      }
     val shortened =
       if (s.length() > 100) {
         val begin = s take 100
