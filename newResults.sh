935baaddb159aa105c967867367758d92e2bcf93
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-04-03 17:58:32 +0200

    Test scripts: compile before benchmarking

--- newResults.sh
+++ newResults.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 #git pull
 nIters=3
+sbt test:compile
 for ((i=0; i < $nIters; i++)); do
   out=testLog-`timestamp`
   { echo -n "Git version: "; git describe --always; time sbt 'test-only performancetests.*'; } 2>&1|tee $out
--- profileLos.sh
+++ profileLos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 #git pull
 nIters=3
+sbt test:compile
 for ((i=0; i < $nIters; i++)); do
   out=profileLog-`timestamp`
   { echo -n "Git version: "; git describe --always; time sbt "test:run lib/scalatest-1.6.1.jar"; } 2>&1|tee $out
