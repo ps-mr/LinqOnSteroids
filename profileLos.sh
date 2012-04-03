4177a6ef9b9992f3d3012ebe93d2ce358c888aa7
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-04-03 18:00:39 +0200

    Separate CSV output

--- profileLos.sh
+++ profileLos.sh
@@ -6,3 +6,5 @@ for ((i=0; i < $nIters; i++)); do
   out=profileLog-`timestamp`
   { echo -n "Git version: "; git describe --always; time sbt "test:run lib/scalatest-1.6.1.jar"; } 2>&1|tee $out
 done
+cat LOSTestLog.csv >> LOSTestLog-ScalaTest.csv
+rm -f LOSTestLog.csv
