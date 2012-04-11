fe47c1fe554f724acf688877dbacc12aeb1a681d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-11 16:43:55 +0200

    Modularize evaluation queries: generate wrappers for new case class

--- sampleapp/sampleLibrary.scala
+++ sampleapp/sampleLibrary.scala
@@ -2,4 +2,16 @@ package sampleapp
 case class Author(firstName: String, lastName: String)
 case class Book(title: String, publisher: String, authors: Seq[Author])
 case class Result(title: String, authorName: String, coauthors: Int)
+
+import de.tud.cs.st.bat.resolved._
+
+/*
+        classFile ← classFiles.asSmart
+        method ← classFile.methods
+        body ← method.body
+          body ← method.body
+          instruction ← body.instructions
+
+ */
+case class MethodRecord(classFile: ClassFile, method: Method, body: Code)
 // vim: set ts=4 sw=4 et:
