688470dc6edbd5321c01c33b8a6d247c053bbe55
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-13 20:42:04 +0200

    Move FindBugsAnalysis into src/main to use the execution script
    This way, we don't keep sbt in memory while running the evaluation.

--- src/test/scala/ivm/tests/Tutorial.scala
+++ src/test/scala/ivm/tests/Tutorial.scala
@@ -8,33 +8,6 @@ import Lifting._
 import optimization.Optimization
 import collection.{mutable, TraversableView}
 
-trait TestUtil {
-  def showExpNoVal[T](t: Exp[T], message: String = "") {
-    print("\nQuery name: %s\n *\tstructure:\n\t%s\n" format (message, t))
-  }
-
-  def showExp[T](t: Exp[T], message: String = "") {
-    showExpNoVal(t, message)
-    print(" *\tvalue:\n\t%s\n\n" format t.interpret())
-  }
-
-  def show(name: String, v: Any) {
-    print(name + ": ")
-    println(v)
-  }
-
-  /*
-  def showInterp(name: String, v: Exp[_]) {
-    show(name, v)
-    show(name + ".interpret()", v.interpret())
-  }
-  */
-
-  def showInterp(name: String, v: Exp[_]) {
-    showExp(v, name)
-  }
-}
-
 /**
  * User: pgiarrusso
  * Date: 18/11/2011
