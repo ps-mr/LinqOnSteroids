cd57f828b89687e790e9bad80755bd49aa6a9b00
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-04-14 00:45:46 +0200

    Adapt profileLosJDK-startup.sh to target/start

---
+++ profileLosJDK-startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+#git pull
+#for ((i=0; i < 40; i++)); do
+#  ./profileFB.sh /usr/lib/jvm/jre-1.6.0-openjdk.x86_64/lib/rt.jar > $out 2>&1
+#  tail -1 $out >> FB-JDK.csv
+#done
+
+out=profileLogJDK-startup-`timestamp`
+outCSV=startup-JDK.csv
+nIters=40
+
+gitV=$(git describe --always --dirty)
+echo "Git version: $gitV" > $out
+> $outCSV
+
+sbt test:compile
+for ((i=0; i < $nIters; i++)); do
+  {
+    $(which time) -f "$gitV;%e;%U;%S;%P" sbt "test:run /usr/lib/jvm/jre-1.6.0-openjdk.x86_64/lib/rt.jar"
+  } >>$out 2>&1
+  tail -1 $out >> $outCSV
+done
+rm -f LOSTestLog.csv
