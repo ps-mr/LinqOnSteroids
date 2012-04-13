789eaa7763fe7726eba45208d9a673e268cfe93f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-13 21:12:09 +0200

    Update evaluation scripts after moving FindBugsAnalysis

--- profileLos.sh
+++ profileLos.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 #git pull
 nIters=3
-sbt test:compile
+sbt compile stage
 for ((i=0; i < $nIters; i++)); do
   out=profileLog-`timestamp`
-  { echo -n "Git version: "; git describe --always --dirty; time sbt "test:run lib/scalatest-1.6.1.jar"; } 2>&1|tee $out
+  { echo -n "Git version: "; git describe --always --dirty; time ./target/start src/test/resources/scalatest-1.6.1.jar; } 2>&1|tee $out
 done
 cat LOSTestLog.csv >> LOSTestLog-ScalaTest.csv
 rm -f LOSTestLog.csv
