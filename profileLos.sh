62ded587f036f1b4ca4e84a455c67aaf8687b8a0
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-04-14 03:11:40 +0200

    target/start: give the needed memory

--- profileLos.sh
+++ profileLos.sh
@@ -2,6 +2,7 @@
 #git pull
 nIters=3
 sbt compile stage
+. javaSettings.inc
 for ((i=0; i < $nIters; i++)); do
   out=profileLog-`timestamp`
   { echo -n "Git version: "; git describe --always --dirty; time ./target/start src/test/resources/scalatest-1.6.1.jar; } 2>&1|tee $out
