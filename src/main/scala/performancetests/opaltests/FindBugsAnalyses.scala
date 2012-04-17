d79f4683863a633b78fa26d4a9f9cde5c7a9c50f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-17 17:32:02 +0200

    FindBugsAnalysis: Add (as comments) some older version of an analysis

--- src/main/scala/performancetests/opaltests/FindBugsAnalyses.scala
+++ src/main/scala/performancetests/opaltests/FindBugsAnalyses.scala
@@ -408,8 +408,14 @@ class FindBugsAnalyses(zipFiles: Seq[String]) extends FunSuite with BeforeAndAft
         body ← method.body
         instruction ← body.instructions.typeCase(when[INVOKESTATIC](
           instruction => instruction.name ==# "runFinalizersOnExit" && (instruction.declaringClass ==# ObjectType("java/lang/System") || instruction.declaringClass ==# ObjectType("java/lang/Runtime")), identity))
+//        if instruction.declaringClass ==# ObjectType("java/lang/System") || instruction.declaringClass ==# ObjectType("java/lang/Runtime")
+//        desc <- Let(instruction.methodDescriptor)
+//        recv <- instruction.declaringClass.ifInstanceOf[ObjectType]
+//        if (recv.className ==# "java/lang/System" || recv.className ==# "java/lang/Runtime") &&
+//          desc ==# MethodDescriptor(Seq(BooleanType), VoidType)
         //the method descriptor is not checked in FindBugs - there's no need
-        // && instruction.methodDescriptor == MethodDescriptor(Seq(BooleanType), VoidType)
+        // && instruction.methodDescriptor ==# MethodDescriptor(Seq(BooleanType), VoidType)
+        //if desc ==# MethodDescriptor(Seq(BooleanType), VoidType)
       } yield (classFile, method, instruction)
     }
   }
