ef7428d0d371fe1257851cfb88674c9281866fa6
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-20 02:44:03 +0200

    sampleapp |-> dbschema

--- sampleapp/sampleLibrary.scala
+++ sampleapp/sampleLibrary.scala
@@ -1,4 +1,4 @@
-package sampleapp
+package dbschema
 case class Author(firstName: String, lastName: String)
 case class Book(title: String, publisher: String, authors: Seq[Author])
 case class Result(title: String, authorName: String, coauthors: Int)
