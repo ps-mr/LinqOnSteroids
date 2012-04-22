dd50cfb61fcb95eaf9beff9214c308bbdb4b3096
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-04-22 23:27:09 +0200

    release.sh fix

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
-mkdir tarballs
+mkdir -p website/tarballs
 for release in v0.1 v0.2; do
-  for extension in gz bz2 xz; do
-    git archive --worktree-attributes -o website/tarballs/squopt-$release.tar.xz --prefix=squopt/ $release
+  for ext in gz bz2 xz; do
+    git archive --worktree-attributes -o website/tarballs/squopt-$release.tar.$ext --prefix=squopt/ $release
   done
 done
