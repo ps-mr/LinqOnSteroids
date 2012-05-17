f445a28f6dacbf79632d3c7ec78b5c428edbd6aa
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-05-17 19:36:34 +0200

    Import most benchmarking code with new methodology
    Still to complete

--- src/main/scala/performancetests/Benchmarking.scala
+++ src/main/scala/performancetests/Benchmarking.scala
@@ -4,6 +4,7 @@ import ivm.GitVersion
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.io.{PrintWriter, FileWriter, BufferedWriter}
+import collection.mutable.ArrayBuffer
 
 trait Benchmarking {
   import Benchmarking._
@@ -11,6 +12,11 @@ trait Benchmarking {
   val execLoops = 1
   val warmUpLoops = 100
   val sampleLoops = 50
+
+  //val maxCov = 0.10
+  val maxCov = 0.02
+  val myMethodology = false
+  val printAllData = false
 
   //Import and re-export to inheritors.
   def debugBench = Benchmarking.debugBench
@@ -40,6 +46,7 @@ trait Benchmarking {
   def benchMark[T](name: String, silent: Boolean = false, execLoops: Int = effectiveExecLoops, warmUpLoops: Int = effectiveWarmUpLoops, sampleLoops: Int = effectiveSampleLoops, verbose: Boolean = true, hasConsoleOutput: Boolean = false)
                (toBench: => T): T =
     benchMarkInternal(name, silent, execLoops, warmUpLoops, sampleLoops, verbose, hasConsoleOutput)(toBench)._1
+
   /**
    * @param warmUpLoops: Warm up the VM - should be more
    * @param sampleLoops Iterations to measure variance.
@@ -61,32 +68,45 @@ trait Benchmarking {
     var ret: T = null.asInstanceOf[T]
     newLine() //Make space at the beginning
     println("Benchmarking params: execLoops: %d, warmUpLoops: %d, sampleLoops: %d" format (execLoops, warmUpLoops, sampleLoops))
-    if (!hasConsoleOutput)
-      println(">>> Name = %s, starting warmup..." format name)
+    if (!hasConsoleOutput) {
+      print(">>> Name = %s" format name)
+      if (myMethodology)
+        print(", starting warmup...")
+      println("")
+    }
     //Use Console.err instead of println and flush here.
     //Console.flush()
 
-    for (i <- 1 to warmUpLoops)
-      toBench
+    if (myMethodology)
+      for (i <- 1 to warmUpLoops)
+        toBench
     if (!debugBench)
       System.gc()
 
     if (hasConsoleOutput)
       newLine()
-    else
-      print(" ending warmup, starting benchmarking...")
+    else {
+      if (myMethodology)
+        print(" ending warmup,")
+      print(" starting benchmarking...")
+    }
 
-    val stats = new VarianceCalc
+    //val stats = if (myMethodology) new VarianceCalcMyMethodology else new VarianceCalc(sampleLoops)
+    val stats = new VarianceCalc(sampleLoops)
+    val values = ArrayBuffer[Long]()
     for (i <- 1 to sampleLoops) {
       val before = System.nanoTime()
       for (j <- 1 to execLoops)
         ret = toBench
-      stats.update((System.nanoTime() - before) / execLoops)
+      val timeD = (System.nanoTime() - before) / execLoops
+      //print("%d;" format timeD)
+      stats.update(timeD)
+      values += timeD
       if (!debugBench)
         System.gc()
     }
     if (!hasConsoleOutput)
-      print(" ended benchmarking, name = %s, time = " format name)
+      print(" ended benchmarking, name = %s, needed iterations = %d, time = " format (name, stats.iterations))
     val avgMs = stats.avg / math.pow(10,6)
     val devStdMs = math.sqrt(stats.variance) / math.pow(10,6)
     //The error of the measured average as an estimator of the average of the underlying random variable
@@ -95,13 +115,23 @@ trait Benchmarking {
     if (verbose) {
       if (hasConsoleOutput)
         print(">>> Name = %s, time = " format name)
-      println("(%.3f +- %.3f (stdErr = %.3f)) ms" format (avgMs, devStdMs, stdErrMs))
+      println("(%.3f +- %.3f (stdErr = %.3f)) ms; relative std.dev. %.3f, std.err. %.3f" format (avgMs, devStdMs, stdErrMs, devStdMs / avgMs, stdErrMs / avgMs))
     }
     //Format output for Jenkins' Measurement Plot plugin - https://wiki.jenkins-ci.org/display/JENKINS/Measurement+Plots+Plugin
-    println(<measurement><name>{name}</name><value>{avgMs}</value></measurement>)
-    //Format output for R
+    println(<measurement><name>{name}</name><value>{avgMs}</value></measurement>) //Remove, that Jenkins plugin does not work.
+
+    //Current log.
     logWriter.println("%s;%s;%s;%f;%f;%f" format (GitVersion.version, testDate, name.replace(';', '_'), avgMs, devStdMs, stdErrMs))
     logWriter.flush()
+    //Detailed log.
+    //Format output for R
+    if (printAllData)
+      rawDataLogWriter.println("%s;%s;%s" format (GitVersion.version, testDate, name.replace(';', '_')))
+    for (v <- if (printAllData) values else stats.buf) {
+      rawDataLogWriter.println("%s;%s;%s" format (GitVersion.version, testDate, v))
+      //Move name back in
+    }
+    rawDataLogWriter.flush()
 
     (ret, avgMs)
   }
@@ -126,23 +156,67 @@ trait Benchmarking {
 object Benchmarking {
   val debugBench = false
 
-  class VarianceCalc {
+  trait IVarianceCalc {
     var sum: Double = 0
     var sumSq: Double = 0
+    //Total iterations whose results are kept (used to compute the avg. and std.dev.)
+    def count: Int
+    //Total iterations executed (counts the calls to update)
+    def iterations: Int
+
+    def avg = sum / count
+
+    def variance = {
+      val t = avg
+      sumSq / count - t * t
+    }
+
+    def update(sample: Double)
+    def cov =
+      math.sqrt(variance) / avg
+  }
+
+  class VarianceCalc(samples: Int) extends IVarianceCalc {
+    val buf = ArrayBuffer.fill(samples)(0.0)
+    var idx = 0
+    var iterations = 0
+
+    //var count = 0
+    def count = samples
+
+    def update(sample: Double) {
+      iterations += 1
+
+      //Safe to do this
+      sum -= buf(idx)
+      sumSq -= buf(idx) * buf(idx)
+
+      buf(idx) = sample
+
+      sum += sample
+      sumSq += sample * sample
+
+      idx = (idx + 1) % samples
+    }
+    override def cov =
+      if (iterations < samples) 1 else super.cov
+  }
+
+  class VarianceCalcMyMethodology extends IVarianceCalc {
     var count = 0
+    def iterations = count
+
     def update(sample: Double) {
       count += 1
       sum += sample
       sumSq += sample * sample
     }
-    def avg = sum / count
-    def variance = {
-      val t = avg
-      sumSq / count - t * t
-    }
   }
+
   private val testDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance.getTime)
   private val logPath = "LOSTestLog.csv"
   private val logWriter = new PrintWriter(new BufferedWriter(new FileWriter(logPath, true)))
+  private val rawDataLogPath = "LOSTestLog.csv"
+  private val rawDataLogWriter = new PrintWriter(new BufferedWriter(new FileWriter(logPath, true)))
   private var usedNames = Set[String]()
 }
