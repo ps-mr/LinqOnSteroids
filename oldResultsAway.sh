e6e44e004c31bae5636ffb69064e84c389e9e1fb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-05-18 22:28:03 +0200

    Fix race condition - take timestamp only once per execution
    It's desirable that files renamed together have the same timestamp.

--- oldResultsAway.sh
+++ oldResultsAway.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
+now=`timestamp`
 for i in ScalaTest JDK; do
   for j in '' -raw; do
-    mv LOSTestLog-$i$j.csv away/LOSTestLog-$i$j-`timestamp`.csv
+    mv LOSTestLog-$i$j.csv away/LOSTestLog-$i$j-$now.csv
   done
 done
