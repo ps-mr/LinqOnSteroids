3033d56a1418e58ea84fb569b9529eb5f62867b5
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-06-30 23:05:04 +0200

    Play with unnesting and renesting exists
    Commented out code is still in.

--- src/main/scala/ivm/optimization/Unnesting.scala
+++ src/main/scala/ivm/optimization/Unnesting.scala
@@ -11,6 +11,13 @@ import OptimizationUtil._
  */
 
 trait Unnesting {
+  val existsUnnester2: Exp[_] => Exp[_] = {
+    case FlatMap(Filter(c0, f@FuncExpBody(Not(IsEmpty(Filter(c, p))))), fmFun) =>
+      val restQuery = Fun.makefun(fmFun(f.x), p.x)
+      stripView(c0) flatMap Fun.makefun((stripView(c) filter p flatMap restQuery)(collection.breakOut): Exp[Traversable[Any]], f.x)
+    case e => e
+  }
+
   val existsUnnester: Exp[_] => Exp[_] = {
     //Rule N9 page 474 in Optimizing Object Queries Using an Effective Calculus, Fegaras and Maier, 2000:
     //c0 withFilter (x0 => c exists (x => p(x))) flatMap (x1 => B [not free in x]) |=> c0 flatMap (x0 => c withFilter (x => p(x)) flatMap restQuery)
@@ -19,7 +26,13 @@ trait Unnesting {
     //the result of flatMap to a set.
     case FlatMap(Filter(c0, f@FuncExpBody(Not(IsEmpty(Filter(c, p))))), fmFun) =>
       //Since x0 = f.x, and fmFun = x1 => B, then [x1 |-> x0]B is (x1 => B) x0, that is fmFun(f.x), and restQuery = x => [x1 |-> x0]B =
-      val restQuery = Fun.makefun(fmFun(f.x), p.x)
+      val restQuery = Fun.makefun(fmFun(f.x), p.x) //Hmm, this is a bit of a hack. The scope of p.x does not extend over f.x.
+      //Hence, that code might as well use a fresh variable. In fact, that's a function which ignores its argument -
+      //const fmFun(f.x).
+      Util.checkSameTypeAndRet {
+        //What about this simpler variant?
+        stripView(c0) flatMap Fun.makefun((stripView(c) filter p flatMap restQuery)(collection.breakOut): Exp[Traversable[Any]], f.x)
+      } {
       //toSet remove any duplicates to preserve semantics; in the expression c exists p, p might be true for more elements of c. When unnesting,
       //we produce c withFilter p, and for each element in the result we apply x1 => B. Instead, with toSet we unify the results after applying
       //filtering. That's unsatisfactory though; we could instead of using toSet on the result, use breakout as CanBuildFrom instance on the last flatMap.
@@ -27,7 +40,42 @@ trait Unnesting {
       //Is duplicate elimination after applying restQuery valid? I guess not: the flatMapped function might just produce an element multiple times.
       //So we produce instead a Set of booleans to do duplicate elimination and then filter with identity!
       //XXX untested.
-      stripView(c0) flatMap Fun.makefun((((stripView(c) map p)(collection.breakOut): Exp[Set[Boolean]]) filter identity flatMap restQuery)(collection.breakOut): Exp[Traversable[Any]], f.x)
+        stripView(c0) flatMap Fun.makefun((((stripView(c) map p)(collection.breakOut): Exp[Set[Boolean]]) filter identity flatMap restQuery)(collection.breakOut): Exp[Traversable[Any]], f.x)
+      }
+    case e => e
+  }
+
+  /*private def varNotFreeInResult[T](e: Exp[T], v: Var): Boolean = {
+    def varNotFreeInResultRec(e: Exp[T]): Boolean =  e match {
+      case Filter(base, f) => varNotFreeInResultRec(base)
+      case Binding(base, f) => varNotFreeInResultRec(base) && varNotFreeInResultRec(f.body)
+      case _ => !e.isOrContains(v)
+    }
+    varNotFreeInResultRec(e)
+  }*/
+
+  private def rebuildExists[T, U, V](coll: Exp[Traversable[T]],
+                                  coll2: Exp[Traversable[U]],
+                                  p: Fun[U, Boolean],
+                                  f: Fun[T, Traversable[V]],
+                                  f2: Fun[U, Traversable[V]]): Exp[Traversable[V]] = {
+    //coll filter Fun.makefun(!(coll2 filter p).isEmpty, f.x) flatMap f2
+    //coll filter Fun.makefun(coll2 exists p, f.x) flatMap f2
+    coll filter Fun.makefun(coll2 exists p, f.x) flatMap Fun.makefun(f2.body, f.x)
+  }
+
+  //This is useful, because exists has a more efficient execution strategy than the nested iteration: exists can stop
+  //as soon as the existence witness is found.
+  val existsRenester: Exp[_] => Exp[_] = {
+    case e @ FlatMap(coll, f@FuncExpBody(FlatMap(Filter(coll2, p), f2))) if !f2.body.isOrContains(f2.x) =>
+      rebuildExists(coll, coll2, p, f, f2)
+    //case e @ FlatMap(coll, f) if varNotFreeInResult(f.body, f.x) =>
+      //e
+    //case FlatMap(xs, fx @ FuncExpBody()) =>
+    /*case FlatMap(coll, f@FuncExpBody(FlatMap(coll2, f2))) if !f2.isOrContains(f.x) =>
+      coll filter Fun.makefun(!coll2.isEmpty, f.x)
+        //flatMap
+      coll*/
     case e => e
   }
 
