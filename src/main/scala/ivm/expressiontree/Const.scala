ps-mr/LinqOnSteroids

commit 21ca401edba6c6b5dde7f18e9dd5783d906b7da6

Add type information to Const to allow cross-stage persistence
In the process, I needed to disable polymorphic constant folding, since
I cannot polymorphically generate manifests for the produced Const nodes.

src/main/scala/ivm/expressiontree/Const.scala
@@ -1,8 +1,26 @@
 package ivm.expressiontree
 
-case class Const[T](x: T) extends Arity0Exp[T] {
+case class Const[T](x: T)(implicit val classManifest: ClassManifest[T]) extends Arity0Exp[T] {
   override def interpret() = x
-  override def toCode = x.toString //XXX: This sucks in a _lot_ of ways. Barely good for testing.
+  private def show(toEval: Boolean = false): String = {
+    val str = x match { //XXX: Doesn't work for most values. Barely good for testing.
+      case s: String =>
+        """"%s"""" format s
+      case x: Number =>
+        if (toEval)
+          "(" + x.toString + ")"
+        else
+          x.toString
+      case _ =>
+        String.valueOf(x)
+    }
+    if (toEval)
+      "(%s: %s)" format (str, classManifest)
+    else
+      str
+  }
+
+  override def toCode = show(toEval = true)
   override def toString = {
     val s =
       x match {
@@ -11,10 +29,8 @@ case class Const[T](x: T) extends Arity0Exp[T] {
         //takes a lot of time for any reason. Still, better than nothing.
         case coll: Traversable[_] =>
           coll.take(3).toString() + (if (coll.size > 3) "..." else "")
-        case s: String =>
-          """"%s"""" format s
         case _ =>
-          x.toString
+          show()
       }
     val shortened =
       if (s.length() > 100) {
@@ -29,7 +45,7 @@ case class Const[T](x: T) extends Arity0Exp[T] {
 }
 
 //This class has much faster hashing and comparison; we use it when we can semantically afford it, that is within asSmart.
-class ConstByIdentity[T](content: T) extends Const(content) {
+class ConstByIdentity[T](content: T, wit: ClassManifest[T]) extends Const(content)(wit) {
   override def canEqual(o: Any) = o.isInstanceOf[ConstByIdentity[_]]
 
   override def equals(other: Any) = other match {
@@ -39,4 +55,8 @@ class ConstByIdentity[T](content: T) extends Const(content) {
 
   override def hashCode() = System.identityHashCode(x.asInstanceOf[AnyRef])
   override def productPrefix = "ConstByIdentity"
+}
+
+object ConstByIdentity {
+  def apply[T: ClassManifest](content: T) = new ConstByIdentity[T](content, classManifest[T])
 }

src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -31,7 +31,7 @@ object SubquerySharing {
   }
 }
 
-class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
+class SubquerySharing(val subqueries: Map[Exp[_], (Any, ClassManifest[Any])]) {
   import SubquerySharing._
 
   // To perform index lookup, we need to put both indexes and original queries in normal form-and it should be the
@@ -52,7 +52,7 @@ class SubquerySharing(val subqueries: Map[Exp[_], Any]) {
 
   val directsubqueryShare: Exp[_] => Exp[_] =
     e => subqueries.get(normalizeBeforeLookup(e)) match {
-      case Some(t) => new ConstByIdentity(t) //this gives the right behavior but is a hack
+      case Some((t, tMan)) => ConstByIdentity(t)(tMan) //this gives the right behavior but is a hack
       case None => e
     }
 

src/main/scala/ivm/expressiontree/simpleOpenEncoding.scala
@@ -33,7 +33,7 @@ trait LiftingConvs extends ConversionDisabler {
   //but it does not work (bug https://issues.scala-lang.org/browse/SI-3346).
   //implicit def pure[T, U <% T](t: U): Exp[T] = Const(t)
   //So let's keep it simple.
-  implicit def pure[T](t: T): Exp[T] = Const(t)
+  implicit def pure[T: ClassManifest](t: T): Exp[T] = Const(t)
 
   //Used to force insertion of the appropriate implicit conversion - unlike ascriptions, one needn't write out the type
   //parameter of Exp here.
@@ -198,14 +198,14 @@ trait NumOps {
   //Doesn't work because of https://issues.scala-lang.org/browse/SI-3346 - expToNumOps is the same as mkOps in their example.
   //Solution 2:
   implicit def expToNumOps[T: Numeric](t: Exp[T]) = new NumericOps(t)
-  implicit def toNumOps[T: Numeric](t: T) = expToNumOps(t)
+  implicit def toNumOps[T: Numeric: ClassManifest](t: T) = expToNumOps(t)
 
   //Same for the others:
   implicit def expToFractionalOps[T: Fractional](t: Exp[T]) = new FractionalOps(t)
-  implicit def toFractionalOps[T: Fractional](t: T) = expToFractionalOps(t)
+  implicit def toFractionalOps[T: Fractional: ClassManifest](t: T) = expToFractionalOps(t)
 
   implicit def expToIntegralOps[T: Integral](t: Exp[T]) = new IntegralOps(t)
-  implicit def toIntegralOps[T: Integral](t: T) = expToIntegralOps(t)
+  implicit def toIntegralOps[T: Integral: ClassManifest](t: T) = expToIntegralOps(t)
 }
 
 trait BaseTypesOps {
@@ -240,7 +240,7 @@ trait BaseTypesOps {
    * just having a polymorphic lift conversion. Other solutions are possible here but don't remove this ambiguity that
    * affects client code then.
    */
-  implicit def toOrderingOps[T: Ordering](t: T) = expToOrderingOps(t)
+  implicit def toOrderingOps[T: Ordering: ClassManifest](t: T) = expToOrderingOps(t)
   implicit def toStringOps(t: String) = expToStringOps(t)
   implicit def toBooleanOps(t: Boolean) = expToBooleanOps(t)
 }

src/main/scala/ivm/expressiontree/traversableOps.scala
@@ -123,8 +123,8 @@ trait TraversableOps {
 
   //Compare collection nodes by identity.
   //Saves costs when comparing collections, which happens during optimization.
-  implicit def pureColl[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](v: Repr with Traversable[T]): Exp[Repr] =
-    new ConstByIdentity(v)
+  implicit def pureColl[T, Repr <: Traversable[T] with TraversableLike[T, Repr]: ClassManifest](v: Repr with Traversable[T]): Exp[Repr] =
+    ConstByIdentity(v)
 
   //This version does not work, due to https://issues.scala-lang.org/browse/SI-5298:
   //implicit def expToTraversableLikeOps[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](v: Exp[Repr])
@@ -141,7 +141,7 @@ trait TraversableOps {
    * irrespective of the order in which the bounds are considered. */
   implicit def expToTraversableLikeOps[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](v: Exp[Repr with TraversableLike[T, Repr]]) =
     new TraversableLikeOps[T, Repr](v)
-  implicit def toTraversableLikeOps[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](v: Repr with TraversableLike[T, Repr]) =
+  implicit def toTraversableLikeOps[T, Repr <: Traversable[T] with TraversableLike[T, Repr]: ClassManifest](v: Repr with TraversableLike[T, Repr]) =
     expToTraversableLikeOps(v)
 
   //Ranges are cheap to compare for equality; hence we can easily use pure, not pureColl, for them.
@@ -156,12 +156,12 @@ trait TraversableOps {
     extends TraversableViewLikeOps[T, Repr, TraversableView[T, Repr]](tp)
 
   implicit def expToTravViewExp[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](t: Exp[TraversableView[T, Repr]]): TraversableViewOps[T, Repr] = new TraversableViewOps(t)
-  implicit def tToTravViewExp[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](t: TraversableView[T, Repr]): TraversableViewOps[T, Repr] = expToTravViewExp(t)
+  implicit def tToTravViewExp[T, Repr <: Traversable[T] with TraversableLike[T, Repr]: ClassManifest](t: TraversableView[T, Repr]): TraversableViewOps[T, Repr] = expToTravViewExp(t)
 
   implicit def expToTravViewExp2[T, C[X] <: Traversable[X] with TraversableLike[X, C[X]]](t: Exp[TraversableView[T, C[_]]]): TraversableViewOps[T, C[T]] = expToTravViewExp(
     t.asInstanceOf[Exp[TraversableView[T, C[T]]]])
   //XXX
-  implicit def tToTravViewExp2[T, C[X] <: Traversable[X] with TraversableLike[X, C[X]]](t: TraversableView[T, C[_]]): TraversableViewOps[T, C[T]] = expToTravViewExp2(t)
+  implicit def tToTravViewExp2[T, C[X] <: Traversable[X] with TraversableLike[X, C[X]]](t: TraversableView[T, C[_]])(implicit evidence: ClassManifest[C[_]]): TraversableViewOps[T, C[T]] = expToTravViewExp2(t)
 
   implicit def TraversableExp2ExpTraversable[T](e: Traversable[Exp[T]]): Exp[Traversable[T]] = ExpSeq(e)
 }
@@ -233,7 +233,7 @@ trait CollectionMapOps {
   }
   implicit def expToMapLikeOps[K, V, Repr <: Map[K, V] with MapLike[K, V, Repr]](v: Exp[Repr with Map[K, V]]) =
     new MapLikeOps[K, V, Repr] {val t = v}
-  implicit def toMapLikeOps[K, V, Repr <: Map[K, V] with MapLike[K, V, Repr]](v: Repr with Map[K, V]) =
+  implicit def toMapLikeOps[K, V, Repr <: Map[K, V] with MapLike[K, V, Repr]: ClassManifest](v: Repr with Map[K, V]) =
     expToMapLikeOps(v)
 }
 
@@ -275,7 +275,7 @@ trait CollectionSetOps {
 
   implicit def expToSetLikeOps[T, Repr <: Set[T] with SetLike[T, Repr]](v: Exp[Repr with Set[T]]) =
     new SetLikeOps[T, Repr] {val t = v}
-  implicit def toSetLikeOps[T, Repr <: Set[T] with SetLike[T, Repr]](v: Repr with Set[T]) =
+  implicit def toSetLikeOps[T, Repr <: Set[T] with SetLike[T, Repr]: ClassManifest](v: Repr with Set[T]) =
     expToSetLikeOps(v)
   implicit def CollectionSetExp2ExpCollectionSet[T](e: Set[Exp[T]]): Exp[Set[T]] = ExpSeq(e).toSet
 }

src/main/scala/ivm/optimization/NumericOptimTransforms.scala
@@ -36,6 +36,7 @@ trait NumericOptimTransforms {
   def buildSum[T: Numeric](l: Exp[T], r: Exp[T]): Exp[T] = {
     r match {
       case rc @ Const(rV) =>
+        implicit val manifest = rc.classManifest
         l match {
           case Const(a) => //R1
             a + rV
@@ -70,7 +71,8 @@ trait NumericOptimTransforms {
   //Copy-n-paste of buildSum. We don't use the distributive rule currently.
   def buildProd[T: Numeric](l: Exp[T], r: Exp[T]): Exp[T] = {
     r match {
-      case Const(rV) =>
+      case rc @ Const(rV) =>
+        implicit val manifest = rc.classManifest
         l match {
           case Const(a) => //R1
             a * rV
@@ -87,7 +89,8 @@ trait NumericOptimTransforms {
   }
 
   val reassociateOps: Exp[_] => Exp[_] = {
-    case n@Negate(Const(c)) =>
+    case n@Negate(constNode@Const(c)) =>
+      implicit val manifest = constNode.classManifest
       n.isNum.negate(c)
     case p@Plus(l, r) =>
       buildSum(l, r)(p.isNum)

src/main/scala/ivm/optimization/Optimization.scala
@@ -10,10 +10,11 @@ import performancetests.Benchmarking
 object Optimization {
   //Should the two normal forms (after flatMapToMap and after mapToFlatMap) be distinguished by different types?
 
-  val subqueries: Map[Exp[_], Any] = Map.empty
+  val subqueries: Map[Exp[_], (Any, ClassManifest[_])] = Map.empty
+  val castedSubqueries = subqueries.asInstanceOf[Map[Exp[_], (Any, ClassManifest[Any])]]
 
   def resetSubqueries() = subqueries.clear()
-  def addIndex[T](_query: UnconvertedExp[Exp[T]], res: Option[T] = None) {
+  def addIndex[T: ClassManifest](_query: UnconvertedExp[Exp[T]], res: Option[T] = None) {
     val query = OptimizationUtil.stripViewUntyped(_query.v)
     val optquery = optimizeIdx(query)
     val intQuery = res match {
@@ -24,8 +25,8 @@ object Optimization {
 
     //Let us ensure that both the unoptimized and the optimized version of the query are recognized by the
     // optimizer. TODO: Reconsider again whether this is a good idea.
-    subqueries += normalize(query) -> intQuery
-    subqueries += normalize(optquery) -> intQuery
+    subqueries += normalize(query) -> (intQuery, classManifest[T])
+    subqueries += normalize(optquery) -> (intQuery, classManifest[T])
   }
 
   def removeIndex[T](_query: UnconvertedExp[Exp[T]]) {
@@ -103,7 +104,7 @@ object Optimization {
 
   //removeIdentityMaps is appropriate here because typed-indexing can introduce identity maps.
   def shareSubqueries[T](query: Exp[T]): Exp[T] =
-    removeIdentityMaps(new SubquerySharing(subqueries).shareSubqueries(query))
+    removeIdentityMaps(new SubquerySharing(castedSubqueries).shareSubqueries(query))
 
   //Internally converts to flatMap normal form
   def handleFilters[T](exp: Exp[T]): Exp[T] =

commit 2d1393d366c1bb726ad5d730fe9482a6dcc2bdb8

Fix indexing breakage
This broke in 21ca401edba6c6b5dde7f18e9dd5783d906b7da6, where I fixed
indexing after the introduction of manifests just as far as required
by the typechecker. Indexing had to be fixed because the query
repository now stores values paired with manifests, instead of just
values. For most lookups, however, the casts on the fetched element
hid the type errors which had arised. I'm not sure how the code found a
manifest before the present fix.

src/main/scala/ivm/expressiontree/Const.scala
@@ -91,4 +91,6 @@ class ConstByIdentity[T](content: T, wit: ClassManifest[T]) extends Const(conten
 
 object ConstByIdentity {
   def apply[T: ClassManifest](content: T) = new ConstByIdentity[T](content, classManifest[T])
+  def apply[T](content: T, cm: ClassManifest[_])(implicit v: DummyImplicit) =
+    new ConstByIdentity[T](content, cm.asInstanceOf[ClassManifest[T]])
 }

src/main/scala/ivm/optimization/SubquerySharing.scala
@@ -68,9 +68,9 @@ class SubquerySharing(val subqueries: Map[Exp[_], (Any, ClassManifest[Any])]) {
       //In both cases, the type of the resulting expression becomes Traversable, which might lead to the result having the wrong dynamic type.
       //Luckily, the CanBuildFrom instances will be too generic but will delegate their work to the source collection,
       //hence producing a result of the right dynamic type.
-      case Some(t) =>
+      case Some((t, tMan)) =>
         println("Found simple index of form " + toLookup)
-        Some(asExp(t.asInstanceOf[Map[U, Traversable[T]]]) apply constantEqSide)
+        Some(ConstByIdentity(t.asInstanceOf[Map[U, Traversable[T]]], tMan) apply constantEqSide)
       case None =>
         println("Found no simple index of form " + toLookup)
         None
@@ -279,10 +279,10 @@ class SubquerySharing(val subqueries: Map[Exp[_], (Any, ClassManifest[Any])]) {
         assertType[Exp[U => Traversable[TupleT]]](groupedBy) //Just for documentation.
         val toLookup = normalizeBeforeLookup(groupedBy)
         subqueries.get(toLookup) match {
-          case Some(t) =>
+          case Some((t, tMan)) =>
             println("Found nested index of form " + toLookup)
             //Some(asExp(t.asInstanceOf[Map[U, Traversable[TupleT]]]) get constantEqSide flatMap identity withFilter Fun.makefun(cond, fx))
-            Some(asExp(t.asInstanceOf[Map[U, Traversable[TupleT]]]) apply constantEqSide withFilter Fun.makefun(cond, fx))
+            Some(ConstByIdentity(t.asInstanceOf[Map[U, Traversable[TupleT]]], tMan) apply constantEqSide withFilter Fun.makefun(cond, fx))
           case None =>
             println("Found no nested index of form " + toLookup)
             None
@@ -364,11 +364,11 @@ class SubquerySharing(val subqueries: Map[Exp[_], (Any, ClassManifest[Any])]) {
         //assertType[Exp[U => Traversable[TupleT]]](groupedBy) //Just for documentation.
         val toLookup = normalizeBeforeLookup(groupedBy)
         subqueries.get(toLookup) match {
-          case Some(t) =>
+          case Some((t, tMan)) =>
             println("Found nested type-index of form " + toLookup)
             type TupleTAnd[+T] = (TupleT, T)
             //This map step is wrong, we rely on substitution!
-            Some(asExp(t.asInstanceOf[TypeMapping[Traversable, TupleTAnd, AnyRef]]).get[T](clazz) /*map(_._1) */withFilter Fun.makefun(cond, fx))
+            Some(ConstByIdentity(t.asInstanceOf[TypeMapping[Traversable, TupleTAnd, AnyRef]], tMan).get[T](clazz) /*map(_._1) */withFilter Fun.makefun(cond, fx))
           case None =>
             println("Found no nested type-index of form " + toLookup)
             None