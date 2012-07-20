eb62a8c9653e190a546acd6a506f9e1ec3c6b3f9
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-07-20 20:38:08 +0200

    Update script

--- oldResultsAway.sh
+++ oldResultsAway.sh
@@ -1,7 +1,8 @@
-#!/bin/sh -x
+#!/bin/sh
 now=`timestamp`
-for i in ScalaTest JDK; do
+for i in -ScalaTest -JDK ''; do
   for j in '' -raw; do
-    mv LOSTestLog-$i$j.csv away/LOSTestLog-$i$j-$now.csv
+    src=LOSTestLog$i$j.csv
+    [ -f $src ] && mv $src away/LOSTestLog$i$j-$now.csv
   done
 done
