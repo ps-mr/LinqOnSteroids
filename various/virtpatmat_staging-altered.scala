3c485b8e346cbc70abc26baa70b39e089743fd8b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-08-04 22:57:16 +0200

    Experiments: Rename for clarity
    Match the names used in the compiler docs:
    
    Matcher -> MatchStrategy
    Maybe -> Matcher

--- various/virtpatmat_staging-altered.scala
+++ various/virtpatmat_staging-altered.scala
@@ -2,12 +2,13 @@
 //for 2.10.0-M6.
 import language.implicitConversions
 import language.higherKinds
+
 trait Intf {
  type Rep[+T]
- type M[+T] = Rep[Maybe[T]]
+ type M[+T] = Rep[Matcher[T]]
 
- val __match: Matcher
- abstract class Matcher {
+ val __match: MatchStrategy
+ abstract class MatchStrategy {
    // runs the matcher on the given input
    def runOrElse[T, U](in: Rep[T])(matcher: Rep[T] => M[U]): Rep[U]
 
@@ -17,12 +18,12 @@ trait Intf {
    def isSuccess[T, U](x: Rep[T])(f: Rep[T] => M[U]): Rep[Boolean] // used for isDefinedAt
  }
 
- abstract class Maybe[+A] {
+ abstract class Matcher[+A] {
    def flatMap[B](f: Rep[A] => M[B]): M[B]
    def orElse[B >: A](alternative: => M[B]): M[B]
  }
 
- implicit def proxyMaybe[A](m: M[A]): Maybe[A]
+ implicit def proxyMaybe[A](m: M[A]): Matcher[A]
  implicit def repInt(x: Int): Rep[Int]
  implicit def repBoolean(x: Boolean): Rep[Boolean]
  implicit def repString(x: String): Rep[String]
@@ -35,7 +36,7 @@ trait Intf {
 trait Impl extends Intf {
  type Rep[+T] = String
 
- object __match extends Matcher {
+ object __match extends MatchStrategy {
    def runOrElse[T, U](in: Rep[T])(matcher: Rep[T] => M[U]): Rep[U] = ("runOrElse("+ in +", ?" + matcher("?") + ")")
    def zero: M[Nothing]                                             = "zero"
    def one[T](x: Rep[T]): M[T]                                      = "one("+x.toString+")"
@@ -43,7 +44,7 @@ trait Impl extends Intf {
    def isSuccess[T, U](x: Rep[T])(f: Rep[T] => M[U]): Rep[Boolean]  = ("isSuccess("+x+", ?" + f("?") + ")")
  }
 
- implicit def proxyMaybe[A](m: M[A]): Maybe[A] = new Maybe[A] {
+ implicit def proxyMaybe[A](m: M[A]): Matcher[A] = new Matcher[A] {
    def flatMap[B](f: Rep[A] => M[B]): M[B]                          = m + ".flatMap(? =>"+ f("?") +")"
    def orElse[B >: A](alternative: => M[B]): M[B]                   = m + ".orElse("+ alternative +")"
  }
