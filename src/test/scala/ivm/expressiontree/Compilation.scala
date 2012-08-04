295c3ad68a8e135382d3f3769c498b328e8b815e
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-08-04 06:06:16 +0200

    Move Compilation to the right package
    Currently this class was not tested often, since I mostly test ivm._.

--- src/test/scala/ivm/expressiontree/Compilation.scala
+++ src/test/scala/ivm/expressiontree/Compilation.scala
@@ -1,11 +1,11 @@
-package performancetests
+package ivm
+package expressiontree
 
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
-import ivm._
-import expressiontree._
 import Lifting._
+import performancetests.Benchmarking
 
 /**
  * User: pgiarrusso
@@ -18,42 +18,42 @@ class Compilation extends FunSuite with ShouldMatchers with Benchmarking {
   //Correctness of code generation {{{
   if (Const.allowInlineInEval) {
     test("strings") {
-      asExp("foo").toCode should be ("\"foo\"")
-      asExp('c').toCode should be ("'c'")
-      asExp(1).toCode should be ("1")
-      asExp(1: Byte).toCode should be ("(1: Byte)")
+      asExp("foo").toCode should be("\"foo\"")
+      asExp('c').toCode should be("'c'")
+      asExp(1).toCode should be("1")
+      asExp(1: Byte).toCode should be("(1: Byte)")
     }
   }
   test("csp") {
-    asExp(List(1)).toCode should not be("List(1)")
+    asExp(List(1)).toCode should not be ("List(1)")
     Compile.precompileReset()
-    asExp(List(1)).toCode should be ("x1")
+    asExp(List(1)).toCode should be("x1")
   }
 
   val e = benchMark("build expression")(((asExp(1) + 2) * 3))
 
   test("code generation") {
     if (Const.allowInlineInEval) {
-      e.toCode should be ("((1) + (2)) * (3)")
+      e.toCode should be("((1) + (2)) * (3)")
     } else {
       Compile.precompileReset()
-      e.toCode should be ("((x1) + (x2)) * (x3)")
+      e.toCode should be("((x1) + (x2)) * (x3)")
     }
     Compile.precompileReset()
-    expOption2Iterable(Some(1)).toCode should be ("Option.option2Iterable(x1)")
-    expOption2Iterable(Some(1)).transform(identity).toCode should be ("Option.option2Iterable(x2)")
+    expOption2Iterable(Some(1)).toCode should be("Option.option2Iterable(x1)")
+    expOption2Iterable(Some(1)).transform(identity).toCode should be("Option.option2Iterable(x2)")
   }
   test("output") {
     if (Const.allowInlineInEval) {
-      Compile.emitSource(e) should be (
+      Compile.emitSource(e) should be(
         """class Outclass1() extends Compiled[Int] {
           |  override def result = ((1) + (2)) * (3)
           |}""".stripMargin)
     } else {
-      Compile.emitSource(e) should be (
-      """class Outclass1(val x1: Int, val x2: Int, val x3: Int) extends Compiled[Int] {
-        |  override def result = ((x1) + (x2)) * (x3)
-        |}""".stripMargin)
+      Compile.emitSource(e) should be(
+        """class Outclass1(val x1: Int, val x2: Int, val x3: Int) extends Compiled[Int] {
+          |  override def result = ((x1) + (x2)) * (x3)
+          |}""".stripMargin)
     }
   }
 
@@ -62,7 +62,7 @@ class Compilation extends FunSuite with ShouldMatchers with Benchmarking {
   }
 
   test("benchmark and test generated code") {
-    benchMark("compiled code")(new Outclass1(1, 2, 3).result) should be (9)
+    benchMark("compiled code")(new Outclass1(1, 2, 3).result) should be(9)
   }
   //}}}
 
@@ -74,13 +74,13 @@ class Compilation extends FunSuite with ShouldMatchers with Benchmarking {
     benchMark("Use Compile.emitSource on expression")(Compile.emitSource(e))
   }
   test("benchMark Compile.toValue") {
-    benchMark("Use Compile.toValue on expression")(Compile.toValue(e)) should be (9)
+    benchMark("Use Compile.toValue on expression")(Compile.toValue(e)) should be(9)
   }
   test("benchMark Compile.toValue for CSP") {
-    benchMark("Use Compile.toValue on CSP expression")(Compile.toValue(asExp(List(1)))) should be (List(1))
+    benchMark("Use Compile.toValue on CSP expression")(Compile.toValue(asExp(List(1)))) should be(List(1))
   }
   test("benchMark Compile.toValue for collection expression") {
-    benchMark("Use Compile.toValue on collection expression")(Compile.toValue(asExp(List(1, 2, 3)) map (_ + 1))) should be (List(2, 3, 4))
+    benchMark("Use Compile.toValue on collection expression")(Compile.toValue(asExp(List(1, 2, 3)) map (_ + 1))) should be(List(2, 3, 4))
   }
   // }}}
 }
