a1284da2f8b34142ba4d3afb5f8b70319481c2ab
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-08-10 18:57:57 +0200

    Compile support for IfInstanceOf

--- src/main/scala/ivm/expressiontree/IfInstanceOf.scala
+++ src/main/scala/ivm/expressiontree/IfInstanceOf.scala
@@ -10,10 +10,12 @@ object IfInstanceOf {
 }
 
 //Equality comparison must consider also classS. Therefore, classS must be a class parameter.
-case class IfInstanceOf[T, S](x: Exp[T], classS: Class[_])(implicit val cS: TypeTag[S]) extends Arity1OpExp[T, Option[S], IfInstanceOf[T, S]](x) {
+case class IfInstanceOf[T, S](x: Exp[T], classS: Class[_])(implicit val cS: TypeTag[S]) extends Arity1OpExp[T, Option[S], IfInstanceOf[T, S]](x) with PersistClassS {
   def copy(x: Exp[T]) = IfInstanceOf(x, classS)
   def interpret() =
     Util.ifInstanceOfBody(x.interpret(), classS)
+  //ifInstanceOfBody has type parameters which by default are deduced to be Nothing - and has _two_ of them. To avoid needing a TypeTag for T, use ascription on the result.
+  override def toCode = "ivm.expressiontree.Util.ifInstanceOfBody(%s, %s): Option[%s]" format (x.toCode, persistedValue, Compile manifestToString cS)
 }
 
 case class IsInstanceOf[T, S](x: Exp[T])(implicit val cS: ClassTag[S], val tS: TypeTag[S]) extends Arity1OpExp[T, Boolean, IsInstanceOf[T, S]](x) {
@@ -27,4 +29,5 @@ case class IsInstanceOf[T, S](x: Exp[T])(implicit val cS: ClassTag[S], val tS: T
 case class AsInstanceOf[T, S](x: Exp[T])(implicit val cS: ClassTag[S], val tS: TypeTag[S]) extends Arity1OpExp[T, S, AsInstanceOf[T, S]](x) {
   def copy(x: Exp[T]) = AsInstanceOf(x)
   def interpret() = (cS.runtimeClass cast x.interpret()).asInstanceOf[S]
+  override def toCode = "%s.asInstanceOf[%s]" format (x.toCode, Compile manifestToString tS)
 }
