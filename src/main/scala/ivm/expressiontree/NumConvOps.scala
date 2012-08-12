fc7a572df9926124ea0d606f149efe153b318961
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-08-13 01:54:04 +0200

    More experiments
    Both versions cause a crash, so this is for now commented out.

--- src/main/scala/ivm/expressiontree/NumConvOps.scala
+++ src/main/scala/ivm/expressiontree/NumConvOps.scala
@@ -12,7 +12,13 @@ trait NumConvOps {
   implicit def expInt2Float[T](t: Exp[Int]): Exp[Float] =
     convLift(t, 'Int2Float, "Int.int2float")
 
-  implicit def int2ExpDouble = convFromBase[Int, Double](_)
-  implicit def int2ExpLong = convFromBase[Int, Long](_)
-  implicit def int2ExpFloat = convFromBase[Int, Float](_)
+  //1st try:
+//  implicit def int2ExpDouble = convFromBase[Int, Double](_)
+//  implicit def int2ExpLong = convFromBase[Int, Long](_)
+//  implicit def int2ExpFloat = convFromBase[Int, Float](_)
+
+  //2nd try:
+//  implicit def int2ExpDouble(t: Int) = convFromBase[Int, Double](t)
+//  implicit def int2ExpLong(t: Int) = convFromBase[Int, Long](t)
+//  implicit def int2ExpFloat(t: Int) = convFromBase[Int, Float](t)
 }
