7de1452e3e7cb2f0f451d286fe4c048cb7f78d23
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-13 15:41:11 +0200

    ported longbits to double checker, has some strange compile errors

---
+++ src/main/scala/performancetests/opaltests/analyses/DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT.scala
@@ -0,0 +1,92 @@
+package performancetests.opaltests.analyses
+
+import de.tud.cs.st.bat.resolved._
+
+/**
+ *
+ * Author: Ralf Mitschke
+ * Date: 06.08.12
+ * Time: 15:53
+ *
+ */
+trait DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT{
+    this: performancetests.opaltests.FBAnalysesBase =>
+
+    import BaseAnalyses._
+
+    import ivm.expressiontree.Exp
+
+    import BaseAnalyses.withIndex
+
+    val doubleClass = ObjectType("java/lang/Double")
+
+    val longBitsToDoubleDescriptor = MethodDescriptor(List(LongType), DoubleType)
+
+    def analyzeBaseWithoutAbstractions() = {
+      for (classFile ← classFiles;
+           method ← classFile.methods if method.body.isDefined;
+           Seq(
+           (I2L, _),
+           (INVOKESTATIC(`doubleClass`, "longBitsToDouble", `longBitsToDoubleDescriptor`), idx)
+              ) ← withIndex(method.body.get.instructions).sliding(2)
+      ) yield
+        (classFile, method, idx)
+    }
+
+/*
+    def analyzeSQuOptWithoutAbstractions() = {
+      import ivm._
+      import expressiontree._
+      import Lifting._
+      import BATLifting._
+      import performancetests.opaltests.InstructionLifting._
+      import ivm.expressiontree.Util.ExtraImplicits._
+      for (classFile ← classFiles.asSmart;
+           method ← classFile.methods if method.body.isDefined;
+           window ← withIndexExp(method.body.get.instructions).sliding(2);
+           //[error] D:\workspace\LinqOnSteroids\src\main\scala\performancetests\opaltests\analyses\DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT.scala:46: value filter is not a member of ivm.expressiontree.Exp[de.tud.cs.st.bat.resolved.INVOKESTATIC]
+           //[error]            second ? window.last._1.asInstanceOf_#[INVOKESTATIC]
+           second ← window.last._1.asInstanceOf_#[INVOKESTATIC]
+           if(  window.head._1.isInstanceOf_#[I2L] &&
+                second.declaringClass ==# doubleClass &&
+                second.name ==# "longBitsToDouble" &&
+                second.methodDescriptor ==# longBitsToDoubleDescriptor
+              )
+      ) yield
+        (classFile, method, window.last._2)
+    }
+*/
+
+    def analyzeBaseWithAbstractions() = {
+      for (schema.BytecodeInstrWindow(Seq(_,idx),
+              Seq(I2L,INVOKESTATIC(`doubleClass`, "longBitsToDouble", `longBitsToDoubleDescriptor`)),
+               classFile, method) ← methodBodiesInstructionsSlidingNative(2)
+      ) yield
+        (classFile, method, idx)
+    }
+
+/*
+    def analyzeSQuOptWithAbstractions() = {
+        import de.tud.cs.st.bat.resolved._
+        import ivm._
+        import expressiontree._
+        import Lifting._
+        import BATLifting._
+        import performancetests.opaltests.InstructionLifting._
+        import ivm.expressiontree.Util.ExtraImplicits._
+        import schema.squopt._
+        for ( window ← methodBodiesInstructionsSlidingSQuOpt(3);
+        //[error] D:\workspace\LinqOnSteroids\src\main\scala\performancetests\opaltests\analyses\DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT.scala:74: value filter is not a member of ivm.expressiontree.Exp[de.tud.cs.st.bat.resolved.INVOKESTATIC]
+        //[error]               second ? window.instrs.last.asInstanceOf_#[INVOKESTATIC]
+              second ← window.instrs.last.asInstanceOf_#[INVOKESTATIC]
+               if(  window.instrs.head.isInstanceOf_#[I2L] &&
+                    second.declaringClass ==# doubleClass &&
+                    second.name ==# "longBitsToDouble" &&
+                    second.methodDescriptor ==# longBitsToDoubleDescriptor
+                  )
+         ) yield
+            (window.classFile, window.method, window.instrIdxs.last)
+
+    }
+*/
+}
