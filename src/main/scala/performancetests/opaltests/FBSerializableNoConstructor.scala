37de3a6bc26808ef500e94c08d85f090ed705ee9
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-17 16:03:10 +0200

    added withAbstraction versions of SE_NO_SUITABLE_CONSTRUCTOR

--- src/main/scala/performancetests/opaltests/FBSerializableNoConstructor.scala
+++ src/main/scala/performancetests/opaltests/FBSerializableNoConstructor.scala
@@ -1,28 +1,21 @@
 package performancetests
 package opaltests
 
-import ivm._
-import expressiontree._
-import Lifting._
-
-import de.tud.cs.st.bat
-import bat.resolved._
-
-import collection.immutable.Seq
-import collection.{Seq => CSeq}
+import de.tud.cs.st.bat.resolved._
 
 /**
  * User: pgiarrusso
  * Date: 4/8/2012
  */
-
+// FB: SE_NO_SUITABLE_CONSTRUCTOR
+// this analysis does not optimized away any of the abstractions
 trait FBSerializableNoConstructor {
   this: FBAnalysesBase =>
 
-  def analyzeSerializableNoConstructor() {
-    // FINDBUGS: Se: Class is Serializable but its superclass doesn't define a void constructor (SE_NO_SUITABLE_CONSTRUCTOR)
-    val serializableClasses = classHierarchy.subclasses(ObjectType("java/io/Serializable")).getOrElse(Set.empty)
-    benchQueryComplete("NO_SUITABLE_CONSTRUCTOR") { // FB: SE_NO_SUITABLE_CONSTRUCTOR
+  private val serializableClasses = classHierarchy.subclasses(ObjectType("java/io/Serializable")).getOrElse(Set.empty)
+
+  private def analyzeBaseWithoutAbstractions() = {
+    import collection.{Seq => CSeq}
       for {
         superclass ← classHierarchy.superclasses(serializableClasses) if getClassFile.isDefinedAt(superclass) && // the class file of some supertypes (defined in libraries, which we do not analyze) may not be available
         {
@@ -30,9 +23,18 @@ trait FBSerializableNoConstructor {
           !superClassFile.isInterfaceDeclaration &&
             !superClassFile.constructors.exists(_.descriptor.parameterTypes.length == 0)
         }
-      } yield superclass // there can be at most one method
-    } {
-      import BATLifting._
+      } yield superclass // only the class is returned; there can be at most one method
+  }
+
+
+  private def analyzeSQuOptWithoutAbstractions() = {
+    import de.tud.cs.st.bat.resolved._
+    import ivm._
+    import expressiontree._
+    import Lifting._
+    import BATLifting._
+    import InstructionLifting._
+    import ivm.expressiontree.Util.ExtraImplicits._
       for {
         superclass ← classHierarchy.superclasses(serializableClasses).asSmart if getClassFile.asSmart.isDefinedAt(superclass) && // the class file of some supertypes (defined in libraries, which we do not analyze) may not be available
         {
@@ -40,7 +42,60 @@ trait FBSerializableNoConstructor {
           !superClassFile.isInterfaceDeclaration &&
             !superClassFile.constructors.exists(_.descriptor.parameterTypes.length ==# 0)
         }
-      } yield superclass // there can be at most one method
-    }
+      } yield superclass // only the class is returned; there can be at most one method
   }
+
+
+  private def analyzeBaseWithAbstractions() = {
+    import de.tud.cs.st.bat.resolved._
+    import collection.{Seq => CSeq}
+    import schema._
+    import collection.{Seq => CSeq}
+      for {
+        superclass ← classHierarchy.superclasses(serializableClasses) if getClassFile.isDefinedAt(superclass) && // the class file of some supertypes (defined in libraries, which we do not analyze) may not be available
+        {
+          val superClassFile = getClassFile(superclass)
+          !superClassFile.isInterfaceDeclaration &&
+            !superClassFile.constructors.exists(_.descriptor.parameterTypes.length == 0)
+        }
+      } yield superclass // only the class is returned; there can be at most one method
+  }
+
+
+  private def analyzeSQuOptWithAbstractions() = {
+      import de.tud.cs.st.bat.resolved._
+      import ivm._
+      import expressiontree._
+      import Lifting._
+      import BATLifting._
+      import InstructionLifting._
+      import ivm.expressiontree.Util.ExtraImplicits._
+      import schema.squopt._
+            for {
+              superclass ← classHierarchy.superclasses(serializableClasses).asSmart if getClassFile.asSmart.isDefinedAt(superclass) && // the class file of some supertypes (defined in libraries, which we do not analyze) may not be available
+              {
+                val superClassFile = getClassFile.asSmart.apply(superclass)
+                !superClassFile.isInterfaceDeclaration &&
+                  !superClassFile.constructors.exists(_.descriptor.parameterTypes.length ==# 0)
+              }
+            } yield superclass // only the class is returned; there can be at most one method
+    }
+
+
+
+  def analyzeSerializableNoConstructor() {
+    // FINDBUGS: Se: Class is Serializable but its superclass doesn't define a void constructor (SE_NO_SUITABLE_CONSTRUCTOR)
+    benchQueryComplete("NO_SUITABLE_CONSTRUCTOR")( // FB: SE_NO_SUITABLE_CONSTRUCTOR
+      // Weakness: In a project, where we extend a predefined class (of the JDK) that
+      // inherits from Comparable and in which we define covariant comparesTo method,
+      // we will not be able to identify this issue unless we have identified the whole
+      // class hierarchy.
+       analyzeBaseWithoutAbstractions(),
+       analyzeBaseWithAbstractions()
+      )(
+       analyzeSQuOptWithoutAbstractions(),
+       analyzeSQuOptWithAbstractions()
+      )
+  }
+
 }
