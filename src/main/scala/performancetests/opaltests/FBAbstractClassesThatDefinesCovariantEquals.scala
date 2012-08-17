1da899e40673409145615ef54bac240e8aa5d8b7
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-17 15:35:32 +0200

    added withAbstraction versions of EQ_ABSTRACT_SELF

--- src/main/scala/performancetests/opaltests/FBAbstractClassesThatDefinesCovariantEquals.scala
+++ src/main/scala/performancetests/opaltests/FBAbstractClassesThatDefinesCovariantEquals.scala
@@ -1,41 +1,76 @@
 package performancetests
 package opaltests
 
-import ivm._
-import expressiontree._
-import Lifting._
-
-import de.tud.cs.st.bat
-import bat.resolved._
-
-import collection.immutable.Seq
-import collection.{Seq => CSeq}
+import de.tud.cs.st.bat.resolved._
 
 /**
- * User: pgiarrusso
+ * User: pgiarrusso, Ralf Mitschke
  * Date: 4/8/2012
  */
-
+// EQ_ABSTRACT_SELF
 trait FBAbstractClassesThatDefinesCovariantEquals {
   this: FBAnalysesBase =>
 
+
+  private def analyzeBaseWithoutAbstractions() = {
+    for {
+      classFile ← classFiles
+      method @ Method(_, "equals", MethodDescriptor(CSeq(parameterType), BooleanType), _) ← classFile.methods
+      if method.isAbstract && parameterType == classFile.thisClass //!= ObjectType.Object
+    } yield (classFile.thisClass, method.name, method.descriptor)
+  }
+
+  private def analyzeSQuOptWithoutAbstractions() = {
+    import de.tud.cs.st.bat.resolved._
+    import ivm._
+    import expressiontree._
+    import Lifting._
+    import BATLifting._
+    import performancetests.opaltests.InstructionLifting._
+    import ivm.expressiontree.Util.ExtraImplicits._
+    import schema.squopt._
+    for {
+      classFile ← classFiles.asSmart
+      method ← classFile.methods
+      if method.isAbstract && method.name ==# "equals" && method.descriptor.returnType ==# BooleanType
+      parameterTypes <- Let(method.descriptor.parameterTypes)
+      if parameterTypes.length ==# 1 && parameterTypes(0) ==# classFile.thisClass //parameterTypes(0) !=# ObjectType.Object
+    } yield (classFile.thisClass, method.name, method.descriptor)
+  }
+
+
+  private def analyzeBaseWithAbstractions() = {
+    import schema._
+    for { MethodRecord(classFile, method @ Method(_, "equals", MethodDescriptor(CSeq(parameterType), BooleanType), _)) ← methodsNative
+          if method.isAbstract && parameterType == classFile.thisClass //!= ObjectType.Object
+    } yield (classFile.thisClass, method.name, method.descriptor)
+  }
+
+  private def analyzeSQuOptWithAbstractions() = {
+    import de.tud.cs.st.bat.resolved._
+    import ivm._
+    import expressiontree._
+    import Lifting._
+    import BATLifting._
+    import performancetests.opaltests.InstructionLifting._
+    import ivm.expressiontree.Util.ExtraImplicits._
+    import schema.squopt._
+    for {
+      methodRecord ← methodsSQuOpt
+      if methodRecord.method.isAbstract && methodRecord.method.name ==# "equals" && methodRecord.method.descriptor.returnType ==# BooleanType
+      parameterTypes <- Let(methodRecord.method.descriptor.parameterTypes)
+      if parameterTypes.length ==# 1 && parameterTypes(0) ==# methodRecord.classFile.thisClass //parameterTypes(0) !=# ObjectType.Object
+    } yield (methodRecord.classFile.thisClass, methodRecord.method.name, methodRecord.method.descriptor)
+  }
+
   def analyzeAbstractClassesThatDefinesCovariantEquals() {
     //XXX this was changed in BAT.
-    benchQueryComplete("COVARIANT_EQUALS") { // FB: EQ_ABSTRACT_SELF") {
-      for {
-        classFile ← classFiles
-        method @ Method(_, "equals", MethodDescriptor(CSeq(parameterType), BooleanType), _) ← classFile.methods
-        if method.isAbstract && parameterType == classFile.thisClass //!= ObjectType.Object
-      } yield (classFile, method)
-    } {
-      import BATLifting._
-      for {
-        classFile ← classFiles.asSmart
-        method ← classFile.methods
-        if method.isAbstract && method.name ==# "equals" && method.descriptor.returnType ==# BooleanType
-        parameterTypes <- Let(method.descriptor.parameterTypes)
-        if parameterTypes.length ==# 1 && parameterTypes(0) ==# classFile.thisClass //parameterTypes(0) !=# ObjectType.Object
-      } yield (classFile, method)
-    }
+    benchQueryComplete("COVARIANT_EQUALS") ( // FB: EQ_ABSTRACT_SELF") {
+         analyzeBaseWithoutAbstractions(),
+         analyzeBaseWithAbstractions()
+       )(
+        analyzeSQuOptWithoutAbstractions(),
+        analyzeSQuOptWithAbstractions()
+        )
   }
 }
