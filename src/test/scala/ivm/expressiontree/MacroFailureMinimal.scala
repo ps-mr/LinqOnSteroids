231ce6a4f0184b2783ec627351811befe5ec1cb2
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-08-28 20:49:06 +0200

    Further testcase variation

--- src/test/scala/ivm/expressiontree/MacroFailureMinimal.scala
+++ src/test/scala/ivm/expressiontree/MacroFailureMinimal.scala
@@ -10,6 +10,7 @@ object MacroFailureMinimalHelper {
   import collection.TraversableLike
   implicit class TraversableLikeOps[T, Repr <: Traversable[T] with TraversableLike[T, Repr]](t: Exp[Repr with TraversableLike[T, Repr]]) {
     def map[U, That <: Traversable[U] with TraversableLike[U, That]](f: Exp[T] => Exp[U])(implicit c: CanBuildFrom[Repr, U, That]): Exp[That] = ???
+    def filter(f: Exp[T] => Exp[Boolean]): Exp[Repr] = ???
   }
   val coll = asExp(List(1, 2, 3))
 }
@@ -28,6 +29,10 @@ object MacroFailureMinimal {
 
   val f15_show = ctShowDebug {
     coll map (i => i)
+  }
+
+  val f15_show_2 = ctShowDebug {
+    coll map (i => i) filter (i => false)
   }
 }
 
