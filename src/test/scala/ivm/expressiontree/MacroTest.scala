f7f523c7f9ebf76356cabf519ca8bf39df85aa6e
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-09-02 01:13:46 +0200

    Add testcases for smart macro on AnyRef methods with type params

--- src/test/scala/ivm/expressiontree/MacroTest.scala
+++ src/test/scala/ivm/expressiontree/MacroTest.scala
@@ -14,7 +14,10 @@ object Helpers {
   def smart_==[T](a: Exp[T], b: Exp[T]) = Eq(a, b)
   //This method is side-effect-based, not clear how to support it.
   //def smart_synchronized[T, T0](a: T, b: =>T0) = a synchronized b
-  //def smart_synchronized[T0](a: Any, b: =>T0) = a synchronized b
+  def smart_synchronized[T0](a: AnyRef, b: =>T0) = {
+    println("smart_synchronized called!")
+    a synchronized b
+  }
 
   //def smart_asInstanceOf[T](a: Exp[Any]) =
   //def smart_isInstanceOf[T](a: Any): Boolean = a.isInstanceOf[T] //argh...
@@ -23,13 +26,15 @@ object Helpers {
   //Still, I don't expect that to work. Let's use implicits instead!
   //please move soon to TypeTag, as soon as I get it.
   //Do I even need these methods to be available? What's the point of having an
-  //overload for not Exp, Exp?
+  //overload whose type does not mention Exp?
   def smart_isInstanceOf[T: ClassTag](a: Any): Boolean =
     implicitly[ClassTag[T]].runtimeClass.isInstance(a)
     //a.isInstanceOf[T] //argh...
   //def smart_asInstanceOf[T](a: Any): T = a.asInstanceOf[T] //no warning...weird.
-  def smart_asInstanceOf[T: ClassTag](a: Any): T =
+  def smart_asInstanceOf[T: ClassTag: TypeTag](a: Any): T =
     implicitly[ClassTag[T]].runtimeClass.cast(a).asInstanceOf[T]
+  def smart_isInstanceOf[T: ClassTag: TypeTag](a: Exp[Any]): Exp[Boolean] =
+    IsInstanceOf(a)
   def smart_asInstanceOf[T: ClassTag: TypeTag](a: Exp[Any]): Exp[T] =
     AsInstanceOf(a)
 }
@@ -41,10 +46,19 @@ object MacroTest extends scala.App with tests.TestUtil {
 
   val c1 = asExp(1)
   val c2 = asExp(2)
+
   /*
   //Test printf
   printf("hello %s!\n", "world")
   */
+  //println("Without macros: " + (1.asInstanceOf: Int)) //This syntax just seems not to work at runtime, the type inferencer figures out Nothing as type parameter.
+  println("With macros: " + smart(c1.asInstanceOf[Int]))
+  //println("With macros: " + smart(c1.asInstanceOf: Int)) //This syntax just seems not to work, the type inferencer figures out Nothing as type parameter. So we don't need to support it.
+
+  println("With macros: " + smart(c1.isInstanceOf[Int]))
+
+  println("With macros: " + smart(c1 synchronized 2))
+  println("With macros: " + smart(c1.synchronized[Int](2)))
 
   println("With macros: " + smart(c1.toString))
   println("With macros: " + smart(c1.toString.toString))
@@ -61,12 +75,6 @@ object MacroTest extends scala.App with tests.TestUtil {
   showExp(smart(((c1 == 2 || c2 == 1), "With macros: ")), "Pair")
   //After all, we want to allow inside smart calls to reifying methods to have modularity, but we don't necessarily care
   //for allowing calls to non-reifying methods - am I right? Usually we in fact want to reify them. We want to reify everything inside smart, and then some (the calls to reifying methods).
-  //println("With macros: " + smart(c1 synchronized 2))
-  //println("With macros: " + smart(c1.synchronized[Int](2)))
-
-  //println("With macros: " + smart(c1.asInstanceOf[Int]))
-
-  //println("With macros: " + smart(c1.isInstanceOf[Int]))
 
   //val coll = (1 to 10).asSmart
   //val coll = List(1, 2, 3).asSmart
@@ -126,6 +134,7 @@ object MacroTest extends scala.App with tests.TestUtil {
     println("With macros: " + f)
     println("With macros, after code: " + (Compile toCode f))
   }
+  //*/
 }
 
 // vim: set sw=2 et:
