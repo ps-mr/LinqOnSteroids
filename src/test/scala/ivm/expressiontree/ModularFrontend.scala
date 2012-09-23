877e9f466cab09d6cc606119ad3c552b29dc4713
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-09-23 17:05:05 +0200

    Prototype modular and adequate frontend

--- src/test/scala/ivm/expressiontree/ModularFrontend.scala
+++ src/test/scala/ivm/expressiontree/ModularFrontend.scala
@@ -1,28 +1,55 @@
 package ivm.expressiontree
 
-trait ScalaIntf {
+trait LangIntf {
+  type Rep[+T]
+}
+/* Compared to the example below, the LangIntf classes can be taken out and LangIntf can just become a type parameter.*/
+/*trait ScalaIntf {
   this: Interface =>
-  type Symantics <: ScalaSymantics
-  trait ScalaSymantics {
-    this: Symantics =>
+  type LangIntf <: ScalaLangIntf*/
+  trait ScalaLangIntf {
+    this: LangIntf =>
+    //Why not an implicit abstract class? Ah I see.
     implicit def toIntOps(v: Rep[Int]): IntOps
     abstract class IntOps(v: Rep[Int]) {
       def +(that: Rep[Int]): Rep[Int]
     }
   }
-}
+//}
 
-trait Interface {
-  type Symantics <: BaseSymantics
-  trait BaseSymantics {
+/*trait Interface {
+  type LangIntf <: BaseLangIntf
+  trait BaseLangIntf {
+  */
+  trait BaseLangIntf extends LangIntf {
     type Rep[+T]
     implicit def unit[T](t: T): Rep[T]
   }
-}
+/*}
 
 trait MyIntf extends Interface with ScalaIntf {
-  type Symantics <: BaseSymantics with ScalaSymantics
+  type LangIntf <: BaseLangIntf with ScalaLangIntf
+}
+*/
+
+trait Interpreted[Sym <: LangIntf, Res] {
+  type ThisLangIntf = Sym
+  def apply(s: ThisLangIntf): s.Rep[Res]
+}
+object ModularFrontendExample extends App {
+  new Interpreted[BaseLangIntf with ScalaLangIntf, Int] {
+    def apply(s: ThisLangIntf): s.Rep[Int] = {
+      import s._
+      unit(1) + 2
+    }
+  }
+}
+class ModularFrontendExample extends Interpreted[BaseLangIntf with ScalaLangIntf, Int] {
+  def apply(s: ThisLangIntf): s.Rep[Int] = {
+    import s._
+    unit(1) + 2
+  }
 }
 
 
-// vim: set ts=4 sw=4 et:
+// vim: set ts=8 sw=2 et:
