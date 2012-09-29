5c27232a473e77c2fcc27dfff3570da6df1fa225
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-09-29 17:37:56 +0200

    Misc cleanups

--- src/main/scala/ivm/expressiontree/Eq.scala
+++ src/main/scala/ivm/expressiontree/Eq.scala
@@ -3,12 +3,7 @@ package ivm.expressiontree
 case class Eq[/*@specialized(Int, Boolean, Double)*/ T](t1: Exp[T], t2: Exp[T]) extends Arity2OpSymmExp[T, Boolean, Eq[T]] with InfixPrinting {
   def copy(x: Exp[T], y: Exp[T]) = Eq(x, y)
   def operator = "=="
-  def interpret() = {
-    val v1 = t1.interpret()
-    val v2 = t2.interpret()
-    v1 == v2
-
-  };
+  def interpret() = t1.interpret() == t2.interpret()
   //The implementation below could work better when specialization is active.
   // But apparently that annotation is not enough. Retry by specializing also Exp[T] for T = Int, Boolean, ...
   //def interpret() = x.interpret() == y.interpret();
