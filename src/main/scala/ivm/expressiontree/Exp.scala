ps-mr/LinqOnSteroids

commit eb5b664a4f6c7c631cd179f280e358690780b686

Move tree navigation methods from Exp to TreeNode

src/main/scala/ivm/expressiontree/Exp.scala
@@ -87,6 +87,25 @@ trait TreeNode[+T] {
   def interpret(): T
   def children: List[Exp[_]]
   def persistValues()
+
+  /* I renamed this method to avoid conflicts with Matcher.find. XXX test if
+  * just making it private also achieves the same result.
+  */
+  def __find(filter: PartialFunction[TreeNode[_], Boolean]): Seq[TreeNode[_]] = {
+    val baseSeq =
+      if (PartialFunction.cond(this)(filter))
+        Seq(this)
+      else
+        Seq.empty
+    children.map(_ __find filter).fold(baseSeq)(_ ++ _)
+  }
+
+  // This overload is not called find because that would confuse type inference - it would fail to infer that filter's
+  // domain type is Exp[_].
+  def findTotFun(filter: TreeNode[_] => Boolean): Seq[TreeNode[_]] = __find(filter.asPartial)
+
+  def isOrContains(e: TreeNode[_]): Boolean =
+    (this findTotFun (_ == e)).nonEmpty
 }
 
 sealed trait Exp[+T] extends TreeNode[T] /*with MsgSeqPublisher[T, Exp[T]]*/ {
@@ -121,25 +140,6 @@ sealed trait Exp[+T] extends TreeNode[T] /*with MsgSeqPublisher[T, Exp[T]]*/ {
     val mappedChilds = for (c <- children) yield c.treeMap(mapper)
     mapper(this, mappedChilds)
   }
-
-  /* I renamed this method to avoid conflicts with Matcher.find. XXX test if
-  * just making it private also achieves the same result.
-  */
-  def __find(filter: PartialFunction[Exp[_], Boolean]): Seq[Exp[_]] = {
-    val baseSeq =
-      if (PartialFunction.cond(this)(filter))
-        Seq(this)
-      else
-        Seq.empty
-    children.map(_ __find filter).fold(baseSeq)(_ ++ _)
-  }
-
-  // This overload is not called find because that would confuse type inference - it would fail to infer that filter's
-  // domain type is Exp[_].
-  def findTotFun(filter: Exp[_] => Boolean): Seq[Exp[_]] = __find(filter.asPartial)
-
-  def isOrContains(e: Exp[_]): Boolean =
-    (this findTotFun (_ == e)).nonEmpty
 
   def substSubTerm[S](SubTerm: Exp[_], e: Exp[S]) =
     this transform {

commit 9bd295dffe48c14d62e41e8d649993d0515cb8f4

Fix test results: Make Exp.isOrContains take an Exp
This fixes a regression introduced by
eb5b664a4f6c7c631cd179f280e358690780b686, which made the conversion
take a TreeNode. Nicely enough, there's code relying on implicit
conversions (from Def to Exp) which stopped working with the more
general type (which doesn't trigger the conversion anymore).

src/main/scala/ivm/expressiontree/Exp.scala
@@ -104,7 +104,7 @@ trait TreeNode[+T] {
   // domain type is Exp[_].
   def findTotFun(filter: TreeNode[_] => Boolean): Seq[TreeNode[_]] = __find(filter.asPartial)
 
-  def isOrContains(e: TreeNode[_]): Boolean =
+  def isOrContains(e: Exp[_]): Boolean =
     (this findTotFun (_ == e)).nonEmpty
 }
 