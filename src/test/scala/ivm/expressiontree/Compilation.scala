ps-mr/LinqOnSteroids

commit 24aed765311009c7fa83f607d2d54cbdd6302c45

Call persistValues on the right expression and cleanup
* Remove code duplication between the two backends
  by creating precompiledExpToValue
* Streamline flow of toValueCSE: it's now just
    precompiledExpToValue(collectSymbols(doCSE(removeConsts(e))))
  while toValue is
    precompiledExpToValue(removeConsts(e))
* Remove extractConsts, it was the wrong abstraction when you want
  to be able to change the backend. The useful bits go to
  extractCSPData: it does a bit too much for some callers, but those
  are test methods anyway.
I tested toValue with FindBugsAnalyses and toValueCSE with CompileCSETest.
More bugs are left though.

src/main/scala/ivm/expressiontree/Compile.scala
@@ -152,11 +152,16 @@ object Compile {
       constNodes.asInstanceOf[mutable.Map[Const[U], Exp[U]]].getOrElseUpdate(c,
         CrossStagePersistence.persist(c.x)(c.cTag, c.tTag))
 
-    e transform {
+    val transfExp = e transform {
       case c: Const[_] =>
         persistConst(c)
       case exp => exp
     }
+    assert((transfExp __find {
+      case c: Const[_] =>
+        true
+    }).isEmpty)
+    transfExp
   }
 
   private def getStaticData(cspValues: Seq[(Any, CSPVar)]): Seq[(ClassTag[_], Any)] =
@@ -184,15 +189,11 @@ object Compile {
     (prefix, restSourceCode, className)
   }
 
-  private def extractConsts[T: TypeTag](e: Exp[T]): (Exp[T], Seq[(Any, CSPVar)]) = {
-    val transfExp = removeConsts(e)
-    assert((transfExp __find {
-      case c: Const[_] =>
-        true
-    }).isEmpty)
-    transfExp.persistValues()
+  private def extractCSPData[T: TypeTag](e: Exp[T]): (Seq[(Any, CSPVar)], Seq[(ClassTag[_], Any)]) = {
+    e.persistValues()
     val cspValues = cspMap.get().toList.toSeq //toList forces immutability.
-    (transfExp, cspValues)
+    val staticData = getStaticData(cspValues)
+    (cspValues, staticData)
   }
 
   private case class Scope(boundVar: Option[Var] = None, bindings: mutable.Map[Int, Def[_]] = mutable.Map.empty)
@@ -286,46 +287,44 @@ object Compile {
       withNewScope(Scope(), topDownTraverse(e))
     }
 
-    val (constlessExp, cspValues) = extractConsts(e)
-    val staticData = getStaticData(cspValues)
-
     //rewrite the tree while sharing common subexpression, producing a DAG.
-    val cseExp = constlessExp transform {
-      case Sym(defNode) =>
-        //XXX We need to build here a symbol which does consider the id in the equality! But write a test for that first.
-        //Otherwise, CSE won't distinguish between Symbols in different scopes, since the IDs are not part of equality comparison. Darn!
-        toAtomCSE(defNode)
-      case c: Const[t] => throw new RuntimeException("Const expression in constlessExp")
+    def doCSE[U](constlessExp: Exp[U]): Exp[U] = {
+      constlessExp transform {
+        case Sym(defNode) =>
+          //XXX We need to build here a symbol which does consider the id in the equality! But write a test for that first.
+          //Otherwise, CSE won't distinguish between Symbols in different scopes, since the IDs are not part of equality comparison. Darn!
+          toAtomCSE(defNode)
+        case c: Const[t] => throw new RuntimeException("Const expression in constlessExp")
+      }
     }
 
-    val symlessExp = collectSymbols(cseExp)
-    val maybeCons = expCodeCache.getOrElseUpdate(symlessExp, {
-      val (prefix, restSourceCode, className) = compileConstlessExp(symlessExp, cspValues)
-      ScalaCompile.invokeCompiler(prefix + restSourceCode, className) map (cls => cls.getConstructor(staticData.map(_._1.runtimeClass):_*))
-    })
-    buildInstance[T](maybeCons, staticData)
+    precompiledExpToValue(collectSymbols(doCSE(removeConsts(e))))
   }
 
-  def toValue[T: TypeTag](e: Exp[T]): T = {
-    val (transfExp, cspValues) = extractConsts(e)
-    val staticData = getStaticData(cspValues)
+  def toValue[T: TypeTag](e: Exp[T]): T =
+    precompiledExpToValue(removeConsts(e))
 
-    val maybeCons = expCodeCache.getOrElseUpdate(transfExp, {
-      val (prefix, restSourceCode, className) = compileConstlessExp(transfExp, cspValues)
-      ScalaCompile.invokeCompiler(prefix + restSourceCode, className) map (cls => cls.getConstructor(staticData.map(_._1.runtimeClass):_*))
+  private def precompiledExpToValue[T: TypeTag](precompiledExp: Exp[T]): T = {
+    val (cspValues, staticData) = extractCSPData(precompiledExp)
+
+    val maybeCons = expCodeCache.getOrElseUpdate(precompiledExp, {
+      val (prefix, restSourceCode, className) = compileConstlessExp(precompiledExp, cspValues)
+      ScalaCompile.invokeCompiler(prefix + restSourceCode, className) map (cls => cls
+        .getConstructor(staticData.map(_._1.runtimeClass): _*))
     })
     buildInstance(maybeCons, staticData)
   }
 
   //Only for testing
   private[expressiontree] def toCode[T: TypeTag](e: Exp[T]): String = {
-    val (transfExp, _) = extractConsts(e)
-    transfExp.toCode
+    removeConsts(e).toCode
   }
 
   private[expressiontree] def emitSource[T: TypeTag](e: Exp[T]): String = {
-    val (transfExp, cspValues) = extractConsts(e)
-    val (prefix, restSourceCode, _) = compileConstlessExp(transfExp, cspValues)
+    val precompiledExp = removeConsts(e)
+    val (cspValues, _) = extractCSPData(precompiledExp)
+
+    val (prefix, restSourceCode, _) = compileConstlessExp(precompiledExp, cspValues)
     prefix + restSourceCode
   }
 

commit 60d251b25b74d8f352d06b43fee84c0fc96aaee8

Test case for nested PersistValue nodes
This is the testcase called for in
bec5e89de53d66cf575c62bc018d99bb30497342.

Also, fix Compile.toCode, which I broke in
24aed765311009c7fa83f607d2d54cbdd6302c45. Add a comment
to prevent similar "cleanups".
Not too bad since it's just a testing method - what's bad is
that this use case was not really tested.

src/main/scala/ivm/expressiontree/Compile.scala
@@ -321,7 +321,9 @@ object Compile {
 
   //Only for testing
   private[expressiontree] def toCode[T: TypeTag](e: Exp[T]): String = {
-    removeConsts(e).toCode
+    val precompiledExp = removeConsts(e)
+    precompiledExp.persistValues() //We ignore the return values, but we need the side effects of this call.
+    precompiledExp.toCode
   }
 
   private[expressiontree] def emitSource[T: TypeTag](e: Exp[T]): String = {

src/test/scala/ivm/expressiontree/Compilation.scala
@@ -32,6 +32,21 @@ class Compilation extends FunSuite with ShouldMatchers with Benchmarking {
     toCode(asExp(List(1))) should be("x1")
   }
 
+  test("csp works for PersistValue nodes") {
+    Compile.precompileReset()
+    val exp = asExp(1).ifInstanceOf[Int]
+    toCode(exp) should be ("ivm.expressiontree.Util.ifInstanceOfBody(x1, x2): Option[Int]")
+    Compile toValue exp should be (Some(1))
+  }
+
+  test("csp works for nested PersistValue nodes") {
+    Compile.precompileReset()
+    val exp = asExp(1).ifInstanceOf[Int].ifInstanceOf[Option[Int]]
+    toCode(exp) should be (
+      "ivm.expressiontree.Util.ifInstanceOfBody(ivm.expressiontree.Util.ifInstanceOfBody(x1, x2): Option[Int], x3): Option[Option[Int]]")
+    Compile toValue exp should be (Some(Some(1)))
+  }
+
   val e = benchMark("build expression")(((asExp(1) + 2) * 3))
 
   test("code generation") {
@@ -46,6 +61,7 @@ class Compilation extends FunSuite with ShouldMatchers with Benchmarking {
     toCode(expOption2Iterable(Some(1)).transform(identity)) should be("Option.option2Iterable(x1)")
   }
   test("output") {
+    Compile.reset()
     if (Const.allowInlineInEval) {
       Compile.emitSource(e) should be(
         """class Outclass1() extends ivm.expressiontree.Compiled[Int] {