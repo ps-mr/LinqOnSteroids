7ff0f6ee6fd9d38359a28dae0dfedde5b606ddb3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-10-04 01:36:43 +0200

    Remove unused script

--- newPerformanceTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-#git pull
-nIters=3
-sbt test:compile
-for ((i=0; i < $nIters; i++)); do
-  out=testLog-`timestamp`
-  { echo -n "Git version: "; git describe --always; time sbt 'test-only performancetests.*'; } 2>&1|tee $out
-done
