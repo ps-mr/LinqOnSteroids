bbdd3fdc23025cfc9d1f57a22974f643f8822685
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-10-08 01:42:45 +0200

    Revert "Avoid excessive inlining by SQuOpt - workaround lack of CSE - part #2"
    This reverts commit f6bd40b67414d1177826d559136f48784b0b0e4f.
    Now CSE can automate this optimization.

--- src/main/scala/performancetests/opaltests/FBAnalysesBase.scala
+++ src/main/scala/performancetests/opaltests/FBAnalysesBase.scala
@@ -421,33 +421,33 @@ abstract class FBAnalysesBase extends QueryBenchmarking with ShouldMatchers {
     sourceMethod.body.get.instructions.exists {
         instr => {
              if_#(instr.isInstanceOf_#[INVOKEINTERFACE]) {
-                letExp(instr.asInstanceOf_#[INVOKEINTERFACE])(invoke =>
-                  invoke.declaringClass ==# targetClass.thisClass &&
-                  invoke.name ==# targetMethod.name &&
-                  invoke.methodDescriptor ==# targetMethod.descriptor)
+                val invoke = instr.asInstanceOf_#[INVOKEINTERFACE]
+                invoke.declaringClass ==# targetClass.thisClass &&
+                invoke.name ==# targetMethod.name &&
+                invoke.methodDescriptor ==# targetMethod.descriptor
              } else_# if_#(instr.isInstanceOf_#[INVOKEVIRTUAL]) {
-                letExp(instr.asInstanceOf_#[INVOKEVIRTUAL])(invoke =>
-                  invoke.declaringClass ==# targetClass.thisClass &&
-                  invoke.name ==# targetMethod.name &&
-                  invoke.methodDescriptor ==# targetMethod.descriptor)
+                val invoke = instr.asInstanceOf_#[INVOKEVIRTUAL]
+                invoke.declaringClass ==# targetClass.thisClass &&
+                invoke.name ==# targetMethod.name &&
+                invoke.methodDescriptor ==# targetMethod.descriptor
              } else_# if_#(instr.isInstanceOf_#[INVOKESTATIC]) {
-                letExp(instr.asInstanceOf_#[INVOKESTATIC])(invoke =>
-                  invoke.declaringClass ==# targetClass.thisClass &&
-                  invoke.name ==# targetMethod.name &&
-                  invoke.methodDescriptor ==# targetMethod.descriptor)
+                val invoke = instr.asInstanceOf_#[INVOKESTATIC]
+                invoke.declaringClass ==# targetClass.thisClass &&
+                invoke.name ==# targetMethod.name &&
+                invoke.methodDescriptor ==# targetMethod.descriptor
              } else_# if_#(instr.isInstanceOf_#[INVOKESPECIAL]) {
-                letExp(instr.asInstanceOf_#[INVOKESPECIAL])(invoke =>
-                  invoke.declaringClass ==# targetClass.thisClass &&
-                  invoke.name ==# targetMethod.name &&
-                  invoke.methodDescriptor ==# targetMethod.descriptor)
+                val invoke = instr.asInstanceOf_#[INVOKESPECIAL]
+                invoke.declaringClass ==# targetClass.thisClass &&
+                invoke.name ==# targetMethod.name &&
+                invoke.methodDescriptor ==# targetMethod.descriptor
              } else_# {
                 false
              }
-             /*
-           (instr.isInstanceOf_#[INVOKEINTERFACE] && letExp(instr.asInstanceOf_#[INVOKEINTERFACE])(invoke => invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor )) ||
-           (instr.isInstanceOf_#[INVOKEVIRTUAL] && letExp(instr.asInstanceOf_#[INVOKEVIRTUAL])(invoke => invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor )) ||
-           (instr.isInstanceOf_#[INVOKESTATIC] && letExp(instr.asInstanceOf_#[INVOKESTATIC])(invoke => invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor )) ||
-           (instr.isInstanceOf_#[INVOKESPECIAL] && letExp(instr.asInstanceOf_#[INVOKESPECIAL])(invoke => invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor ))
+           /*
+           (instr.isInstanceOf_#[INVOKEINTERFACE] && { val invoke = instr.asInstanceOf_#[INVOKEINTERFACE]; invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor }) ||
+           (instr.isInstanceOf_#[INVOKEVIRTUAL] && { val invoke = instr.asInstanceOf_#[INVOKEVIRTUAL]; invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor }) ||
+           (instr.isInstanceOf_#[INVOKESTATIC] && { val invoke = instr.asInstanceOf_#[INVOKESTATIC]; invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor }) ||
+           (instr.isInstanceOf_#[INVOKESPECIAL] && { val invoke = instr.asInstanceOf_#[INVOKESPECIAL]; invoke.declaringClass ==# targetClass.thisClass && invoke.name ==# targetMethod.name && invoke.methodDescriptor ==# targetMethod.descriptor })
            */
         }
     }
