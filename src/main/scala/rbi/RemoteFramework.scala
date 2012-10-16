a5be5dd8265bcbaeab8b3e5b45c14cdfa7144441
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-10-16 16:06:30 +0200

    Play more with remote DSL

--- src/main/scala/rbi/RemoteFramework.scala
+++ src/main/scala/rbi/RemoteFramework.scala
@@ -60,7 +60,7 @@ trait RemoteFramework {
 
   val AnywhereHost: Host
   def moveTo[T: Serializator](value: Host#Rep[T], dest: Host): dest.Rep[T]
-  implicit def autoMove[T: Serializator](value: Host#Rep[T])(implicit dest: Host): dest.Rep[T] = moveTo(value, dest)
+  implicit def move[T: Serializator](value: Host#Rep[T])(implicit dest: Host): dest.Rep[T] = moveTo(value, dest)
   //also takes a look at *TypeTag:
   //def in[U <: Universe with Singleton](otherMirror: MirrorOf[U]): U # AbsTypeTag[T]
   //Hm. I have no clue sure why they would want to use U# instead of passing u: U and writing u.AbsTypeTag[T]. But I also don't fully get mirrors.
@@ -70,9 +70,15 @@ trait ScalaRemoteFramework extends RemoteFramework with ScalaLanguageFramework {
   this: JavaSerializerFramework =>
   type Host <: HostApi
   trait HostApi extends LangIntf with super.HostApi
+  def onHostOf[T](h1: Host)(body: h1.type => T): T = {
+    body(h1)
+  }
+}
+
+trait ScalaRemoteFrameworkTest extends ScalaRemoteFramework with JavaSerializerFramework {
   def test(h: Host)(v: h.Rep[Int]): h.Rep[Int] = {
     import h.{pure => _, _} //XXX we need to remove pure from the interface, and replace it with our pureTo. This
-    // implies breaking tons of code.
+    // implies breaking tons of code. Rather make the type for constraints a separate typeclass
     v + 1
   }
   def test2(h1: Host, h2: Host)(v1: h1.Rep[Int], v2: h2.Rep[Int]) = {
@@ -83,12 +89,19 @@ trait ScalaRemoteFramework extends RemoteFramework with ScalaLanguageFramework {
   def testImplicit(h1: Host, h2: Host)(v1: h1.Rep[Int], v2: h2.Rep[Int]) = {
     import h1.{pure => _, _}
     implicit val h1Impl: h1.type = h1 //The type annotation is necessary
-//    v1 + v2
-    //Wrappers for methods should incorporate the call to autoMove, probably. That way, though, they get longer (because
+    //    v1 + v2 //fails to compile.
+    //Wrappers for methods should incorporate the call to move, probably. That way, though, they get longer (because
     //of all the constraints they need to incorporate.
-    v1 + autoMove(v2)
+    v1 + move(v2)
+  }
+  def testOnHostOf(h1: Host, h2: Host)(v1: h1.Rep[Int], v2: h2.Rep[Int]) = {
+    import h1.{pure => _, _}
+    onHostOf(h1){ implicit h1I =>
+      v1 + move(v2)
+    }
   }
 }
+
 object ScalaRemoteFrameworkImpl extends RemoteFramework with JavaSerializerFramework with ScalaLanguageFramework with ScalaRemoteFramework {
   type Host = HostApi
   private[this] def newHost(nHostAddr: Option[Address]): Host {type Rep[+T] = Exp[T]} = new HostApi with LiftingLangIntf with LiftingTrait {
