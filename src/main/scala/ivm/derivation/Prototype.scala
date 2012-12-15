03ca2fea42b035e15499209f8ab9c7c4c96a1180
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-12-15 02:34:42 +0100

    Continue WIP derivation

--- src/main/scala/ivm/derivation/Prototype.scala
+++ src/main/scala/ivm/derivation/Prototype.scala
@@ -12,8 +12,15 @@ import collection.IterableLike
  * Date: 13/12/2012
  */
 trait Prototype {
-  def derive[T, U: TypeTag](e: Exp[T], BaseColl: Exp[Traversable[U]]): Exp[T] = {
-    val DeltaV = Sym(Fun.gensym[Traversable[U]]())
+  def derive[T, U: TypeTag](e: Exp[T], BaseColl: Exp[Traversable[U]]): Exp[Traversable[U] => T] = {
+    val deltaVVar = Fun.gensym[Traversable[U]]()
+    val DeltaV = Sym(deltaVVar)
+    def correctDependencies(a: Exp[_], b: Exp[_]) =
+      (a isOrContains BaseColl) && !(a isOrContains DeltaV) &&
+      (b isOrContains DeltaV) /*
+      && !(b isOrContains BaseColl) //That's only required for self-maintainable views.
+      */
+
     val deriver = Transformer2 {
       case BaseColl =>
         //The interesting operator is not really union - it should be extended to handle also removals, but that's doable.
@@ -22,14 +29,15 @@ trait Prototype {
         //But for a Set or a Bag, no problem. For a Map, ?.
         //For a List[T], it's more a set of events like Add(pos: Int, el: T), or even Add(pos: Int, el: List[T]) (to
         // batch additions).
-      case Sym(MapNode(Sym(Union(BaseColl, DeltaV)), f)) => (BaseColl map f) union (DeltaV map f)
-      //This should match more something like Union of f(BaseColl) and g(DeltaV). More like:
+      //case Sym(MapNode(Sym(Union(BaseColl, DeltaV)), f)) => (BaseColl map f) union (DeltaV map f)
+      //This should match more something like Union of f(BaseColl) and g(DeltaV, BaseColl). I guess more something
+      // like:
 
-      case Sym(MapNode(Sym(Union(a: Exp[Traversable[_]], b: Exp[Traversable[_]])), f))
-        if (a isOrContains BaseColl) && !(a isOrContains DeltaV) &&
-                          !(b isOrContains BaseColl) && (b isOrContains DeltaV)
-      => (a map f) union (b map f)
+      case Sym(MapNode(Sym(Union(a, b)), f)) if correctDependencies(a, b) =>
+        (a map f) union (b map f)
 
+      case Sym(Union(Sym(Union(BaseColl, DeltaV)), coll)) =>
+        BaseColl union coll union DeltaV //Side condition: no ordering.
       case Sym(FlatMap(Sym(Union(BaseColl, DeltaV)), f)) => (BaseColl flatMap f) union (DeltaV flatMap f)
       case Sym(Filter(Sym(Union(BaseColl, DeltaV)), p)) => (BaseColl filter p) union (DeltaV filter p)
       case Sym(i: IndexBy[U, repr, k, that]) =>
@@ -39,7 +47,8 @@ trait Prototype {
             (BaseColl indexBy f) union (DeltaV indexBy f)
         }
     }
-    e transform (deriver | emptyTransform2)
+    val res = Sym(new FunInterp(e transform (deriver | emptyTransform2), deltaVVar))
+    res
   }
 }
 
--- src/test/scala/ivm/derivation/PrototypeTest.scala
+++ src/test/scala/ivm/derivation/PrototypeTest.scala
@@ -10,9 +10,24 @@ import org.scalatest._
  * Date: 13/12/2012
  */
 
-class PrototypeTest extends FunSuite with Prototype {
+class PrototypeTest extends FunSuite with ShouldMatchers with Prototype {
+  val l = asExp(List(1, 2, 3))
   test("derive") {
-    val l = asExp(List(1, 2, 3))
-    println (derive(l map (_ + 1), l))
+    val query1 = l map (_ + 1)
+    val query1p = derive(query1, l)
+    query1p should be (FunSym(Fun((x: Exp[List[Int]]) => l map (_ + 1) union (x map (_ + 1)))))
+    //println(query1p)
+    val query2 = query1 map (_ - 1)
+    val query2p = derive(query2, l)
+    query2p should be (FunSym(Fun((x: Exp[List[Int]]) => l map (_ + 1) map (_ - 1) union (x map (_ + 1) map (_ - 1)))))
+    //println(query2p)
+  }
+
+  test("derive more") {
+    val query = l union List(1)
+    //println(query)
+    val queryp = derive(query, l)
+    queryp should be (FunSym(Fun((x: Exp[List[Int]]) => query union x)))
+    //println(queryp)
   }
 }
