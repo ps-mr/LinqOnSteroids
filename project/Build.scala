ba04a5ebc60b4a0939b506d848f720ab5e35404f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-27 01:38:48 +0200

    Simplify Build

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object LoSBuild extends Build {
     lazy val root = Project(id = "los-root",
-        base = file("."), settings = Defaults.defaultSettings ++ CompileQuickPlugin.compileQuickSettings)
+        base = file("."))
 }
 
 // vim: set ts=4 sw=4 et:
