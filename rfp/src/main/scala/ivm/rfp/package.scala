4e9fc695abddf202c7d2d2785d57a67ea8755561
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-27 01:35:31 +0200

    Remove more misc Scala code (from other projects)

--- rfp/src/main/scala/ivm/rfp/package.scala
+++
@@ -1,7 +0,0 @@
-package ivm
-
-package object rfp {
-  type TravMessage[+T] = Message[Traversable[T]]
-  type TravMsgSeqPublisher[+T, +Pub <: TravMsgSeqPublisher[T, Pub]] = MsgSeqPublisher[Traversable[T], Pub]
-  type TravMsgSeqSubscriber[-T, -Repr] = MsgSeqSubscriber[Traversable[T], Repr]
-}
