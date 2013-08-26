8aaeeabd90b0a10f7edac14d92b047013955d4cf
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-27 01:25:42 +0200

    Remove yet more irrelevant stuff

--- src/main/scala/ivm/expressiontree/Exp.scala
+++ src/main/scala/ivm/expressiontree/Exp.scala
@@ -1,9 +1,3 @@
 package ivm.expressiontree
 
 sealed trait Exp[+T]
-
-trait Def[+T]
-
-case class Sym[+T](defNode: Def[T]) extends Exp[T]
-
-case class Const[T](x: T)(implicit val cTag: ClassTag[T], val tTag: TypeTag[T]) extends Exp[T]
