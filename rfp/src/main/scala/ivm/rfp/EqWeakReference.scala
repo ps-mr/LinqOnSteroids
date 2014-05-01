8aca117841489855d47d36653168f39b32cbad08
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-05-01 17:49:08 +0200

    EqWeakReference: cleanup getOrNull
    Somehow, getOrNull ignored this and took the object to act on as an
    argument.
    
    This was introduced in 3a8cf38d395ce1acfd2cbc4335f9bc7050de8e16, when
    turning getOrNull from a local function into a private method.

--- rfp/src/main/scala/ivm/rfp/EqWeakReference.scala
+++ rfp/src/main/scala/ivm/rfp/EqWeakReference.scala
@@ -12,21 +12,21 @@ import ref.WeakReference
  */
 class EqWeakReference[+T >: Null <: AnyRef](t: T) extends WeakReference[T](t: T) with Equals {
   override def canEqual(that: Any) = that.isInstanceOf[EqWeakReference[_]]
-  private def getOrNull[S >: Null <: AnyRef](x: WeakReference[S]): S = x.get.orNull
+  private def getOrNull(): T = get.orNull
   override def equals(other: Any) =
     other match {
       case that: AnyRef if that eq this => true
       case that: EqWeakReference[_] =>
         (that canEqual this) &&
         //XXX: use eq or equals? equals makes more sense in general, but eq makes more sense for our use case.
-        (getOrNull(this) eq getOrNull(that))
+        (this.getOrNull() eq that.getOrNull())
       case _ =>
         false
       // There is a definition coming from Proxy; however Proxy is used around java.lang.ref.WeakReference, which
       // uses identity comparison.
       //super.equals(other)
     }
-  override def hashCode() = System.identityHashCode(getOrNull(this)) //Use identityHashCode(_) instead of _.hashCode() to get a constant hashcode;
+  override def hashCode() = System.identityHashCode(getOrNull) //Use identityHashCode(_) instead of _.hashCode() to get a constant hashcode;
   // but call it on the actual object, not on some Option wrapper.
 }
 
