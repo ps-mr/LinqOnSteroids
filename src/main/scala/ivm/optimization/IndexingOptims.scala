f80104bda2dd28ca9f2d0d3ab3cc1df8b4347c63
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-06-15 13:16:15 +0200

    Drop commented out code, add comments
    Some comments are taken from the commented out code.

--- src/main/scala/ivm/optimization/IndexingOptims.scala
+++ src/main/scala/ivm/optimization/IndexingOptims.scala
@@ -29,6 +29,7 @@ trait IndexingOptims {
   //XXX Not very useful in general, we'd need the List monad instead. This allows prototyping the client code one would write.
   def FirstSplitList[T, U](Ext: Extractor[T, U]) = SplitList(Ext) map (_.head)
   class Comprehension[T](head: Exp[T], qualifiers: Seq[Qualifier]) {
+    //XXX Huge monolithic rule.
     def transformed = {
       val GenExtractor = extractor(Generator.unapply[Any] _)
       val EqPred = pfQualExtractor { case Predicate(Sym(Eq(a, b))) => Seq(a, b) }
@@ -52,100 +53,21 @@ trait IndexingOptims {
             (before2, eqOperands @ Seq(a, b), rest) <- splits2
           } yield {
             val MatchV = uniqueMatcherAndContexts(v)
+            //Look for (proj v == [ ]) or ([ ] == proj v), returns the content of the hole if found.
             (for {
               side <- eqOperands
               MatchV(vAgain, proj) = side
               if guard(vAgain == v && eqOperands.size == 2) //DEBUG
+              //XXX: we should iterate over permutations of the pair!
               otherSide = (eqOperands.toSet - side).head //XXX could fail if the two sides are equal. Normalize away such things before!
             } yield (proj, otherSide)) match {
               case Seq((proj, otherSide)) => // Check there's a single match.
+                //If we have two results, v appears in both, so we cannot lift the second one.
                 import Lifting._
                 //XXX finish reconstructing the target expression.
                 (before, coll indexBy proj apply otherSide, before2, rest)
             }
           }
-      }
-      //This tries the sequence at different points. Must readd the head!
-      qualifiers.tails collect {
-        case Seq(Generator(v, coll), /*..., */ Predicate(Sym(Eq(a, b))))
-        =>
-          val MatchV = uniqueMatcherAndContexts(v)
-          val eqOperands = Seq(a, b)
-          (for {
-            side <- eqOperands
-            MatchV(vAgain, proj) = side
-            if guard(vAgain == v) //DEBUG
-            otherSide = (eqOperands.toSet - side).head
-          } yield (proj, otherSide)) match {
-            case Seq((proj, otherSide)) => // Check there's a single match.
-              import Lifting._
-              coll indexBy proj apply otherSide
-          }
-        /*
-        case Seq(Generator(v, coll), /*..., */ Predicate(pred))
-          if pred isOrContains v
-        =>
-          //val MatchV = uniqueFinderAndContexts(comparer(v))
-          val getEqSides = pfExpExtractor { case Sym(Eq(a, b)) => Set(a, b) }
-          //val G = uniqueFinderAndContexts(getEqSides)
-          pred match {
-            case G(matches) =>
-              for { (vOccurs, ctx) <- matches } {
-                import Lifting._
-                coll indexBy ctx
-              }
-            case _ =>
-          }
-          /*flatMap { eqOperands =>
-            //Look for (proj v == [ ]) or ([ ] == proj v), returns the content of the hole if found.
-            val results = for {
-              side <- eqOperands
-              MatchV(vAgain, proj) = side
-              if guard(vAgain == v) //DEBUG
-              otherSide = (eqOperands - side).head
-            } yield (otherSide, proj)
-            if (results.size == 1)
-              Some(results.head)
-            else
-              None /*If we have two results, v appears in both, so we cannot lift the second one.
-              XXX too clever. */
-            /*if (eqOperands contains v)
-              Some((eqOperands - v).head)
-            else
-              None*/
-          }
-          */
-          /*
-          //Look for (v == [ ]) or ([ ] == v), returns the content of the hole if found.
-          val getEqSides = pfExpExtractor { case Sym(Eq(a, b)) => Set (a, b) } flatMap { eqOperands =>
-            val results = for {
-              side <- eqOperands
-              MatchV(vAgain, proj) = side
-              if guard(vAgain == v) //DEBUG
-              otherSide = (eqOperands - side).head
-            } yield (otherSide, proj)
-            if (results.size == 1)
-              Some(results.head)
-            else
-              None /*If we have two results, v appears in both, so we cannot lift the second one.
-              XXX too clever. */
-            /*if (eqOperands contains v)
-              Some((eqOperands - v).head)
-            else
-              None*/
-          }
-          //(pfExtractor[Exp[Any], Exp[Any]] { case Sym(Eq(`v`, u)) => u; case Sym(Eq(u, `v`)) => u })
-          val G = uniqueFinderAndContexts(getEqSides)
-          e match {
-            case G(matches) =>
-              for { (vOccurs, ctx) <- matches } {
-                import Lifting._
-                coll indexBy ctx
-              }
-            case _ =>
-          }
-          */
-          */
       }
     }
   }
